b426469e3eca72bf53a4a62734eadd4f91d1e042
fxzjshm <fxzjshm@163.com>
2022-06-25 16:33:10 +0800

    [prototype] add license

--- prototype/filterbank-reader.py
+++ prototype/filterbank-reader.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 
+################################################################################
+# Copyright (c) 2022 fxzjshm
+# This software is licensed under Mulan PubL v2.
+# You can use this software according to the terms and conditions of the Mulan PubL v2.
+# You may obtain a copy of Mulan PubL v2 at:
+#          http://license.coscl.org.cn/MulanPubL-2.0
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PubL v2 for more details.
+################################################################################
+
 # Usage: see `./filterbank-reader.py --help
 
 import sigpyproc
@@ -7,6 +19,7 @@ import sys
 import seaborn
 import matplotlib.pyplot as plt
 import argparse
+
 
 def main():
     parser = argparse.ArgumentParser(description="Dedisperse filterbank, and write time series or show heatmap")
