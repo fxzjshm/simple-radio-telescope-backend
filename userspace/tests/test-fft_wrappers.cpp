aeb314ab6b889409f33cafa41d52af11509a5389
fxzjshm <fxzjshm@163.com>
2022-07-04 00:38:21 +0800

    [userspace] [test] test-fft_wrapper: set environment correctly & print benchmark

--- userspace/tests/test-fft_wrappers.cpp
+++ userspace/tests/test-fft_wrappers.cpp
@@ -26,17 +26,29 @@ int main(int argc, char** argv) {
   srtb::config.baseband_input_length =
       n * srtb::config.baseband_input_bits / srtb::BITS_PER_BYTE;
   assert(srtb::config.unpacked_input_count() == n);
+  srtb::config.log_level = static_cast<int>(srtb::log::levels::DEBUG);
 
   std::vector<sycl::device> devices = sycl::device::get_devices();
   for (auto device : devices) {
     srtb::queue = sycl::queue{device};
-    auto allocator = srtb::memory::cached_allocator<
-        srtb::memory::device_allocator<std::byte, srtb::MEMORY_ALIGNMENT> >(
-        srtb::queue);
+    srtb::device_allocator = std::move(
+        srtb::memory::cached_allocator<
+            srtb::memory::device_allocator<std::byte, srtb::MEMORY_ALIGNMENT> >{
+            srtb::queue});
     {
-      auto in = allocator.allocate_smart<srtb::real>(n);
-      auto out = allocator.allocate_smart<srtb::complex>(n / 2 + 1);
+      srtb::fft::init_1d_r2c();
+      auto in = srtb::device_allocator.allocate_smart<srtb::real>(n);
+      auto out =
+          srtb::device_allocator.allocate_smart<srtb::complex>(n / 2 + 1);
+      auto start_time = std::chrono::system_clock::now();
       srtb::fft::dispatch_1d_r2c(in.get(), out.get());
+      auto end_time = std::chrono::system_clock::now();
+      auto run_time = end_time - start_time;
+      SRTB_LOGI << " [test fft wrappers] "
+                << "size = 2^" << bit << ", time = " << run_time.count()
+                << "ns, device name = " << '\"'
+                << device.get_info<sycl::info::device::name>() << '\"'
+                << std::endl;
     }
 
     {
