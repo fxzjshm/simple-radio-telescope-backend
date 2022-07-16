e427e078f7ae9fd8577da493ebbef7f47cbf75eb
fxzjshm <fxzjshm@163.com>
2022-07-16 17:33:12 +0800

    [userspace] add ROCm interop & refactor CMakeLists * auto detect CUDA & ROCm backends and generate compiler flags * add function `srtb_add_dependency` * export(?) SRTB_ENABLE_* as macro to correctly detect SYCL targets at compile time * fix warning message when no known backend of intel-llvm found

--- userspace/include/srtb/sycl.hpp
+++ userspace/include/srtb/sycl.hpp
@@ -27,11 +27,15 @@
 // TODO: platform specific things
 // clang-format off
 #if defined(SYCL_IMPLEMENTATION_ONEAPI)
-    #if defined(SYCL_EXT_ONEAPI_BACKEND_CUDA)
+    // maybe related: Compile-time definitions to detect used SYCL targets
+    //                https://github.com/intel/llvm/issues/5562
+    #if defined(SYCL_EXT_ONEAPI_BACKEND_CUDA) && defined(SRTB_ENABLE_CUDA) && SRTB_ENABLE_CUDA
         #define SRTB_ENABLE_CUDA_INTEROP 1
+    #elif defined(SYCL_EXT_ONEAPI_BACKEND_HIP) && defined(SRTB_ENABLE_ROCM) && SRTB_ENABLE_ROCM
+        #define SRTB_ENABLE_ROCM_INTEROP 1
     #else
-        #warning "Detected OneAPI but no known advice. (TODO)"
-    #endif  // defined(SYCL_EXT_ONEAPI_BACKEND_CUDA)
+        #warning "Detected OneAPI but no known backend. (TODO)"
+    #endif  // defined(SYCL_EXT_ONEAPI_BACKEND_CUDA) or defined(SYCL_EXT_ONEAPI_BACKEND_HIP)
 #elif defined(__HIPSYCL__)
     #if defined(__HIPSYCL_ENABLE_CUDA_TARGET__)
         #define SRTB_ENABLE_CUDA_INTEROP 1
