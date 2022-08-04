dbc9674ba0ddf48132b789f7d272254415811a4f
fxzjshm <fxzjshm@163.com>
2022-08-04 15:21:05 +0800

    [userspace] [udp receiver] add warning if nsamps_reserved > baseband_input_length

--- userspace/include/srtb/pipeline/udp_receiver_pipe.hpp
+++ userspace/include/srtb/pipeline/udp_receiver_pipe.hpp
@@ -71,7 +71,12 @@ class udp_receiver_pipe : public pipe<udp_receiver_pipe> {
 
     // reserved some samples for next round
     size_t nsamps_reserved = srtb::codd::nsamps_reserved();
-    worker.consume(baseband_input_length - nsamps_reserved);
+    
+    if (nsamps_reserved < baseband_input_length) {
+      worker.consume(baseband_input_length - nsamps_reserved);
+    } else {
+      SRTB_LOGW << " [udp receiver pipe] " << "baseband_input_length = " << baseband_input_length << " >= nsamps_reserved = " << nsamps_reserved << srtb::endl; 
+    }
     SRTB_LOGD << " [udp receiver pipe] "
               << "reserved " << nsamps_reserved << " samples" << srtb::endl;
 
