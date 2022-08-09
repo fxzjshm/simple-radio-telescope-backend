a7ecfb39f5113e15112ed5368d966a699ddfdb1d
fxzjshm <fxzjshm@163.com>
2022-08-09 13:21:50 +0800

    [userspace] [fft] fix typo

--- userspace/include/srtb/fft/cufft_wrapper.hpp
+++ userspace/include/srtb/fft/cufft_wrapper.hpp
@@ -92,7 +92,7 @@ class cufft_1d_r2c_wrapper_abstract
 #elif defined(__HIPSYCL__)
     // ref: https://github.com/illuhad/hipSYCL/issues/722
     cufftResult ret = CUFFT_SUCCESS;
-    q.submit([&](sycl::handler& cgh) {
+    queue.submit([&](sycl::handler& cgh) {
        cgh.hipSYCL_enqueue_custom_operation([&](sycl::interop_handle& h) {
          stream = h.get_native_queue<sycl::backend::cuda>();
          ret = cufftSetStream(plan, stream);
