fxzjshm/simple-radio-telescope-backend

commit 02d33198f89879f7e0ddb68c8056061369262c56

[userspace] signal_detect_pipe: add boxcar method

userspace/include/srtb/pipeline/baseband_output_pipe.hpp
@@ -42,7 +42,8 @@ template <bool continuous_write = false>
 class baseband_output_pipe;
 
 template <>
-class baseband_output_pipe<true> : public pipe<baseband_output_pipe<true> > {
+class baseband_output_pipe</* continuous_write = */ true>
+    : public pipe<baseband_output_pipe<true> > {
   friend pipe<baseband_output_pipe<true> >;
 
  protected:
@@ -61,6 +62,7 @@ class baseband_output_pipe<true> : public pipe<baseband_output_pipe<true> > {
     // since writing all baseband, ignore signal detect result
     while (!srtb::signal_detect_result_queue.empty()) {
       signal_detect_result_queue.pop();
+      // drop()
     }
 
     // file name need time stamp, so cannot create early
@@ -99,8 +101,11 @@ class baseband_output_pipe<true> : public pipe<baseband_output_pipe<true> > {
   }
 };
 
+// --------------------------------------------------------------------
+
 template <>
-class baseband_output_pipe<false> : public pipe<baseband_output_pipe<false> > {
+class baseband_output_pipe</* continuous_write = */ false>
+    : public pipe<baseband_output_pipe<false> > {
   friend pipe<baseband_output_pipe<false> >;
   std::vector<srtb::real> time_series_buffer;
 
@@ -130,51 +135,46 @@ class baseband_output_pipe<false> : public pipe<baseband_output_pipe<false> > {
                             srtb::signal_detect_result_queue,
                             signal_detect_result, stop_token);
     while (baseband_output_work.timestamp != signal_detect_result.timestamp)
-        [[unlikely]] {
-      if (baseband_output_work.timestamp < signal_detect_result.timestamp) {
-        SRTB_LOGW << " [baseband_output_pipe] "
-                  << "baseband_output_work.timestamp = "
-                  << baseband_output_work.timestamp << " < "
-                  << "signal_detect_result.timestamp = "
-                  << signal_detect_result.timestamp << srtb::endl;
-        SRTB_POP_WORK_OR_RETURN(" [baseband_output_pipe] ",
-                                srtb::baseband_output_queue,
-                                baseband_output_work, stop_token);
-      } else if (baseband_output_work.timestamp >
-                 signal_detect_result.timestamp) {
-        SRTB_LOGW << " [baseband_output_pipe] "
-                  << "baseband_output_work.timestamp = "
-                  << baseband_output_work.timestamp << " > "
-                  << "signal_detect_result.timestamp = "
-                  << signal_detect_result.timestamp << srtb::endl;
-        SRTB_POP_WORK_OR_RETURN(" [baseband_output_pipe] ",
-                                srtb::signal_detect_result_queue,
-                                signal_detect_result, stop_token);
-      } else {
-        SRTB_LOGE << " [baseband_output_pipe] "
-                  << "Logic error. Something must be wrong." << srtb::endl;
+      [[unlikely]] {
+        if (baseband_output_work.timestamp < signal_detect_result.timestamp) {
+          SRTB_LOGW << " [baseband_output_pipe] "
+                    << "baseband_output_work.timestamp = "
+                    << baseband_output_work.timestamp << " < "
+                    << "signal_detect_result.timestamp = "
+                    << signal_detect_result.timestamp << srtb::endl;
+          SRTB_POP_WORK_OR_RETURN(" [baseband_output_pipe] ",
+                                  srtb::baseband_output_queue,
+                                  baseband_output_work, stop_token);
+        } else if (baseband_output_work.timestamp >
+                   signal_detect_result.timestamp) {
+          SRTB_LOGW << " [baseband_output_pipe] "
+                    << "baseband_output_work.timestamp = "
+                    << baseband_output_work.timestamp << " > "
+                    << "signal_detect_result.timestamp = "
+                    << signal_detect_result.timestamp << srtb::endl;
+          SRTB_POP_WORK_OR_RETURN(" [baseband_output_pipe] ",
+                                  srtb::signal_detect_result_queue,
+                                  signal_detect_result, stop_token);
+        } else {
+          SRTB_LOGE << " [baseband_output_pipe] "
+                    << "Logic error. Something must be wrong." << srtb::endl;
+        }
       }
-    }
 
-    if (signal_detect_result.has_signal) {
+    const bool has_signal = (signal_detect_result.time_series.size() > 0);
+    if (has_signal) {
       auto timestamp = baseband_output_work.timestamp;
       SRTB_LOGI << " [baseband_output_pipe] "
                 << "Begin writing baseband data, timestamp = " << timestamp
                 << srtb::endl;
 
-      std::string file_name_no_extension =
+      const std::string file_name_no_extension =
           srtb::config.baseband_output_file_prefix + std::to_string(timestamp);
-      std::string baseband_file_path = file_name_no_extension + ".bin";
-      std::string time_series_file_path = file_name_no_extension + ".tim";
-      std::string time_series_picture_file_path =
-          file_name_no_extension + ".tim.pdf";
+      const std::string baseband_file_path = file_name_no_extension + ".bin";
 
       boost::iostreams::stream<boost::iostreams::file_descriptor_sink>
           baseband_output_stream{baseband_file_path,
                                  BOOST_IOS::binary | BOOST_IOS::out};
-      boost::iostreams::stream<boost::iostreams::file_descriptor_sink>
-          time_series_output_stream{time_series_file_path,
-                                    BOOST_IOS::binary | BOOST_IOS::out};
 
       // write original baseband data
       const char* baseband_ptr =
@@ -186,37 +186,67 @@ class baseband_output_pipe<false> : public pipe<baseband_output_pipe<false> > {
               sizeof(decltype(baseband_output_work.ptr)::element_type));
       baseband_output_stream.flush();
 
-      // write time series
-      const auto time_series_ptr = signal_detect_result.time_series_ptr.get();
-      const size_t time_series_length = signal_detect_result.time_series_length;
-      time_series_output_stream.write(
-          reinterpret_cast<const char*>(time_series_ptr),
-          time_series_length *
-              sizeof(decltype(signal_detect_result
-                                  .time_series_ptr)::element_type));
-      time_series_output_stream.flush();
+      // iterate over all time series, assumed with signal
+      for (auto time_series_holder : signal_detect_result.time_series) {
+        const auto boxcar_length = time_series_holder.boxcar_length;
+        const std::string time_series_file_path =
+            file_name_no_extension + "." + std::to_string(boxcar_length) +
+            ".tim";
+        const std::string time_series_picture_file_path =
+            time_series_file_path + ".pdf";
 
-      // draw time series using matplotlib cpp
-      try {
-        namespace plt = matplotlibcpp;
-        time_series_buffer.resize(signal_detect_result.time_series_length);
-        std::copy(time_series_ptr, time_series_ptr + time_series_length,
-                  time_series_buffer.begin());
-        plt::named_plot(time_series_file_path, time_series_buffer);
-        plt::save(time_series_picture_file_path);
-        plt::cla();
-      } catch (const std::runtime_error& error) {
-        SRTB_LOGW << " [baseband_output_pipe] "
-                  << "Failed to plot time series: " << error.what()
-                  << srtb::endl;
+        boost::iostreams::stream<boost::iostreams::file_descriptor_sink>
+            time_series_output_stream{time_series_file_path,
+                                      BOOST_IOS::binary | BOOST_IOS::out};
+
+        // write time series
+        const auto time_series_ptr = time_series_holder.time_series_ptr.get();
+        const size_t time_series_length = time_series_holder.time_series_length;
+        time_series_output_stream.write(
+            reinterpret_cast<const char*>(time_series_ptr),
+            time_series_length *
+                sizeof(decltype(time_series_holder
+                                    .time_series_ptr)::element_type));
+        time_series_output_stream.flush();
+
+        // draw time series using matplotlib cpp
+        try {
+          namespace plt = matplotlibcpp;
+          time_series_buffer.resize(time_series_length);
+          std::copy(time_series_ptr, time_series_ptr + time_series_length,
+                    time_series_buffer.begin());
+          plt::named_plot(time_series_file_path, time_series_buffer);
+          plt::save(time_series_picture_file_path);
+          plt::cla();
+        } catch (const std::runtime_error& error) {
+          SRTB_LOGW << " [baseband_output_pipe] "
+                    << "Failed to plot time series: " << error.what()
+                    << srtb::endl;
+        }
+
+        // check handle of time series data
+        if (time_series_output_stream) [[likely]] {
+#ifdef _POSIX_VERSION
+          // sometimes file is not fully written, so force syncing it
+          const int err = ::fdatasync(time_series_output_stream->handle());
+          if (err != 0) [[unlikely]] {
+            SRTB_LOGW << " [baseband_output_pipe] "
+                      << "Failed to call ::fdatasync" << srtb::endl;
+          }
+#endif
+        } else [[unlikely]] {
+          SRTB_LOGW << " [baseband_output_pipe] "
+                    << "Failed to write baseband data! timestamp = "
+                    << timestamp << srtb::endl;
+        }
       }
 
-      if (baseband_output_stream && time_series_output_stream) [[likely]] {
+      // check handle of baseband data
+      if (baseband_output_stream) [[likely]] {
 #ifdef _POSIX_VERSION
         // sometimes file is not fully written, so force syncing it
-        const int err1 = ::fdatasync(baseband_output_stream->handle());
-        const int err2 = ::fdatasync(time_series_output_stream->handle());
-        if (err1 != 0 || err2 != 0) [[unlikely]] {
+        const int err = ::fdatasync(baseband_output_stream->handle());
+        if (err != 0) [[unlikely]] {
           SRTB_LOGW << " [baseband_output_pipe] "
                     << "Failed to call ::fdatasync" << srtb::endl;
         }
@@ -224,7 +254,7 @@ class baseband_output_pipe<false> : public pipe<baseband_output_pipe<false> > {
         SRTB_LOGI << " [baseband_output_pipe] "
                   << "Finished writing baseband data, timestamp = " << timestamp
                   << srtb::endl;
-      } else {
+      } else [[unlikely]] {
         SRTB_LOGW << " [baseband_output_pipe] "
                   << "Failed to write baseband data! timestamp = " << timestamp
                   << srtb::endl;

.gitmodules
@@ -1,3 +1,6 @@
 [submodule "userspace/3rdparty/matplotlib-cpp"]
 	path = userspace/3rdparty/matplotlib-cpp
 	url = https://github.com/lava/matplotlib-cpp
+[submodule "userspace/3rdparty/SyclParallelSTL"]
+	path = userspace/3rdparty/SyclParallelSTL
+	url = https://github.com/fxzjshm/SyclParallelSTL

userspace/CMakeLists.txt
@@ -117,6 +117,7 @@ find_package(Python3 COMPONENTS Interpreter Development REQUIRED)
 find_package(Python3 COMPONENTS NumPy)
 include_directories(SYSTEM "3rdparty/")
 include_directories(SYSTEM "3rdparty/matplotlib-cpp/")
+include_directories(SYSTEM "3rdparty/SyclParallelSTL/include/")
 find_package(Qt5 COMPONENTS Quick Gui REQUIRED)
 
 

userspace/include/srtb/config.hpp
@@ -213,7 +213,7 @@ struct configs {
   size_t refft_length = 1 << 15;
 
   /**
-   * @brief threshold for signal detect.
+   * @brief threshold for signal detect, target signal / noise ratio
    *        If $ \exist x_i $ s.t. $$ x_i > \mu + k * \sigma $$,
    *        where $ x_i $ is a value of time series,
    *              $ \mu $ is its mean value, $ \sigma $ is variance,
@@ -221,6 +221,11 @@ struct configs {
    *        then it is thought a signal.
    */
   srtb::real signal_detect_threshold = 6;
+
+  /**
+   * @brief max boxcar length for signal detect.
+   */
+  size_t signal_detect_max_boxcar_length = 1024;
 
   /**
    * @brief Wait time in naneseconds for a thread to sleep if it fails to get work now.

userspace/include/srtb/pipeline/signal_detect_pipe.hpp
@@ -14,6 +14,8 @@
 #ifndef __SRTB_PIPELINE_SIGNAL_DETECT_PIPE__
 #define __SRTB_PIPELINE_SIGNAL_DETECT_PIPE__
 
+#include "sycl/execution_policy"
+// --- divide line for clang-format
 #include "srtb/commons.hpp"
 #include "srtb/pipeline/pipe.hpp"
 // --- divide line for clang-format
@@ -35,6 +37,9 @@ namespace pipeline {
  */
 class signal_detect_pipe : public pipe<signal_detect_pipe> {
   friend pipe<signal_detect_pipe>;
+
+ protected:
+  sycl::sycl_execution_policy<> execution_policy{q};
 
  protected:
   void run_once_impl(std::stop_token stop_token) {
@@ -81,10 +86,10 @@ class signal_detect_pipe : public pipe<signal_detect_pipe> {
     }
 
     // time series
-    const size_t out_count = batch_size;
-    auto d_out_shared =
-        srtb::device_allocator.allocate_shared<srtb::real>(out_count);
-    auto d_out = d_out_shared.get();
+    const size_t time_series_count = batch_size;
+    auto d_time_series_shared =
+        srtb::device_allocator.allocate_shared<srtb::real>(time_series_count);
+    auto d_time_series = d_time_series_shared.get();
 
     constexpr auto map = []([[maybe_unused]] size_t pos,
                             srtb::complex<srtb::real> c) {
@@ -92,7 +97,7 @@ class signal_detect_pipe : public pipe<signal_detect_pipe> {
     };
 
     srtb::algorithm::multi_mapreduce(
-        d_in, count_per_batch, batch_size, d_out, map,
+        d_in, count_per_batch, batch_size, d_time_series, map,
         /* reduce = */ sycl::plus<srtb::real>(), q);
 
     d_in = nullptr;
@@ -106,42 +111,113 @@ class signal_detect_pipe : public pipe<signal_detect_pipe> {
     // TODO: does baseline changes a lot in this time scale ? Is a linear approximation needed ?
     {
       auto d_average_shared = srtb::algorithm::map_average(
-          d_out, out_count, srtb::algorithm::map_identity(), q);
+          d_time_series, time_series_count, srtb::algorithm::map_identity(), q);
       auto d_average = d_average_shared.get();
-      q.parallel_for(sycl::range<1>{out_count}, [=](sycl::item<1> item) {
-         const auto i = item.get_id(0);
-         d_out[i] -= (*d_average);
-       }).wait();
+      q.parallel_for(sycl::range<1>{time_series_count},
+                     [=](sycl::item<1> item) {
+                       const auto i = item.get_id(0);
+                       d_time_series[i] -= (*d_average);
+                     })
+          .wait();
     }
+    // time series is now available
 
-    // TODO: matched filter
+    srtb::work::signal_detect_result signal_detect_result{
+        .timestamp = signal_detect_work.timestamp};
 
-    // trivial signal detect
-    {
-      const size_t h_signal_count = srtb::signal_detect::count_signal(
-          d_out, out_count, signal_detect_threshold, q);
-
-      // if too many frequency channels are masked, result is often inaccurate
-      const bool has_signal =
-          ((zero_count < 0.9 * count_per_batch) && (h_signal_count > 0));
-      srtb::work::signal_detect_result signal_detect_result{
-          .timestamp = signal_detect_work.timestamp,
-          .has_signal = has_signal,
-          .time_series_ptr = std::shared_ptr<srtb::real>{},
-          .time_series_length = 0};
-      if (has_signal) {
-        signal_detect_result.time_series_ptr = d_out_shared;
-        signal_detect_result.time_series_length = out_count;
-        SRTB_LOGI << " [signal_detect_pipe] " << h_signal_count
-                  << " signal(s) detected!" << srtb::endl;
-      } else {
-        SRTB_LOGD << " [signal_detect_pipe] "
-                  << "no signal detected." << srtb::endl;
+    // if too many frequency channels are masked, result is often inaccurate
+    if (zero_count < 0.9 * count_per_batch) {
+      // trivial signal detect on raw time series
+      {
+        const size_t h_signal_count = srtb::signal_detect::count_signal(
+            d_time_series, time_series_count, signal_detect_threshold, q);
+        const bool has_signal = (h_signal_count > 0);
+        if (has_signal) /* [[unlikely]] */ {
+          srtb::work::time_series_holder time_series_holder{
+              .time_series_ptr = d_time_series_shared,
+              .time_series_length = time_series_count,
+              .boxcar_length = 1};
+          signal_detect_result.time_series.push_back(time_series_holder);
+        }
       }
-      SRTB_PUSH_WORK_OR_RETURN(" [signal_detect_pipe] ",
-                               srtb::signal_detect_result_queue,
-                               signal_detect_result, stop_token);
+
+      // boxcar method
+      // ref: heimdall
+      {
+        auto d_accumulated_unique_ptr =
+            srtb::device_allocator.allocate_unique<srtb::real>(
+                time_series_count);
+        auto d_accumulated = d_accumulated_unique_ptr.get();
+        sycl::impl::inclusive_scan(execution_policy, d_time_series,
+                                   d_time_series + time_series_count,
+                                   d_accumulated, srtb::real{0}, std::plus());
+        // TODO: reuse d_time_series ?
+        // this is reused for different boxcar_length
+        auto d_boxcared_time_series_unique_ptr =
+            srtb::device_allocator.allocate_unique<srtb::real>(
+                time_series_count);
+        auto d_boxcared_time_series = d_boxcared_time_series_unique_ptr.get();
+
+        const size_t max_boxcar_length =
+            srtb::config.signal_detect_max_boxcar_length;
+        // TODO: async submit kernel ?
+        for (size_t boxcar_length = 2; (boxcar_length <= max_boxcar_length &&
+                                        boxcar_length < time_series_count);
+             boxcar_length *= 2) {
+          // compute boxcared time series
+          const size_t boxcared_time_series_count =
+              time_series_count - boxcar_length + 1;
+          q.parallel_for(sycl::range{boxcared_time_series_count},
+                         [=](sycl::item<1> id) {
+                           const size_t i = id.get_id(0);
+                           d_boxcared_time_series[i] =
+                               d_accumulated[i + boxcar_length] -
+                               d_accumulated[i];
+                         })
+              .wait();
+
+          // trivially detect signal, again
+          const size_t h_signal_count = srtb::signal_detect::count_signal(
+              d_boxcared_time_series, boxcared_time_series_count,
+              signal_detect_threshold, q);
+          const bool has_signal = (h_signal_count > 0);
+          if (has_signal) /* [[unlikely]] */ {
+            // d_boxcared_time_series is reused for different boxcar_length,
+            // so need to copy if a signal is detected
+            auto d_out_shared =
+                srtb::device_allocator.allocate_shared<srtb::real>(
+                    time_series_count);
+            auto d_out = d_out_shared.get();
+            q.copy(d_boxcared_time_series, /* -> */ d_out,
+                   boxcared_time_series_count)
+                .wait();
+            srtb::work::time_series_holder time_series_holder{
+                .time_series_ptr = std::move(d_out_shared),
+                .time_series_length = boxcared_time_series_count,
+                .boxcar_length = boxcar_length};
+            signal_detect_result.time_series.push_back(time_series_holder);
+          }
+        }
+      }
+    } else {
+      // signal_detect_result.has_signal = false;
+      // currently represented as time_series.size() == 0
+      // that is, do nothing
     }
+
+    const bool has_signal = (signal_detect_result.time_series.size() > 0);
+    if (has_signal) {
+      SRTB_LOGI << " [signal_detect_pipe] "
+                << " signal detected in "
+                << signal_detect_result.time_series.size() << " time series"
+                << srtb::endl;
+    } else {
+      SRTB_LOGD << " [signal_detect_pipe] "
+                << "no signal detected" << srtb::endl;
+    }
+    SRTB_PUSH_WORK_OR_RETURN(" [signal_detect_pipe] ",
+                             srtb::signal_detect_result_queue,
+                             signal_detect_result, stop_token);
   }
 };
 

userspace/include/srtb/program_options.hpp
@@ -116,6 +116,8 @@ namespace program_options {
        "of complex numbers, so refft_length <= baseband_input_count / 2")
       ("signal_detect_threshold", boost::program_options::value<std::string>(),
        "threshold for signal detect, target signal / noise ratio")
+      ("signal_detect_max_boxcar_length", boost::program_options::value<std::string>(),
+       "max boxcar length for signal detect")
     ;
   /* clang-format on */
   data_io_option.add(udp_receiver_options).add(file_io_options);
@@ -221,6 +223,7 @@ inline void evaluate_and_apply_changed_config(const std::string& name,
   SRTB_ASSIGN(mitigate_rfi_freq_list)
   SRTB_PARSE(refft_length)
   SRTB_PARSE(signal_detect_threshold)
+  SRTB_PARSE(signal_detect_max_boxcar_length)
   SRTB_PARSE(thread_query_work_wait_time)
   /* else */ {
     SRTB_LOGW << " [program_options] "

userspace/include/srtb/work.hpp
@@ -16,6 +16,8 @@
 
 #include <boost/lockfree/spsc_queue.hpp>
 #include <thread>
+#include <vector>
+#include <memory>
 
 #include "srtb/config.hpp"
 
@@ -200,20 +202,29 @@ using signal_detect_work = simplify_spectrum_work;
 
 /**
  * @brief contains baseband data without UDP packet counter in host memory.
- * TODO: put this in a pool or first-in-first-out queue, and write data only if signal is detected.
  */
 using baseband_output_work = srtb::work::work<std::shared_ptr<std::byte> >;
 
 /**
- * @brief contains timestamp of a work & whether it has signal.
+ * @brief contains info for a time series of a spectrum
+ */
+struct time_series_holder {
+  std::shared_ptr<srtb::real> time_series_ptr;
+  size_t time_series_length;
+  /** @brief size of boxcar used to compute the time series; = 1 if not used */
+  size_t boxcar_length;
+};
+
+/**
+ * @brief contains timestamp of a work & signal related time-series.
  *        If it has signal, @c ptr is host pointer containing dedispersed baseband data
  *        Should be generated by @c signal_detect_pipe and received by @c baseband_output_pipe
+ * @note time_series.size() == 0 means no signal
+ * @see time_series_holder
  */
 struct signal_detect_result {
   uint64_t timestamp;
-  bool has_signal;
-  std::shared_ptr<srtb::real> time_series_ptr;
-  size_t time_series_length;
+  std::vector<time_series_holder> time_series;
 };
 
 // work queues are in global_variables.hpp

userspace/srtb_config.cfg
@@ -1,6 +1,6 @@
 # example config file
 baseband_input_count = 2 ** 29
-refft_length = 2 ** 19
+refft_length = 2 ** 12
 baseband_input_bits = 8
 baseband_output_file_prefix = /dev/shm/srtb_baseband_
 udp_receiver_cpu_preferred = 29
@@ -8,6 +8,7 @@ log_level = 3
 mitigate_rfi_average_method_threshold = 5
 mitigate_rfi_spectral_kurtosis_threshold = 1.05
 signal_detect_threshold = 8
+signal_detect_max_boxcar_length = 2048
 
 # for example cpsr2 file from dspsr, 
 # "By default, the data from each polarization is interleaved byte by byte"

commit 9521ef933e27fd0d8319ebd7575c09a38cf55862

[userspace] various fixes
* baseband_output_pipe: remove extension in file name when checking path
  * so that some script will not identify it as a baseband output file
* baseband_output_pipe: fix incorrect format caused by old clang-format
* pipe: add missing include
* pipe: unify constructor
* main: correct comment

Fixes: 02d3319 ("[userspace] signal_detect_pipe: add boxcar method")

userspace/include/srtb/pipeline/baseband_output_pipe.hpp
@@ -112,7 +112,7 @@ class baseband_output_pipe</* continuous_write = */ false>
  public:
   baseband_output_pipe() {
     std::string check_file_path =
-        srtb::config.baseband_output_file_prefix + "check.bin";
+        srtb::config.baseband_output_file_prefix + "check";
     try {
       boost::iostreams::stream<boost::iostreams::file_descriptor_sink>
           baseband_output_stream{check_file_path,
@@ -135,31 +135,31 @@ class baseband_output_pipe</* continuous_write = */ false>
                             srtb::signal_detect_result_queue,
                             signal_detect_result, stop_token);
     while (baseband_output_work.timestamp != signal_detect_result.timestamp)
-      [[unlikely]] {
-        if (baseband_output_work.timestamp < signal_detect_result.timestamp) {
-          SRTB_LOGW << " [baseband_output_pipe] "
-                    << "baseband_output_work.timestamp = "
-                    << baseband_output_work.timestamp << " < "
-                    << "signal_detect_result.timestamp = "
-                    << signal_detect_result.timestamp << srtb::endl;
-          SRTB_POP_WORK_OR_RETURN(" [baseband_output_pipe] ",
-                                  srtb::baseband_output_queue,
-                                  baseband_output_work, stop_token);
-        } else if (baseband_output_work.timestamp >
-                   signal_detect_result.timestamp) {
-          SRTB_LOGW << " [baseband_output_pipe] "
-                    << "baseband_output_work.timestamp = "
-                    << baseband_output_work.timestamp << " > "
-                    << "signal_detect_result.timestamp = "
-                    << signal_detect_result.timestamp << srtb::endl;
-          SRTB_POP_WORK_OR_RETURN(" [baseband_output_pipe] ",
-                                  srtb::signal_detect_result_queue,
-                                  signal_detect_result, stop_token);
-        } else {
-          SRTB_LOGE << " [baseband_output_pipe] "
-                    << "Logic error. Something must be wrong." << srtb::endl;
-        }
+        [[unlikely]] {
+      if (baseband_output_work.timestamp < signal_detect_result.timestamp) {
+        SRTB_LOGW << " [baseband_output_pipe] "
+                  << "baseband_output_work.timestamp = "
+                  << baseband_output_work.timestamp << " < "
+                  << "signal_detect_result.timestamp = "
+                  << signal_detect_result.timestamp << srtb::endl;
+        SRTB_POP_WORK_OR_RETURN(" [baseband_output_pipe] ",
+                                srtb::baseband_output_queue,
+                                baseband_output_work, stop_token);
+      } else if (baseband_output_work.timestamp >
+                 signal_detect_result.timestamp) {
+        SRTB_LOGW << " [baseband_output_pipe] "
+                  << "baseband_output_work.timestamp = "
+                  << baseband_output_work.timestamp << " > "
+                  << "signal_detect_result.timestamp = "
+                  << signal_detect_result.timestamp << srtb::endl;
+        SRTB_POP_WORK_OR_RETURN(" [baseband_output_pipe] ",
+                                srtb::signal_detect_result_queue,
+                                signal_detect_result, stop_token);
+      } else {
+        SRTB_LOGE << " [baseband_output_pipe] "
+                  << "Logic error. Something must be wrong." << srtb::endl;
       }
+    }
 
     const bool has_signal = (signal_detect_result.time_series.size() > 0);
     if (has_signal) {

userspace/include/srtb/pipeline/pipe.hpp
@@ -18,6 +18,10 @@
 #include <thread>
 #include <type_traits>
 
+#if __has_include(<pthread.h>)
+#include <pthread.h>
+#endif
+
 #include "srtb/commons.hpp"
 
 namespace srtb {
@@ -100,11 +104,7 @@ class pipe {
   // run on main thread, so setup_impl() & teardown_impl() are needed.
 
  private:
-  pipe() {
-    q = sycl::queue{srtb::queue.get_context(), srtb::queue.get_device()};
-  }
-
-  pipe(sycl::queue q_) : q{q_} {};
+  pipe(sycl::queue q_ = srtb::queue) : q{q_} {};
 
   ~pipe() {
     // wait until all operations in this pipe finish, then exit

userspace/src/main.cpp
@@ -149,7 +149,7 @@ int main(int argc, char** argv) {
     srtb::queue.single_task([=]() { (*d_out) = srtb::real{42}; }).wait();
   }
 
-  // TODO std::thread for other pipelines
+  // TODO std::jthread for other pipelines
 
   SRTB_LOGI << " [main] "
             << "device name = "