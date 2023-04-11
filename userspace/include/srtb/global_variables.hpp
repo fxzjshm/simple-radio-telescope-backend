fxzjshm/simple-radio-telescope-backend

commit e42144901d47b6dcb97dbabe7264c708178620ae

[userspace] udp_receiver_pipe: start multiple receiver pipes

userspace/src/main.cpp
@@ -53,66 +53,68 @@ inline namespace detail {
  * 
  * this function is quite ugly, and should be updated once pipeline structure chnages.
  */
-inline void allocate_memory_regions() {
+inline void allocate_memory_regions(size_t input_pipe_count) {
   // hold all pointers; using RAII
   std::vector<std::shared_ptr<std::byte> > ptrs;
 
-  // host side udp receiver buffer, raw baseband data
-  for (size_t i = 0; i < 5; i++) {
-    ptrs.push_back(srtb::host_allocator.allocate_shared<std::byte>(
+  for (size_t k = 0; k < input_pipe_count; k++) {
+    // host side udp receiver buffer, raw baseband data
+    for (size_t i = 0; i < 5; i++) {
+      ptrs.push_back(srtb::host_allocator.allocate_shared<std::byte>(
+          sizeof(std::byte) * srtb::config.baseband_input_count *
+          std::abs(srtb::config.baseband_input_bits) / srtb::BITS_PER_BYTE));
+    }
+
+    // device side raw baseband data, to be unpacked
+    ptrs.push_back(srtb::device_allocator.allocate_shared<std::byte>(
         sizeof(std::byte) * srtb::config.baseband_input_count *
         std::abs(srtb::config.baseband_input_bits) / srtb::BITS_PER_BYTE));
-  }
 
-  // device side raw baseband data, to be unpacked
-  ptrs.push_back(srtb::device_allocator.allocate_shared<std::byte>(
-      sizeof(std::byte) * srtb::config.baseband_input_count *
-      std::abs(srtb::config.baseband_input_bits) / srtb::BITS_PER_BYTE));
-
-  // device side unpacked baseband data / FFT-ed spectrum / STFT-ed waterfall (if in place)
-  ptrs.push_back(srtb::device_allocator.allocate_shared<std::byte>(
-      sizeof(srtb::complex<srtb::real>) *
-      (srtb::config.baseband_input_count / 2 + 1)));
-
-  // device side time series (original / accumulated / boxcar-ed)
-  for (size_t i = 0; i < 3; i++) {
+    // device side unpacked baseband data / FFT-ed spectrum / STFT-ed waterfall (if in place)
     ptrs.push_back(srtb::device_allocator.allocate_shared<std::byte>(
-        sizeof(srtb::real) * srtb::config.baseband_input_count /
-        srtb::config.refft_length / 2));
-  }
+        sizeof(srtb::complex<srtb::real>) *
+        (srtb::config.baseband_input_count / 2 + 1)));
 
-  // host side time series for a segment of baseband
-  for (size_t w = 1; w <= srtb::config.signal_detect_max_boxcar_length;
-       w *= 2) {
-    ptrs.push_back(srtb::host_allocator.allocate_shared<std::byte>(
-        sizeof(srtb::real) * srtb::config.baseband_input_count /
-        srtb::config.refft_length / 2));
-  }
+    // device side time series (original / accumulated / boxcar-ed)
+    for (size_t i = 0; i < 3; i++) {
+      ptrs.push_back(srtb::device_allocator.allocate_shared<std::byte>(
+          sizeof(srtb::real) * srtb::config.baseband_input_count /
+          srtb::config.refft_length / 2));
+    }
 
-  // device side STFT buffer, for spectrural kurtosis & mean value
-  for (size_t i = 0; i < 2; i++) {
+    // host side time series for a segment of baseband
+    for (size_t w = 1; w <= srtb::config.signal_detect_max_boxcar_length;
+         w *= 2) {
+      ptrs.push_back(srtb::host_allocator.allocate_shared<std::byte>(
+          sizeof(srtb::real) * srtb::config.baseband_input_count /
+          srtb::config.refft_length / 2));
+    }
+
+    // device side STFT buffer, for spectrural kurtosis & mean value
+    for (size_t i = 0; i < 2; i++) {
+      ptrs.push_back(srtb::device_allocator.allocate_shared<std::byte>(
+          sizeof(srtb::complex<srtb::real>) * srtb::config.refft_length));
+    }
+
+    // host & device side waterfall
     ptrs.push_back(srtb::device_allocator.allocate_shared<std::byte>(
-        sizeof(srtb::complex<srtb::real>) * srtb::config.refft_length));
-  }
-
-  // host & device side waterfall
-  ptrs.push_back(srtb::device_allocator.allocate_shared<std::byte>(
-      sizeof(srtb::real) *
-      (srtb::config.baseband_input_count - srtb::codd::nsamps_reserved()) /
-      srtb::config.refft_length / 2 * srtb::gui::spectrum::width));
-  for (size_t i = 0; i < 5; i++) {
-    ptrs.push_back(srtb::host_allocator.allocate_shared<std::byte>(
         sizeof(srtb::real) *
         (srtb::config.baseband_input_count - srtb::codd::nsamps_reserved()) /
         srtb::config.refft_length / 2 * srtb::gui::spectrum::width));
-  }
+    for (size_t i = 0; i < 5; i++) {
+      ptrs.push_back(srtb::host_allocator.allocate_shared<std::byte>(
+          sizeof(srtb::real) *
+          (srtb::config.baseband_input_count - srtb::codd::nsamps_reserved()) /
+          srtb::config.refft_length / 2 * srtb::gui::spectrum::width));
+    }
 
-  // misc value holders
-  for (size_t i = 0; i < 2; i++) {
-    ptrs.push_back(
-        srtb::device_allocator.allocate_shared<std::byte>(sizeof(srtb::real)));
-    ptrs.push_back(srtb::device_allocator.allocate_shared<std::byte>(
-        sizeof(srtb::complex<srtb::real>)));
+    // misc value holders
+    for (size_t i = 0; i < 2; i++) {
+      ptrs.push_back(srtb::device_allocator.allocate_shared<std::byte>(
+          sizeof(srtb::real)));
+      ptrs.push_back(srtb::device_allocator.allocate_shared<std::byte>(
+          sizeof(srtb::complex<srtb::real>)));
+    }
   }
 
   // ptrs.drop(); a.k.a. ~ptrs();
@@ -131,7 +133,12 @@ int main(int argc, char** argv) {
   cudaSetDeviceFlags(cudaDeviceScheduleYield);
 #endif
 
-  allocate_memory_regions();
+  const size_t input_pipe_count =
+      std::max(std::max(srtb::config.udp_receiver_sender_address.size(),
+                        srtb::config.udp_receiver_sender_port.size()),
+               size_t{1});
+
+  allocate_memory_regions(input_pipe_count);
 
   // trigger JIT
   // some implementations may pack intermediate representation with executable binary
@@ -149,12 +156,12 @@ int main(int argc, char** argv) {
             << srtb::queue.get_device().get_info<sycl::info::device::name>()
             << srtb::endl;
 
-  std::jthread input_thread;
+  std::vector<std::jthread> input_thread;
   auto input_file_path = srtb::config.input_file_path;
   if (std::filesystem::exists(input_file_path)) {
     SRTB_LOGI << " [main] "
               << "Reading file " << input_file_path << srtb::endl;
-    input_thread = srtb::pipeline::read_file_pipe::start();
+    input_thread.push_back(srtb::pipeline::read_file_pipe::start());
   } else {
     if (input_file_path != "") {
       SRTB_LOGE << " [main] "
@@ -163,7 +170,10 @@ int main(int argc, char** argv) {
     }
     SRTB_LOGI << " [main] "
               << "Receiving UDP packets" << srtb::endl;
-    input_thread = srtb::pipeline::udp_receiver_pipe::start();
+    for (size_t i = 0; i < input_pipe_count; i++) {
+      input_thread.push_back(
+          srtb::pipeline::udp_receiver_pipe::start(/* id = */ i));
+    }
   }
 
   std::jthread unpack_thread = srtb::pipeline::unpack_pipe::start();
@@ -199,7 +209,7 @@ int main(int argc, char** argv) {
       srtb::pipeline::simplify_spectrum_pipe::start();
 
   std::vector<std::jthread> threads;
-  threads.push_back(std::move(input_thread));
+  threads = std::move(input_thread);
   threads.push_back(std::move(unpack_thread));
   threads.push_back(std::move(fft_1d_r2c_thread));
   threads.push_back(std::move(rfi_mitigation_thread));
@@ -211,7 +221,7 @@ int main(int argc, char** argv) {
   threads.push_back(std::move(simplify_spectrum_thread));
 
   srtb::pipeline::expected_running_pipe_count = threads.size();
-  srtb::pipeline::expected_input_pipe_count = 1;
+  srtb::pipeline::expected_input_pipe_count = input_pipe_count;
 
   return srtb::gui::show_gui(argc, argv, std::move(threads));
 }

userspace/include/srtb/config.hpp
@@ -18,6 +18,7 @@
 #include <limits>
 #include <string>
 #include <string_view>
+#include <vector>
 
 #include "srtb/sycl.hpp"
 
@@ -132,17 +133,17 @@ struct configs {
   /**
    * @brief Address to receive baseband UDP packets.
    */
-  std::string udp_receiver_sender_address = "10.0.1.2";
+  std::vector<std::string> udp_receiver_sender_address = {"10.0.1.2"};
 
   /**
    * @brief Port to receive baseband UDP packets.
    */
-  unsigned short udp_receiver_sender_port = 12004;
+  std::vector<unsigned short> udp_receiver_sender_port = {12004};
 
   /**
    * @brief CPU core that UDP receiver should be bound to.
    */
-  unsigned int udp_receiver_cpu_preferred = 0;
+  std::vector<unsigned int> udp_receiver_cpu_preferred = {0};
 
   /**
    * @brief Path to the binary file to be read as baseband input.

userspace/include/srtb/pipeline/udp_receiver_pipe.hpp
@@ -34,11 +34,17 @@ class udp_receiver_pipe : public pipe<udp_receiver_pipe> {
 
  protected:
   std::optional<srtb::io::udp_receiver::udp_receiver_worker> opt_worker;
+  /**
+   * @brief identifier for different pipe instances
+   * 
+   * e.g. id == 0 and 1 for two polarizations
+   */
+  size_t id;
 
  public:
   // init of worker is deferred because this pipe may not be used,
   // and failure of binding address will result in a error
-  udp_receiver_pipe() = default;
+  udp_receiver_pipe(size_t id_ = 0) : id{id_} {};
 
  protected:
   void setup_impl([[maybe_unused]] std::stop_token stop_token) {
@@ -51,14 +57,57 @@ class udp_receiver_pipe : public pipe<udp_receiver_pipe> {
           std::chrono::nanoseconds(srtb::config.thread_query_work_wait_time));
     }
 
-    const std::string& sender_address =
-        srtb::config.udp_receiver_sender_address;
-    const unsigned short sender_port = srtb::config.udp_receiver_sender_port;
+    std::string sender_address;
+    {
+      const auto& sender_addresses = srtb::config.udp_receiver_sender_address;
+      if (sender_addresses.size() == 1) {
+        sender_address = sender_addresses.at(0);
+      } else if (id < sender_addresses.size()) {
+        sender_address = sender_addresses.at(id);
+      } else {
+        SRTB_LOGE << " [udp receiver pipe] "
+                  << "id = " << id << ": "
+                  << "no UDP sender address set" << srtb::endl;
+        throw std::runtime_error{
+            " [udp receiver pipe] no UDP sender address for id = " +
+            std::to_string(id)};
+      }
+    }
+    unsigned short sender_port;
+    {
+      const auto& sender_ports = srtb::config.udp_receiver_sender_port;
+      if (sender_ports.size() == 1) {
+        sender_port = sender_ports.at(0);
+      } else if (id < sender_ports.size()) {
+        sender_port = sender_ports.at(id);
+      } else {
+        SRTB_LOGE << " [udp receiver pipe] "
+                  << "id = " << id << ": "
+                  << "no UDP sender port set" << srtb::endl;
+        throw std::runtime_error{
+            " [udp receiver pipe] no UDP sender port for id = " +
+            std::to_string(id)};
+      }
+    }
     const auto udp_receiver_buffer_size = srtb::config.udp_receiver_buffer_size;
     opt_worker.emplace(sender_address, sender_port, udp_receiver_buffer_size);
 
-    srtb::thread_affinity::set_thread_affinity(
-        srtb::config.udp_receiver_cpu_preferred);
+    const auto& cpus_preferred = srtb::config.udp_receiver_cpu_preferred;
+    if (0 <= id && id < cpus_preferred.size()) {
+      const auto cpu_preferred = cpus_preferred.at(id);
+      srtb::thread_affinity::set_thread_affinity(cpu_preferred);
+    } else {
+      // no preferred CPU is set, so not setting thread affinity
+      SRTB_LOGW << " [udp receiver pipe] "
+                << "id = " << id << ": "
+                << "CPU affinity not set, performance may be degraded"
+                << srtb::endl;
+    }
+
+    SRTB_LOGI << " [udp receiver pipe] "
+              << "id = " << id << ": "
+              << "start reading, address = " << sender_address << ", "
+              << "port = " << sender_port << srtb::endl;
   }
 
   void run_once_impl(std::stop_token stop_token) {
@@ -71,10 +120,12 @@ class udp_receiver_pipe : public pipe<udp_receiver_pipe> {
                                         std::abs(baseband_input_bits) /
                                         srtb::BITS_PER_BYTE;
     SRTB_LOGD << " [udp receiver pipe] "
+              << "id = " << id << ": "
               << "start receiving" << srtb::endl;
     auto [buffer_pointer, first_counter] =
         worker.receive(/* required_length = */ baseband_input_bytes);
     SRTB_LOGD << " [udp receiver pipe] "
+              << "id = " << id << ": "
               << "receive finished" << srtb::endl;
 
     auto time_before_push = std::chrono::system_clock::now();
@@ -118,9 +169,11 @@ class udp_receiver_pipe : public pipe<udp_receiver_pipe> {
     if (nsamps_reserved < baseband_input_count) {
       worker.consume(baseband_input_count - nsamps_reserved);
       SRTB_LOGD << " [udp receiver pipe] "
+                << "id = " << id << ": "
                 << "reserved " << nsamps_reserved << " samples" << srtb::endl;
     } else {
       SRTB_LOGW << " [udp receiver pipe] "
+                << "id = " << id << ": "
                 << "baseband_input_count = " << baseband_input_count
                 << " >= nsamps_reserved = " << nsamps_reserved << srtb::endl;
       worker.consume(baseband_input_count);

userspace/include/srtb/program_options.hpp
@@ -81,9 +81,9 @@ namespace program_options {
       ("udp_receiver_buffer_size", boost::program_options::value<std::string>(),
        "Buffer size of socket for receving udp packet.")
       ("udp_receiver_sender_address", boost::program_options::value<std::string>(),
-       "Address to receive baseband UDP packets")
+       "Address(es) to receive baseband UDP packets")
       ("udp_receiver_sender_port", boost::program_options::value<std::string>(),
-       "Port to receive baseband UDP packets")
+       "Port(s) to receive baseband UDP packets")
       ("udp_receiver_cpu_preferred", boost::program_options::value<std::string>(),
        "CPU core that UDP receiver should be bound to.")
     ;
@@ -239,9 +239,9 @@ inline void evaluate_and_apply_changed_config(const std::string& name,
   SRTB_PARSE(baseband_sample_rate)
   SRTB_PARSE(dm)
   SRTB_PARSE(udp_receiver_buffer_size)
-  SRTB_ASSIGN(udp_receiver_sender_address)
-  SRTB_PARSE(udp_receiver_sender_port)
-  SRTB_PARSE(udp_receiver_cpu_preferred)
+  SRTB_SPLIT_ASSIGN(udp_receiver_sender_address, /* delimiter = */ ",")
+  SRTB_SPLIT_PARSE(udp_receiver_sender_port, /* delimiter = */ ",")
+  SRTB_SPLIT_PARSE(udp_receiver_cpu_preferred, /* delimiter = */ ",")
   SRTB_ASSIGN(input_file_path)
   SRTB_PARSE(input_file_offset_bytes)
   SRTB_ASSIGN(baseband_output_file_prefix)

commit 7192e92d317d2b174233e60a771b7b61666ee9f0

[userspace] work_queue: use multi-producer queue if necessary
it's strange that starting multiple UDP receiver pipe with spsc queue
used didn't lead to crash...
maybe memory consistency on x86 is strong enough.

Fixes: e421449 ("[userspace] udp_receiver_pipe: start multiple receiver pipes")
Signed-off-by: fxzjshm <fxzjshm@163.com>

.gitmodules
@@ -4,3 +4,6 @@
 [submodule "userspace/3rdparty/SyclParallelSTL"]
 	path = userspace/3rdparty/SyclParallelSTL
 	url = https://github.com/fxzjshm/SyclParallelSTL
+[submodule "userspace/3rdparty/concurrentqueue"]
+	path = userspace/3rdparty/concurrentqueue
+	url = https://github.com/cameron314/concurrentqueue.git

userspace/CMakeLists.txt
@@ -139,6 +139,7 @@ include_directories(SYSTEM "3rdparty/")
 include_directories(SYSTEM "3rdparty/matplotlib-cpp/")
 include_directories(SYSTEM "3rdparty/SyclParallelSTL/include/")
 include_directories(SYSTEM "3rdparty/dsmath/")
+include_directories(SYSTEM "3rdparty/concurrentqueue/")
 ## Qt (optional)
 ### ref: https://doc.qt.io/qt-6/cmake-qt5-and-qt6-compatibility.html
 find_package(QT NAMES Qt6 Qt5 COMPONENTS Core)

userspace/include/srtb/global_variables.hpp
@@ -73,7 +73,8 @@ inline srtb::memory::cached_allocator<
 
 // work queues
 // TODO: global work queues seem ugly (no extensibility), better ideas ?
-inline srtb::work_queue<srtb::work::unpack_work> unpack_queue;
+inline srtb::work_queue<srtb::work::unpack_work, /* spsc = */ false>
+    unpack_queue;
 inline srtb::work_queue<srtb::work::fft_1d_r2c_work> fft_1d_r2c_queue;
 inline srtb::work_queue<srtb::work::rfi_mitigation_work> rfi_mitigation_queue;
 inline srtb::work_queue<srtb::work::dedisperse_work> dedisperse_queue;
@@ -88,9 +89,8 @@ inline srtb::work_queue<srtb::work::signal_detect_work> signal_detect_queue;
 // it also acts as a buffer, that is, only when signal_detect_pipe has result
 // can baseband_output_pipe write or drop it,
 // so capacity must be LARGE
-inline srtb::work_queue<srtb::work::baseband_output_work,
-                        /* fixed_size = */ false>
-    baseband_output_queue{1024 * srtb::work_queue_capacity};
+inline srtb::work_queue<srtb::work::baseband_output_work, /* spsc = */ false>
+    baseband_output_queue;
 inline srtb::work_queue<srtb::work::signal_detect_result>
     signal_detect_result_queue;
 

userspace/include/srtb/work.hpp
@@ -15,10 +15,11 @@
 #define __SRTB_WORK__
 
 #include <boost/lockfree/spsc_queue.hpp>
+#include <memory>
 #include <thread>
 #include <vector>
-#include <memory>
 
+#include "concurrentqueue.h"
 #include "srtb/config.hpp"
 
 #define SRTB_PUSH_WORK_OR_RETURN(tag, work_queue, work, stop_token)        \
@@ -62,23 +63,40 @@ namespace srtb {
 
 // definition of work queue, a container of works to be processed.
 // TODO: check should use spsc_queue or shared queue with mutex here
-template <typename T, bool fixed_size = srtb::work_queue_fixed_size,
+template <typename T, bool spsc = true,
+          bool fixed_size = srtb::work_queue_fixed_size,
           size_t capacity = srtb::work_queue_capacity>
 class work_queue;
 
 template <typename T, size_t capacity>
-class work_queue<T, true, capacity>
+class work_queue<T, true, true, capacity>
     : public boost::lockfree::spsc_queue<T,
                                          boost::lockfree::capacity<capacity> > {
 };
 
 template <typename T, size_t initial_capacity>
-class work_queue<T, false, initial_capacity>
+class work_queue<T, true, false, initial_capacity>
     : public boost::lockfree::spsc_queue<T> {
  public:
   using super_class = boost::lockfree::spsc_queue<T>;
   work_queue(size_t initial_capacity_ = initial_capacity)
       : super_class{initial_capacity_} {}
+};
+
+template <typename T, bool unused_1, size_t unused_2>
+class work_queue<T, false, unused_1, unused_2>
+    : public moodycamel::ConcurrentQueue<T> {
+ public:
+  using super_class = moodycamel::ConcurrentQueue<T>;
+  template <typename... Args>
+  inline decltype(auto) push(Args&&... args) {
+    return super_class::try_enqueue(std::forward<Args>(args)...);
+  }
+
+  template <typename... Args>
+  inline decltype(auto) pop(Args&&... args) {
+    return super_class::try_dequeue(std::forward<Args>(args)...);
+  }
 };
 
 /**

userspace/src/main.cpp
@@ -260,9 +260,8 @@ int main(int argc, char** argv) {
     return_value = srtb::gui::show_gui(argc, argv, std::move(threads));
   } else {
 #endif  // SRTB_ENABLE_GUI
-    // NOTE: read_available() here may not safe, check this
     while (!(srtb::pipeline::no_more_work &&
-             srtb::baseband_output_queue.read_available() == 0)) {
+             srtb::baseband_output_queue.size_approx() == 0)) {
       std::this_thread::sleep_for(
           std::chrono::nanoseconds(srtb::config.thread_query_work_wait_time));
     }