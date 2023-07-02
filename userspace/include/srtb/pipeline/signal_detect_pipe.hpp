fxzjshm/simple-radio-telescope-backend

commit 5cba62ee98baf7ff078ad204edd8670dc600fa38

[userspace] rfi_mitigation: optimize spectral kurtosis method
* moved SK method into separate function
* added normalization in pipeline so sum_real_t can be changed to `float`
* merged two kernels (`d_sk` was for debugging purpose)

userspace/include/srtb/pipeline/signal_detect_pipe.hpp
@@ -46,51 +46,9 @@ class signal_detect_pipe : public pipe<signal_detect_pipe> {
     const size_t count_per_batch = signal_detect_work.count;
     const size_t batch_size = signal_detect_work.batch_size;
 
-    // rfi mitigation based on spectral kurtosis
-    // ref: Antoni, 2004 (doi:10.1016/j.ymssp.2004.09.001)
-    //      Jiang, 2022
-    {
-      using sum_real_t = double;
-      // notice the difference of definition of spectral kurtosis (constant can be -1 or -2)
-      // here -1 is picked, and constants are moved into threshold to reduce computation on device
-      const size_t M = batch_size;
-      const srtb::real M_ = M;
-      const srtb::real threshold =
-          (srtb::config.mitigate_rfi_spectral_kurtosis_threshold + 1) *
-          ((M_ - 1) / (M_ + 1));
-      auto d_sk_shared =
-          srtb::device_allocator.allocate_unique<srtb::real>(count_per_batch);
-      // spectral kurtosis + 1
-      auto d_sk_ = d_sk_shared.get();
-      q.parallel_for(sycl::range<1>{count_per_batch}, [=](sycl::item<1> id) {
-         const size_t j = id.get_id(0);
-         sum_real_t s2 = 0, s4 = 0;
-         for (size_t i = 0; i < M; i++) {
-           const size_t index = i * count_per_batch + j;
-           SRTB_ASSERT_IN_KERNEL(index < count_per_batch * batch_size);
-           const srtb::complex<srtb::real> in = d_in[index];
-           const sum_real_t x2 = srtb::norm(in);
-           const sum_real_t x4 = x2 * x2;
-           s2 += x2;
-           s4 += x4;
-         }
-         // notice the comment of threshold above
-         const srtb::real sk_ = M * (s4 / (s2 * s2));
-         d_sk_[j] = sk_;
-       }).wait();
-      q.parallel_for(sycl::range<1>{count_per_batch}, [=](sycl::item<1> id) {
-         const size_t j = id.get_id(0);
-         constexpr auto zero = srtb::complex<srtb::real>{0, 0};
-         const srtb::real sk_ = d_sk_[j];
-         if (sk_ > threshold) {
-           for (size_t i = 0; i < M; i++) {
-             const size_t index = i * count_per_batch + j;
-             SRTB_ASSERT_IN_KERNEL(index < count_per_batch * batch_size);
-             d_in[index] = zero;
-           }
-         }
-       }).wait();
-    }
+    srtb::spectrum::mitigate_rfi_spectural_kurtosis_method(
+        d_in, count_per_batch, batch_size,
+        srtb::config.mitigate_rfi_spectral_kurtosis_threshold, q);
 
     size_t zero_count = 0;
     // count masked channels

userspace/include/srtb/spectrum/rfi_mitigation.hpp
@@ -31,7 +31,7 @@ namespace spectrum {
  * @brief Mitigate radio frequency interference (RFI),
  *        in this method RFI is determined by intensity of single frequency 
  *        compared to average intensity, so better for long time scale RFI
- * @note "spectural kurtosis" method in signal_detect_pipe
+ * @note not used because normalization fused in, see @c mitigate_rfi_pipe
  * TODO: compute norm twice or once with temporary buffer for it ?
  */
 template <typename T = srtb::real, typename C = srtb::complex<srtb::real>,
@@ -154,6 +154,60 @@ inline void mitigate_rfi_manual(DeviceComplexInputAccessor d_in,
   for (auto iter = events.rbegin(); iter != events.rend(); iter++) {
     (*iter).wait();
   }
+}
+
+// ---------------- rfi mitigation using spectrum kutorsis ----------------
+
+/**
+ * @brief Mitigate radio frequency interference (RFI), using spectural kurtosis
+ * 
+ *        frequency
+ *       ---------->
+ *     1111......1111 |
+ *     1111......1111 | time
+ *          ....      |
+ *     1111......1111 v
+ * 
+ * ->  xxxx......xxxx
+ * 
+ * ref: Antoni, 2004 (doi:10.1016/j.ymssp.2004.09.001)
+ *      Jiang, 2022
+ */
+template <typename T = srtb::real, typename C = srtb::complex<srtb::real>,
+          typename DeviceComplexInputAccessor = C*>
+inline void mitigate_rfi_spectural_kurtosis_method(
+    DeviceComplexInputAccessor d_in, size_t fft_bins, size_t time_counts,
+    T sk_threshold, sycl::queue& q = srtb::queue) {
+  // sometimes float is not enough
+  using sum_real_t = srtb::real;
+  // notice the difference of definition of spectral kurtosis (constant can be -1 or -2)
+  // here -1 is picked, and constants are moved into threshold to reduce computation on device
+  const size_t M = time_counts;
+  const srtb::real M_ = M;
+  const srtb::real threshold_ = (sk_threshold + 1) * ((M_ - 1) / (M_ + 1));
+  q.parallel_for(sycl::range<1>{fft_bins}, [=](sycl::item<1> id) {
+     const size_t j = id.get_id(0);
+     sum_real_t s2 = 0, s4 = 0;
+     for (size_t i = 0; i < M; i++) {
+       const size_t index = i * fft_bins + j;
+       SRTB_ASSERT_IN_KERNEL(index < fft_bins * time_counts);
+       const srtb::complex<srtb::real> in = d_in[index];
+       const sum_real_t x2 = srtb::norm(in);
+       const sum_real_t x4 = x2 * x2;
+       s2 += x2;
+       s4 += x4;
+     }
+     // notice the comment of threshold above
+     const srtb::real sk_ = M * (s4 / (s2 * s2));
+     constexpr auto zero = srtb::complex<srtb::real>{0, 0};
+     if (sk_ > threshold_) {
+       for (size_t i = 0; i < M; i++) {
+         const size_t index = i * fft_bins + j;
+         SRTB_ASSERT_IN_KERNEL(index < fft_bins * time_counts);
+         d_in[index] = zero;
+       }
+     }
+   }).wait();
 }
 
 }  // namespace spectrum

userspace/include/srtb/pipeline/rfi_mitigation_pipe.hpp
@@ -24,11 +24,18 @@
 namespace srtb {
 namespace pipeline {
 
+/**
+ * @brief this pipe mitigates radio frequency interferences (RFI)
+ *        using average intensity of frequency channels and manually set ranges.
+ * @note another RFI mitigation using spectral kurtosis is in signal_detect_pipe
+ */
 class rfi_mitigation_pipe : public pipe<rfi_mitigation_pipe> {
   friend pipe<rfi_mitigation_pipe>;
 
  protected:
+  /** @brief frequency ranges that is manuallly set in config by user */
   std::string mitigate_rfi_freq_list;
+  /** @brief frequency tanges parsed from the string above */
   std::vector<srtb::spectrum::rfi_range_type> rfi_ranges;
 
  protected:
@@ -42,19 +49,46 @@ class rfi_mitigation_pipe : public pipe<rfi_mitigation_pipe> {
     auto d_in = d_in_shared.get();
     const size_t in_count = rfi_mitigation_work.count;
 
-    const srtb::real threshold =
-        srtb::config.mitigate_rfi_average_method_threshold;
-    srtb::spectrum::mitigate_rfi_average_method(d_in, in_count, threshold, q);
-
-    if (srtb::config.mitigate_rfi_freq_list != mitigate_rfi_freq_list)
-        [[unlikely]] {
-      mitigate_rfi_freq_list = srtb::config.mitigate_rfi_freq_list;
-      rfi_ranges = srtb::spectrum::eval_rfi_ranges(mitigate_rfi_freq_list);
+    // mitigate using average method & normalize
+    {
+      const srtb::real threshold =
+          srtb::config.mitigate_rfi_average_method_threshold;
+      auto d_norm_avg_shared = srtb::algorithm::map_average(
+          d_in, in_count,
+          /* map = */
+          []([[maybe_unused]] size_t pos, const srtb::complex<srtb::real> c) {
+            return srtb::norm(c);
+          },
+          q);
+      auto d_norm_avg = d_norm_avg_shared.get();
+      q.parallel_for(sycl::range<1>{in_count}, [=](sycl::item<1> id) {
+         const size_t i = id.get_id(0);
+         const srtb::real norm_avg = (*d_norm_avg);
+         const auto in = d_in[i];
+         const srtb::real val = srtb::norm(in);
+         constexpr auto zero = srtb::complex<srtb::real>{0, 0};
+         if (val > threshold * norm_avg) {
+           // zap this channel
+           d_in[i] = zero;
+         } else {
+           // normalize
+           d_in[i] = in / static_cast<srtb::real>(in_count);
+         }
+       }).wait();
     }
-    const auto baseband_freq_low = srtb::config.baseband_freq_low;
-    const auto baseband_bandwidth = srtb::config.baseband_bandwidth;
-    srtb::spectrum::mitigate_rfi_manual(d_in, in_count, baseband_freq_low,
-                                        baseband_bandwidth, rfi_ranges, q);
+
+    // manual zap channel
+    {
+      if (srtb::config.mitigate_rfi_freq_list != mitigate_rfi_freq_list)
+          [[unlikely]] {
+        mitigate_rfi_freq_list = srtb::config.mitigate_rfi_freq_list;
+        rfi_ranges = srtb::spectrum::eval_rfi_ranges(mitigate_rfi_freq_list);
+      }
+      const auto baseband_freq_low = srtb::config.baseband_freq_low;
+      const auto baseband_bandwidth = srtb::config.baseband_bandwidth;
+      srtb::spectrum::mitigate_rfi_manual(d_in, in_count, baseband_freq_low,
+                                          baseband_bandwidth, rfi_ranges, q);
+    }
 
     // shortcut
     //srtb::work::simplify_spectrum_work simplify_spectrum_work;

commit 0aeb82b4ccaaa66609199d880f024738d202d283

[userspace] rfi_mitigation: correct spelling of spectral kurtosis method
........................

Found while writing presentation.

Fixes: 5cba62e ("[userspace] rfi_mitigation: optimize spectral kurtosis method")
Fixes: 51034ae ("[userspace] rfi_mitigation: fix spectural kurtosis threshold")
Fixes: 7173df7 ("[userspace] mitigate-rfi: try fixing conflict of manual channel zapping & spectural kurtosis method")
Signed-off-by: fxzjshm <fxzjshm@163.com>

userspace/include/srtb/pipeline/signal_detect_pipe.hpp
@@ -52,12 +52,12 @@ class signal_detect_pipe : public pipe<signal_detect_pipe> {
     const size_t count_per_batch = signal_detect_work.count;
     const size_t batch_size = signal_detect_work.batch_size;
 
-    srtb::spectrum::mitigate_rfi_spectural_kurtosis_method(
+    srtb::spectrum::mitigate_rfi_spectral_kurtosis_method(
         d_in, count_per_batch, batch_size,
         srtb::config.mitigate_rfi_spectral_kurtosis_threshold, q);
 
     SRTB_LOGD << " [signal_detect_pipe] "
-              << "mitigate_rfi_spectural_kurtosis_method finished"
+              << "mitigate_rfi_spectral_kurtosis_method finished"
               << srtb::endl;
 
     size_t zero_count = 0;

userspace/include/srtb/spectrum/rfi_mitigation.hpp
@@ -89,7 +89,6 @@ inline auto eval_rfi_ranges(const std::string& mitigate_rfi_freq_list) {
  * @brief Mitigate radio frequency interference (RFI),
  *        in this method RFI is determined manually
  * @param rfi_ranges frequency range of RFI, set manually
- * TODO: use "spectural kurtosis"
  */
 template <typename T = srtb::real, typename C = srtb::complex<srtb::real>,
           typename DeviceComplexInputAccessor = C*>
@@ -160,7 +159,7 @@ inline void mitigate_rfi_manual(DeviceComplexInputAccessor d_in,
 // ---------------- rfi mitigation using spectrum kutorsis ----------------
 
 /**
- * @brief Mitigate radio frequency interference (RFI), using spectural kurtosis
+ * @brief Mitigate radio frequency interference (RFI), using spectral kurtosis
  * 
  *        frequency
  *       ---------->
@@ -181,7 +180,7 @@ inline void mitigate_rfi_manual(DeviceComplexInputAccessor d_in,
  */
 template <typename T = srtb::real, typename C = srtb::complex<srtb::real>,
           typename DeviceComplexInputAccessor = C*, bool normalization = false>
-inline void mitigate_rfi_spectural_kurtosis_method(
+inline void mitigate_rfi_spectral_kurtosis_method(
     DeviceComplexInputAccessor d_in, size_t fft_bins, size_t time_counts,
     T sk_threshold, sycl::queue& q = srtb::queue) {
   // sometimes float is not enough, change to double if so