fxzjshm/simple-radio-telescope-backend

commit 163eff0f0dcb33c0f1633355b4b328964ce63d35

[userspace] pipeline: add fft pipe for spectrum
`watfft_1d_c2c` is to replace `ifft_1d_c2c` & `refft_1d_c2c`,
however output matrix is transposed, so `signal_detect` and
`SpectrumImageProvider` (TODO) should be adjusted accordingly.

Signed-off-by: fxzjshm <fxzjshm@163.com>

userspace/include/srtb/pipeline/fft_pipe.hpp
@@ -302,11 +302,115 @@ class refft_1d_c2c_pipe : public pipe<refft_1d_c2c_pipe> {
     signal_detect_work.ptr = d_out_shared;
     signal_detect_work.count = refft_length;
     signal_detect_work.batch_size = refft_batch_size;
-    signal_detect_work.baseband_data = std::move(refft_1d_c2c_work.baseband_data);
+    signal_detect_work.baseband_data =
+        std::move(refft_1d_c2c_work.baseband_data);
     signal_detect_work.timestamp = refft_1d_c2c_work.timestamp;
-    signal_detect_work.udp_packet_counter = refft_1d_c2c_work.udp_packet_counter;
+    signal_detect_work.udp_packet_counter =
+        refft_1d_c2c_work.udp_packet_counter;
     SRTB_PUSH_WORK_OR_RETURN(" [refft 1d c2c pipe] ", srtb::signal_detect_queue,
                              signal_detect_work, stop_token);
+  }
+};
+
+/**
+ * @brief This pipe generates time-frequency spectrum from dedispersed spectrum.
+ * May used to substitute ifft_pipe & refft_pipe, although a transpose may required.
+ * @deprecated Not used now because dedispersed baseband may used for other purposes in the future.
+ */
+class watfft_1d_c2c_pipe : public pipe<watfft_1d_c2c_pipe> {
+  friend pipe<watfft_1d_c2c_pipe>;
+
+ protected:
+  std::optional<srtb::fft::fft_1d_dispatcher<srtb::fft::type::C2C_1D_BACKWARD> >
+      opt_watfft_dispatcher;
+  std::optional<srtb::fft::fft_window_functor_manager<
+      srtb::real, srtb::fft::default_window> >
+      opt_watfft_window_functor_manager;
+
+ public:
+  watfft_1d_c2c_pipe() = default;
+
+ protected:
+  void setup_impl([[maybe_unused]] std::stop_token stop_token) {
+    // divided by 2 because baseband input is real number, but here is complex
+    const size_t baseband_input_count_complex =
+        srtb::config.baseband_input_count / 2;
+    size_t watfft_batch_size = srtb::config.spectrum_channel_count;
+    size_t watfft_length = baseband_input_count_complex / watfft_batch_size;
+    if (watfft_length == 0) [[unlikely]] {
+      SRTB_LOGW << " [watfft 1d c2c pipe] "
+                << "watfft_batch_size too large! Set to input length now.";
+      watfft_batch_size = baseband_input_count_complex;
+      watfft_length = 1;
+    }
+    opt_watfft_dispatcher.emplace(/* n = */ watfft_length,
+                                  /* batch_size = */ watfft_batch_size, q);
+    opt_watfft_window_functor_manager.emplace(srtb::fft::default_window{},
+                                              /* n = */ watfft_length, q);
+  }
+
+  void run_once_impl(std::stop_token stop_token) {
+    srtb::work::ifft_1d_c2c_work ifft_1d_c2c_work;
+    SRTB_POP_WORK_OR_RETURN(" [watfft 1d c2c pipe] ", srtb::ifft_1d_c2c_queue,
+                            ifft_1d_c2c_work, stop_token);
+    const size_t input_count = ifft_1d_c2c_work.count;
+
+    auto& watfft_dispatcher = opt_watfft_dispatcher.value();
+    const size_t watfft_batch_size =
+        std::min(srtb::config.spectrum_channel_count, input_count);
+    const size_t watfft_length = input_count / watfft_batch_size;
+
+    // reset FFT plan if mismatch
+    if (watfft_dispatcher.get_n() != watfft_length ||
+        watfft_dispatcher.get_batch_size() != watfft_batch_size) [[unlikely]] {
+      watfft_dispatcher.set_size(watfft_length, watfft_batch_size);
+      opt_watfft_window_functor_manager.emplace(srtb::fft::default_window{},
+                                                /* n = */ watfft_length, q);
+    }
+
+    auto& d_in_shared = ifft_1d_c2c_work.ptr;
+    auto d_in = d_in_shared.get();
+
+    std::shared_ptr<srtb::complex<srtb::real> > d_out_shared;
+    if constexpr (srtb::fft_operate_in_place) {
+      d_out_shared = d_in_shared;
+    } else {
+      d_out_shared =
+          srtb::device_allocator.allocate_shared<srtb::complex<srtb::real> >(
+              input_count);
+    }
+
+    auto d_out = d_out_shared.get();
+    watfft_dispatcher.process(d_in, d_out);
+
+    // de-apply window function of size watfft_length
+    if constexpr (!std::is_same_v<srtb::fft::default_window,
+                                  srtb::fft::window::rectangle<srtb::real> >) {
+      auto watfft_window =
+          opt_watfft_window_functor_manager.value().get_coefficients();
+      q.parallel_for(sycl::range<1>{input_count}, [=](sycl::item<1> id) {
+         const auto i = id.get_id(0);
+         const auto channel_id = i / watfft_length;
+         const auto j = i - (channel_id * watfft_length);
+         SRTB_ASSERT_IN_KERNEL(j < watfft_length);
+         const auto x = d_in[i];
+         d_in[i] = x / watfft_window[j];
+       }).wait();
+    }
+
+    // TODO: nsamps_reserved
+    //const auto nsamps_reserved_real =
+    //    srtb::coherent_dedispersion::nsamps_reserved();
+    //const auto nsamps_reserved_complex = nsamps_reserved_real / 2;
+
+    srtb::work::signal_detect_work signal_detect_work;
+    signal_detect_work.ptr = d_out_shared;
+    signal_detect_work.count = watfft_length;
+    signal_detect_work.batch_size = watfft_batch_size;
+    signal_detect_work.timestamp = ifft_1d_c2c_work.timestamp;
+    SRTB_PUSH_WORK_OR_RETURN(" [watfft 1d c2c pipe] ",
+                             srtb::signal_detect_queue, signal_detect_work,
+                             stop_token);
   }
 };
 

userspace/include/srtb/pipeline/signal_detect_pipe.hpp
@@ -14,6 +14,10 @@
 #ifndef __SRTB_PIPELINE_SIGNAL_DETECT_PIPE__
 #define __SRTB_PIPELINE_SIGNAL_DETECT_PIPE__
 
+#include <boost/iterator/counting_iterator.hpp>
+#include <boost/iterator/permutation_iterator.hpp>
+#include <boost/iterator/transform_iterator.hpp>
+
 #include "sycl/execution_policy"
 // --- divide line for clang-format
 #include "srtb/commons.hpp"
@@ -32,6 +36,17 @@ namespace pipeline {
  *        and detect if there's signal in it.
  *        If yes, notify someone to write the original baseband data;
  *        otherwise, just drop it.
+ * @note In this variant, @code count is @code spectrum_channel_count and
+ *       @code batch_size is count of time series
+ *        frequency
+ *       ---------->
+ *     1111......1111 |
+ *     1111......1111 | time
+ *          ....      |
+ *     1111......1111 v
+ * 
+ * ->  xxxx......xxxx
+ * 
  * TODO: separate this into 2 pipes ? 
  *       (one for sum into time series, one for actual signal detect)
  */
@@ -57,8 +72,7 @@ class signal_detect_pipe : public pipe<signal_detect_pipe> {
         srtb::config.mitigate_rfi_spectral_kurtosis_threshold, q);
 
     SRTB_LOGD << " [signal_detect_pipe] "
-              << "mitigate_rfi_spectral_kurtosis_method finished"
-              << srtb::endl;
+              << "mitigate_rfi_spectral_kurtosis_method finished" << srtb::endl;
 
     size_t zero_count = 0;
     // count masked channels
@@ -124,7 +138,7 @@ class signal_detect_pipe : public pipe<signal_detect_pipe> {
     // time series is now available
 
     srtb::work::baseband_output_work baseband_output_work;
-    baseband_output_work.ptr = nullptr;  // TODO
+    baseband_output_work.ptr = nullptr;
     baseband_output_work.count = 0;
     baseband_output_work.batch_size = 0;
     baseband_output_work.baseband_data =
@@ -234,6 +248,249 @@ class signal_detect_pipe : public pipe<signal_detect_pipe> {
                 << "no signal detected" << srtb::endl;
     }
     SRTB_PUSH_WORK_OR_RETURN(" [signal_detect_pipe] ",
+                             srtb::baseband_output_queue, baseband_output_work,
+                             stop_token);
+  }
+};
+
+/**
+ * @brief this pipe reads from refft-ed spectrum, sum it into time series,
+ *        and detect if there's signal in it.
+ *        If yes, notify someone to write the original baseband data;
+ *        otherwise, just drop it.
+ * @note In this variant, @code count is @code count of time series, and
+ *       @code batch_size is spectrum_channel_count
+ *          time
+ *       ---------->
+ *     1111......1111 |                x
+ *     1111......1111 | frequency  ->  x
+ *          ....      |                .
+ *     1111......1111 v                x
+ */
+class signal_detect_pipe_2 : public pipe<signal_detect_pipe_2> {
+  friend pipe<signal_detect_pipe_2>;
+
+ protected:
+  sycl::sycl_execution_policy<> execution_policy{q};
+
+ protected:
+  void run_once_impl(std::stop_token stop_token) {
+    srtb::work::signal_detect_work signal_detect_work;
+    SRTB_POP_WORK_OR_RETURN(" [signal_detect_pipe_2] ",
+                            srtb::signal_detect_queue, signal_detect_work,
+                            stop_token);
+
+    auto& d_in_shared = signal_detect_work.ptr;
+    auto d_in = d_in_shared.get();
+    const size_t time_sample_count = signal_detect_work.count;
+    const size_t frequency_bin_count = signal_detect_work.batch_size;
+
+    srtb::spectrum::mitigate_rfi_spectral_kurtosis_method_2(
+        d_in, time_sample_count, frequency_bin_count,
+        srtb::config.mitigate_rfi_spectral_kurtosis_threshold, q);
+
+    SRTB_LOGD << " [signal_detect_pipe_2] "
+              << "mitigate_rfi_spectral_kurtosis_method_2 finished"
+              << srtb::endl;
+
+    size_t zero_count = 0;
+    // count masked channels
+    {
+      auto d_zero_count_shared = srtb::algorithm::map_reduce(
+          boost::permutation_iterator{
+              d_in,
+              boost::transform_iterator{
+                  boost::counting_iterator<size_t>{0},
+                  [=](size_t i) { return i * time_sample_count; }}},
+          frequency_bin_count, /* map = */
+          []([[maybe_unused]] const size_t pos,
+             const srtb::complex<srtb::real> x) {
+            if (srtb::norm(x) == 0) {
+              return size_t{1};
+            } else {
+              return size_t{0};
+            }
+          },
+          sycl::plus<size_t>(), q);
+      auto d_zero_count = d_zero_count_shared.get();
+      q.copy(d_zero_count, &zero_count, 1).wait();
+      SRTB_LOGD << " [signal_detect_pipe_2] "
+                 << "zero_count = " << zero_count << srtb::endl;
+    }
+
+    if (SRTB_ENABLE_GUI && srtb::config.gui_enable) {
+      // temporary work: spectrum analyzer
+      srtb::work::simplify_spectrum_work simplify_spectrum_work;
+      simplify_spectrum_work.ptr = d_in_shared;
+      simplify_spectrum_work.count = time_sample_count;
+      simplify_spectrum_work.batch_size = frequency_bin_count;
+      simplify_spectrum_work.timestamp = signal_detect_work.timestamp;
+      // just try once, in case GUI is stuck (e.g. when using X forwarding on SSH)
+      srtb::simplify_spectrum_queue.push(simplify_spectrum_work);
+    }
+
+    // time series
+    // Note: time_series_count <= time_sample_count, as some time samples are reserved for next segment
+    size_t time_series_count;
+    const size_t time_reserved_count =
+        srtb::codd::nsamps_reserved() / frequency_bin_count;
+    if (time_sample_count <= time_reserved_count) [[unlikely]] {
+      SRTB_LOGW << " [signal_detect_pipe_2] "
+                << "time_sample_count = " << time_sample_count
+                << " <= time_reserved_count = " << time_reserved_count
+                << srtb::endl;
+      time_series_count = time_sample_count;
+    } else {
+      time_series_count = time_sample_count - time_reserved_count;
+    }
+    auto d_time_series_shared =
+        srtb::device_allocator.allocate_shared<srtb::real>(time_series_count);
+    auto d_time_series = d_time_series_shared.get();
+
+    // sum each row
+    q.parallel_for(sycl::range<1>{time_series_count}, [=](sycl::item<1> id) {
+       const size_t j = id.get_id(0);
+       srtb::real intensity_sum = 0;
+       for (size_t i = 0; i < frequency_bin_count; i++) {
+         // should be very careful about index here
+         const size_t index = i * time_sample_count + j;
+         SRTB_ASSERT_IN_KERNEL(index < time_sample_count * frequency_bin_count);
+         const auto in = d_in[index];
+         intensity_sum += srtb::norm(in);
+       }
+       d_time_series[j] = intensity_sum;
+     }).wait();
+
+    const srtb::real snr_threshold =
+        srtb::config.signal_detect_signal_noise_threshold;
+
+    // remove baseline -- substract average
+    // this is done first to avoid big float - big float when calculating variance
+    // TODO: does baseline changes a lot in this time scale ? Is a linear approximation needed ?
+    {
+      auto d_average_shared = srtb::algorithm::map_average(
+          d_time_series, time_series_count, srtb::algorithm::map_identity(), q);
+      auto d_average = d_average_shared.get();
+      q.parallel_for(sycl::range<1>{time_series_count},
+                     [=](sycl::item<1> item) {
+                       const auto i = item.get_id(0);
+                       d_time_series[i] -= (*d_average);
+                     })
+          .wait();
+    }
+    // time series is now available
+
+    srtb::work::baseband_output_work baseband_output_work;
+    baseband_output_work.ptr = nullptr;
+    baseband_output_work.count = 0;
+    baseband_output_work.batch_size = 0;
+    baseband_output_work.baseband_data =
+        std::move(signal_detect_work.baseband_data);
+    baseband_output_work.timestamp = signal_detect_work.timestamp;
+    baseband_output_work.udp_packet_counter =
+        signal_detect_work.udp_packet_counter;
+
+    // if too many frequency channels are masked, result is often inaccurate
+    if (zero_count <
+        srtb::config.signal_detect_channel_threshold * time_sample_count) {
+      // trivial signal detect on raw time series
+      {
+        const size_t h_signal_count = srtb::signal_detect::count_signal(
+            d_time_series, time_series_count, snr_threshold, q);
+        const bool has_signal = (h_signal_count > 0);
+        if (has_signal) /* [[unlikely]] */ {
+          // copy from device to host
+          auto h_out_shared = srtb::host_allocator.allocate_shared<srtb::real>(
+              time_series_count);
+          auto h_out = h_out_shared.get();
+          sycl::event event =
+              q.copy(d_time_series, /* -> */ h_out, time_series_count);
+          srtb::work::time_series_holder time_series_holder{
+              .h_time_series = h_out_shared,
+              .time_series_length = time_series_count,
+              .boxcar_length = 1,
+              .transfer_event = event};
+          baseband_output_work.time_series.push_back(time_series_holder);
+        }
+      }
+
+      // boxcar method
+      // ref: heimdall
+      {
+        auto d_accumulated_unique_ptr =
+            srtb::device_allocator.allocate_unique<srtb::real>(
+                time_series_count);
+        auto d_accumulated = d_accumulated_unique_ptr.get();
+        sycl::impl::inclusive_scan(execution_policy, d_time_series,
+                                   d_time_series + time_series_count,
+                                   d_accumulated, srtb::real{0}, std::plus());
+        // TODO: reuse d_time_series ?
+        // this is reused for different boxcar_length
+        auto d_boxcared_time_series_unique_ptr =
+            srtb::device_allocator.allocate_unique<srtb::real>(
+                time_series_count);
+        auto d_boxcared_time_series = d_boxcared_time_series_unique_ptr.get();
+
+        const size_t max_boxcar_length =
+            srtb::config.signal_detect_max_boxcar_length;
+        // TODO: async submit kernel ?
+        for (size_t boxcar_length = 2; (boxcar_length <= max_boxcar_length &&
+                                        boxcar_length < time_series_count);
+             boxcar_length *= 2) {
+          // compute boxcared time series
+          const size_t boxcared_time_series_count =
+              time_series_count - boxcar_length;
+          q.parallel_for(sycl::range{boxcared_time_series_count},
+                         [=](sycl::item<1> id) {
+                           const size_t i = id.get_id(0);
+                           d_boxcared_time_series[i] =
+                               d_accumulated[i + boxcar_length] -
+                               d_accumulated[i];
+                         })
+              .wait();
+
+          // trivially detect signal, again
+          const size_t h_signal_count = srtb::signal_detect::count_signal(
+              d_boxcared_time_series, boxcared_time_series_count, snr_threshold,
+              q);
+          const bool has_signal = (h_signal_count > 0);
+          if (has_signal) /* [[unlikely]] */ {
+            // copy from device to host
+            auto h_out_shared =
+                srtb::host_allocator.allocate_shared<srtb::real>(
+                    time_series_count);
+            auto h_out = h_out_shared.get();
+            sycl::event event = q.copy(d_boxcared_time_series, /* -> */ h_out,
+                                       boxcared_time_series_count);
+            srtb::work::time_series_holder time_series_holder{
+                .h_time_series = h_out_shared,
+                .time_series_length = boxcared_time_series_count,
+                .boxcar_length = boxcar_length,
+                .transfer_event = event};
+            baseband_output_work.time_series.push_back(time_series_holder);
+          }
+        }
+      }
+    } else {
+      // baseband_output_work.has_signal = false;
+      // currently represented as time_series.size() == 0
+      // that is, do nothing
+    }
+
+    const bool has_signal = (baseband_output_work.time_series.size() > 0);
+    if (has_signal) {
+      baseband_output_work.ptr = std::move(d_in_shared);
+      baseband_output_work.count = signal_detect_work.count;
+      baseband_output_work.batch_size = signal_detect_work.batch_size;
+      SRTB_LOGI << " [signal_detect_pipe_2] "
+                << " signal detected in "
+                << baseband_output_work.time_series.size() << " time series"
+                << srtb::endl;
+    } else {
+      SRTB_LOGD << " [signal_detect_pipe_2] "
+                << "no signal detected" << srtb::endl;
+    }
+    SRTB_PUSH_WORK_OR_RETURN(" [signal_detect_pipe_2] ",
                              srtb::baseband_output_queue, baseband_output_work,
                              stop_token);
   }

userspace/include/srtb/spectrum/rfi_mitigation.hpp
@@ -22,6 +22,7 @@
 // --- divide line for clang-format ---
 #include "srtb/algorithm/map_identity.hpp"
 #include "srtb/algorithm/map_reduce.hpp"
+#include "srtb/algorithm/multi_reduce.hpp"
 
 namespace srtb {
 namespace spectrum {
@@ -170,8 +171,7 @@ inline void mitigate_rfi_manual(DeviceComplexInputAccessor d_in,
  * 
  * ->  xxxx......xxxx
  * 
- * ref: Antoni, 2004 (doi:10.1016/j.ymssp.2004.09.001)
- *      Jiang, 2022
+ * ref: Vrabie, Granjon, and Serviere (2003), Nita (2016), Taylor et al. (2018), and Jiang (2022)
  * 
  * @note current implementation is not efficient if fft_bins is small
  * 
@@ -184,7 +184,7 @@ inline void mitigate_rfi_spectral_kurtosis_method(
     DeviceComplexInputAccessor d_in, size_t fft_bins, size_t time_counts,
     T sk_threshold, sycl::queue& q = srtb::queue) {
   // sometimes float is not enough, change to double if so
-  using sum_real_t = srtb::real;
+  using sum_real_t = T;
   // notice the difference of definition of spectral kurtosis (constant can be -1 or -2)
   // here -1 is picked, and constants are moved into threshold to reduce computation on device
   const size_t M = time_counts;
@@ -270,6 +270,71 @@ inline void mitigate_rfi_spectral_kurtosis_method(
        if (zeroing) {
          d_in[i] = zero;
        }
+     }
+   }).wait();
+}
+
+/**
+ * @brief Mitigate radio frequency interference (RFI), using spectral kurtosis.
+ * 
+ * @note notice different interpretations of axis:
+ *          time
+ *       ---------->
+ *     1111......1111 |                x
+ *     1111......1111 | frequency  ->  x
+ *          ....      |                .
+ *     1111......1111 v                x
+ * 
+ * ref: Vrabie, Granjon, and Serviere (2003), Nita (2016), Taylor et al. (2018), and Jiang (2022)
+ */
+template <typename T = srtb::real, typename C = srtb::complex<srtb::real>,
+          typename DeviceComplexInputAccessor = C*>
+inline void mitigate_rfi_spectral_kurtosis_method_2(
+    DeviceComplexInputAccessor d_in, size_t time_counts, size_t fft_bins,
+    T sk_threshold, sycl::queue& q) {
+  // notice the difference of definition of spectral kurtosis (constant can be -1 or -2)
+  // here -1 is picked, and constants are moved into threshold to reduce computation on device
+  const size_t M = time_counts;
+  const size_t total_size = fft_bins * time_counts;
+  const T M_ = M;
+  T threshold_high = sk_threshold;
+  T threshold_low = 2 - sk_threshold;
+  if (threshold_low > threshold_high) {
+    std::swap(threshold_low, threshold_high);
+  }
+  const T threshold_low_ = threshold_low * ((M_ - 1) / (M_ + 1)) + 1;
+  const T threshold_high_ = threshold_high * ((M_ - 1) / (M_ + 1)) + 1;
+
+  // <x^2, x^4>
+  using power_pair = std::pair<T, T>;
+  auto d_power_pair_unique =
+      srtb::device_allocator.allocate_unique<power_pair>(fft_bins);
+  auto d_power_pair = d_power_pair_unique.get();
+
+  // sum all x^2, x^4
+  srtb::algorithm::multi_mapreduce(
+      d_in, time_counts, fft_bins, d_power_pair,
+      /* map = */
+      []([[maybe_unused]] auto pos, C x) {
+        T x2 = srtb::norm(x), x4 = x2 * x2;
+        return power_pair{x2, x4};
+      },
+      /* reduce = */
+      [](power_pair a, power_pair b) {
+        return power_pair{a.first + b.first, a.second + b.second};
+      },
+      q);
+
+  q.parallel_for(sycl::range<1>(total_size), [=](sycl::item<1> id) {
+     const size_t i = id.get_id(0);
+     const size_t j = (i / time_counts);  // index on frequency axis
+     //const size_t k = i - (j * time_counts);  // index on time axis
+     const auto [s2, s4] = d_power_pair[j];
+     const T sk_ = M * (s4 / (s2 * s2));
+     const bool zeroing = (sk_ > threshold_high_ || sk_ < threshold_low_);
+     constexpr auto zero = C{T{0}, T{0}};
+     if (zeroing) {
+       d_in[i] = zero;
      }
    }).wait();
 }

userspace/src/main.cpp
@@ -220,8 +220,13 @@ int main(int argc, char** argv) {
 
   std::jthread refft_1d_c2c_thread = srtb::pipeline::refft_1d_c2c_pipe::start();
 
+  //std::jthread watfft_1d_c2c_thread = srtb::pipeline::watfft_1d_c2c_pipe::start();
+
   std::jthread signal_detect_thread =
       srtb::pipeline::signal_detect_pipe::start();
+  
+  //std::jthread signal_detect_thread =
+  //    srtb::pipeline::signal_detect_pipe_2::start();
 
   std::jthread baseband_output_thread;
   if (srtb::config.baseband_write_all) {
@@ -250,6 +255,7 @@ int main(int argc, char** argv) {
   threads.push_back(std::move(dedisperse_thread));
   threads.push_back(std::move(ifft_1d_c2c_thread));
   threads.push_back(std::move(refft_1d_c2c_thread));
+  //threads.push_back(std::move(watfft_1d_c2c_thread));
   threads.push_back(std::move(signal_detect_thread));
   threads.push_back(std::move(baseband_output_thread));
   threads.push_back(std::move(simplify_spectrum_thread));

commit 182abdeb1a2d226a3c8a8f37498eaf89f988c811

[userspace] pipeline: fix baseband data transfer
forget to copy this, then baseband cannot be written in output...

Fixes: 163eff0 ("[userspace] pipeline: add fft pipe for spectrum")

userspace/include/srtb/pipeline/fft_pipe.hpp
@@ -408,8 +408,9 @@ class watfft_1d_c2c_pipe : public pipe<watfft_1d_c2c_pipe> {
     signal_detect_work.count = watfft_length;
     signal_detect_work.batch_size = watfft_batch_size;
     signal_detect_work.timestamp = ifft_1d_c2c_work.timestamp;
-    signal_detect_work.udp_packet_counter =
-        ifft_1d_c2c_work.udp_packet_counter;
+    signal_detect_work.udp_packet_counter = ifft_1d_c2c_work.udp_packet_counter;
+    signal_detect_work.baseband_data =
+        std::move(ifft_1d_c2c_work.baseband_data);
     SRTB_PUSH_WORK_OR_RETURN(" [watfft 1d c2c pipe] ",
                              srtb::signal_detect_queue, signal_detect_work,
                              stop_token);