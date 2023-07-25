fxzjshm/simple-radio-telescope-backend

commit 43ce08b61ea926a8b9302b22b4973bdf4a5fea88

[userspace] work: split timestamp & UDP counter

userspace/include/srtb/pipeline/baseband_output_pipe.hpp
@@ -200,13 +200,17 @@ class baseband_output_pipe</* continuous_write = */ false>
 
     const bool has_signal = (signal_detect_result.time_series.size() > 0);
     if (has_signal) {
-      auto timestamp = baseband_output_work.timestamp;
+      auto file_counter = baseband_output_work.udp_packet_counter;
+      if (file_counter == baseband_output_work.no_udp_packet_counter) {
+        file_counter = baseband_output_work.timestamp;
+      }
       SRTB_LOGI << " [baseband_output_pipe] "
-                << "Begin writing baseband data, timestamp = " << timestamp
-                << srtb::endl;
+                << "Begin writing baseband data, file_counter = "
+                << file_counter << srtb::endl;
 
       const std::string file_name_no_extension =
-          srtb::config.baseband_output_file_prefix + std::to_string(timestamp);
+          srtb::config.baseband_output_file_prefix +
+          std::to_string(file_counter);
 
       boost::asio::post(
           baseband_output_thread_pool,
@@ -241,18 +245,18 @@ class baseband_output_pipe</* continuous_write = */ false>
               }
 #endif
               SRTB_LOGI << " [baseband_output_pipe] "
-                        << "Finished writing baseband data, timestamp = "
-                        << timestamp << srtb::endl;
+                        << "Finished writing baseband data, file_counter = "
+                        << file_counter << srtb::endl;
             } else [[unlikely]] {
               SRTB_LOGW << " [baseband_output_pipe] "
-                        << "Failed to write baseband data! timestamp = "
-                        << timestamp << srtb::endl;
+                        << "Failed to write baseband data! file_counter = "
+                        << file_counter << srtb::endl;
             }
           });
 
       boost::asio::post(
           time_series_plot_thread_pool,
-          [=, signal_detect_result = std::move(signal_detect_result)]() {
+          [=, this, signal_detect_result = std::move(signal_detect_result)]() {
             // iterate over all time series, assumed with signal
             for (auto time_series_holder : signal_detect_result.time_series) {
               const auto boxcar_length = time_series_holder.boxcar_length;
@@ -314,8 +318,8 @@ class baseband_output_pipe</* continuous_write = */ false>
 #endif
               } else [[unlikely]] {
                 SRTB_LOGW << " [baseband_output_pipe] "
-                          << "Failed to write baseband data! timestamp = "
-                          << timestamp << srtb::endl;
+                          << "Failed to write time series! file_counter = "
+                          << file_counter << srtb::endl;
               }
             }
           });
@@ -323,7 +327,7 @@ class baseband_output_pipe</* continuous_write = */ false>
     }  // if(has_signal)
 
     srtb::pipeline::notify();
-  }    // void run_once_inpl()
+  }  // void run_once_inpl()
 };
 
 }  // namespace pipeline

userspace/include/srtb/pipeline/dedisperse_pipe.hpp
@@ -63,6 +63,7 @@ class dedisperse_pipe : public pipe<dedisperse_pipe> {
     ifft_1d_c2c_work.ptr = d_in_shared;
     ifft_1d_c2c_work.count = N;
     ifft_1d_c2c_work.timestamp = work.timestamp;
+    ifft_1d_c2c_work.udp_packet_counter = work.udp_packet_counter;
     SRTB_PUSH_WORK_OR_RETURN(" [dedisperse pipe] ", srtb::ifft_1d_c2c_queue,
                              ifft_1d_c2c_work, stop_token);
   }

userspace/include/srtb/pipeline/fft_pipe.hpp
@@ -78,6 +78,7 @@ class fft_1d_r2c_pipe : public pipe<fft_1d_r2c_pipe> {
     rfi_mitigation_work.ptr = d_out_shared;
     rfi_mitigation_work.count = out_count;
     rfi_mitigation_work.timestamp = fft_1d_r2c_work.timestamp;
+    rfi_mitigation_work.udp_packet_counter = fft_1d_r2c_work.udp_packet_counter;
     SRTB_PUSH_WORK_OR_RETURN(" [fft 1d r2c pipe] ", srtb::rfi_mitigation_queue,
                              rfi_mitigation_work, stop_token);
   }
@@ -178,6 +179,7 @@ class ifft_1d_c2c_pipe : public pipe<ifft_1d_c2c_pipe> {
     refft_1d_c2c_work.ptr = d_out_shared;
     refft_1d_c2c_work.count = output_count;
     refft_1d_c2c_work.timestamp = ifft_1d_c2c_work.timestamp;
+    refft_1d_c2c_work.udp_packet_counter = ifft_1d_c2c_work.udp_packet_counter;
     SRTB_PUSH_WORK_OR_RETURN(" [ifft 1d c2c pipe] ", srtb::refft_1d_c2c_queue,
                              refft_1d_c2c_work, stop_token);
   }
@@ -296,6 +298,7 @@ class refft_1d_c2c_pipe : public pipe<refft_1d_c2c_pipe> {
     signal_detect_work.count = refft_length;
     signal_detect_work.batch_size = refft_batch_size;
     signal_detect_work.timestamp = refft_1d_c2c_work.timestamp;
+    signal_detect_work.udp_packet_counter = refft_1d_c2c_work.udp_packet_counter;
     SRTB_PUSH_WORK_OR_RETURN(" [refft 1d c2c pipe] ", srtb::signal_detect_queue,
                              signal_detect_work, stop_token);
   }

userspace/include/srtb/pipeline/read_file_pipe.hpp
@@ -77,13 +77,14 @@ class read_file_pipe : public pipe<read_file_pipe> {
       char* d_in = d_in_shared.get();
       q.copy(h_in, /* -> */ d_in, time_sample_bytes).wait();
 
-      uint64_t timestamp =
+      const uint64_t timestamp =
           std::chrono::system_clock::now().time_since_epoch().count();
       {
         srtb::work::unpack_work unpack_work;
         unpack_work.ptr = std::reinterpret_pointer_cast<std::byte>(d_in_shared);
         unpack_work.count = time_sample_bytes;
         unpack_work.timestamp = timestamp;
+        unpack_work.udp_packet_counter = unpack_work.no_udp_packet_counter;
         unpack_work.baseband_input_bits = baseband_input_bits;
         SRTB_PUSH_WORK_OR_RETURN(" [read_file] ", srtb::unpack_queue,
                                  unpack_work, stop_token);
@@ -95,6 +96,8 @@ class read_file_pipe : public pipe<read_file_pipe> {
             std::reinterpret_pointer_cast<std::byte>(h_in_shared);
         baseband_output_work.count = time_sample_bytes;
         baseband_output_work.timestamp = timestamp;
+        baseband_output_work.udp_packet_counter =
+            baseband_output_work.no_udp_packet_counter;
         SRTB_PUSH_WORK_OR_RETURN(" [read_file] ", srtb::baseband_output_queue,
                                  baseband_output_work, stop_token);
       }

userspace/include/srtb/pipeline/rfi_mitigation_pipe.hpp
@@ -96,6 +96,7 @@ class rfi_mitigation_pipe : public pipe<rfi_mitigation_pipe> {
     //simplify_spectrum_work.ptr = d_in_shared;
     //simplify_spectrum_work.count = in_count;
     //simplify_spectrum_work.timestamp = rfi_mitigation_work.timestamp;
+    //simplify_spectrum_work.udp_packet_counter = rfi_mitigation_work.udp_packet_counter;
     //simplify_spectrum_work.batch_size = 1;
     //SRTB_PUSH_WORK_OR_RETURN(" [rfi mitigation pipe] ", srtb::simplify_spectrum_queue,
     //               simplify_spectrum_work);
@@ -104,6 +105,7 @@ class rfi_mitigation_pipe : public pipe<rfi_mitigation_pipe> {
     out_work.ptr = d_in_shared;
     out_work.count = in_count;
     out_work.timestamp = rfi_mitigation_work.timestamp;
+    out_work.udp_packet_counter = rfi_mitigation_work.udp_packet_counter;
     out_work.baseband_freq_low = srtb::config.baseband_freq_low;
     out_work.baseband_sample_rate = srtb::config.baseband_sample_rate;
     out_work.dm = srtb::config.dm;

userspace/include/srtb/pipeline/spectrum_pipe.hpp
@@ -81,6 +81,8 @@ class simplify_spectrum_pipe : public pipe<simplify_spectrum_pipe> {
     draw_spectrum_work.count = out_count;
     draw_spectrum_work.batch_size = batch_size;
     draw_spectrum_work.timestamp = simplify_spectrum_work.timestamp;
+    draw_spectrum_work.udp_packet_counter =
+        simplify_spectrum_work.udp_packet_counter;
     // same as SRTB_PUSH_WORK_OR_RETURN(), but supressed logs
     {
       bool success = false;

userspace/include/srtb/pipeline/udp_receiver_pipe.hpp
@@ -153,9 +153,7 @@ class udp_receiver_pipe : public pipe<udp_receiver_pipe> {
     sycl::event host_to_devive_copy_event =
         q.copy(h_ptr.get(), /* -> */ d_ptr.get(), baseband_input_bytes);
 
-    // choose first counter of new packet in this segment of data as time stamp
-    // TODO: better ideas?
-    const uint64_t timestamp = first_counter;
+    const uint64_t timestamp = std::chrono::system_clock::now().time_since_epoch().count();
 
     // no need to process data if just recording baseband data & not showing spectrum
     if (!(srtb::config.baseband_write_all && (!srtb::config.gui_enable))) {
@@ -165,6 +163,7 @@ class udp_receiver_pipe : public pipe<udp_receiver_pipe> {
       unpack_work.count = baseband_input_bytes;
       unpack_work.baseband_input_bits = baseband_input_bits;
       unpack_work.timestamp = timestamp;
+      unpack_work.udp_packet_counter = first_counter;
       unpack_work.host_to_device_copy_event = host_to_devive_copy_event;
       SRTB_PUSH_WORK_OR_RETURN(" [udp receiver pipe] ", srtb::unpack_queue,
                                unpack_work, stop_token);
@@ -175,6 +174,7 @@ class udp_receiver_pipe : public pipe<udp_receiver_pipe> {
       baseband_output_work.ptr = h_ptr;
       baseband_output_work.count = baseband_input_bytes;
       baseband_output_work.timestamp = timestamp;
+      baseband_output_work.udp_packet_counter = first_counter;
       SRTB_PUSH_WORK_OR_RETURN(" [udp receiver pipe] ",
                                srtb::baseband_output_queue,
                                baseband_output_work, stop_token);

userspace/include/srtb/pipeline/unpack_pipe.hpp
@@ -120,6 +120,7 @@ class unpack_pipe : public pipe<unpack_pipe> {
     fft_1d_r2c_work.ptr = d_out_shared;
     fft_1d_r2c_work.count = out_count;
     fft_1d_r2c_work.timestamp = unpack_work.timestamp;
+    fft_1d_r2c_work.udp_packet_counter = unpack_work.udp_packet_counter;
     SRTB_PUSH_WORK_OR_RETURN(" [unpack pipe] ", srtb::fft_1d_r2c_queue,
                              fft_1d_r2c_work, stop_token);
   }

commit 011ed455f0ab93e904bf012b72d218bbccac305c

[userspace] baseband_output_pipe: fix file name when write all baseband
Fixes: 43ce08b ("[userspace] work: split timestamp & UDP counter")

userspace/include/srtb/pipeline/baseband_output_pipe.hpp
@@ -68,9 +68,12 @@ class baseband_output_pipe</* continuous_write = */ true>
 
     // file name need time stamp, so cannot create early
     if (!file_output_stream) [[unlikely]] {
+      auto file_counter = baseband_output_work.udp_packet_counter;
+      if (file_counter == baseband_output_work.no_udp_packet_counter) {
+        file_counter = baseband_output_work.timestamp;
+      }
       std::string file_path = srtb::config.baseband_output_file_prefix +
-                              std::to_string(baseband_output_work.timestamp) +
-                              ".bin";
+                              std::to_string(file_counter) + ".bin";
       file_output_stream = std::ofstream(file_path.c_str(), std::ios::binary);
       if (!file_output_stream) [[unlikely]] {
         auto err = "Cannot open file " + file_path;