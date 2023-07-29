fxzjshm/simple-radio-telescope-backend

commit 4a1f047ec3e481e9ea0b1e2a43b344666a304e46

[userspace] signal_detect_pipe: copy time series asynchronously
signal_detect_pipe stucks sometimes, so don't wait for copying

userspace/include/srtb/pipeline/signal_detect_pipe.hpp
@@ -138,11 +138,13 @@ class signal_detect_pipe : public pipe<signal_detect_pipe> {
           auto h_out_shared = srtb::host_allocator.allocate_shared<srtb::real>(
               time_series_count);
           auto h_out = h_out_shared.get();
-          q.copy(d_time_series, /* -> */ h_out, time_series_count).wait();
+          sycl::event event =
+              q.copy(d_time_series, /* -> */ h_out, time_series_count);
           srtb::work::time_series_holder time_series_holder{
-              .time_series_ptr = h_out_shared,
+              .h_time_series = h_out_shared,
               .time_series_length = time_series_count,
-              .boxcar_length = 1};
+              .boxcar_length = 1,
+              .transfer_event = event};
           signal_detect_result.time_series.push_back(time_series_holder);
         }
       }
@@ -193,13 +195,13 @@ class signal_detect_pipe : public pipe<signal_detect_pipe> {
                 srtb::host_allocator.allocate_shared<srtb::real>(
                     time_series_count);
             auto h_out = h_out_shared.get();
-            q.copy(d_boxcared_time_series, /* -> */ h_out,
-                   boxcared_time_series_count)
-                .wait();
+            sycl::event event = q.copy(d_boxcared_time_series, /* -> */ h_out,
+                                       boxcared_time_series_count);
             srtb::work::time_series_holder time_series_holder{
-                .time_series_ptr = h_out_shared,
+                .h_time_series = h_out_shared,
                 .time_series_length = boxcared_time_series_count,
-                .boxcar_length = boxcar_length};
+                .boxcar_length = boxcar_length,
+                .transfer_event = event};
             signal_detect_result.time_series.push_back(time_series_holder);
           }
         }

userspace/include/srtb/work.hpp
@@ -209,10 +209,11 @@ using baseband_output_work = srtb::work::work<std::shared_ptr<std::byte> >;
  * @brief contains info for a time series of a spectrum
  */
 struct time_series_holder {
-  std::shared_ptr<srtb::real> time_series_ptr;
+  std::shared_ptr<srtb::real> h_time_series;
   size_t time_series_length;
   /** @brief size of boxcar used to compute the time series; = 1 if not used */
   size_t boxcar_length;
+  sycl::event transfer_event;
 };
 
 /**

userspace/include/srtb/pipeline/baseband_output_pipe.hpp
@@ -199,14 +199,17 @@ class baseband_output_pipe</* continuous_write = */ false>
             time_series_output_stream{time_series_file_path,
                                       BOOST_IOS::binary | BOOST_IOS::out};
 
+        // wait until data copy completed
+        time_series_holder.transfer_event.wait();
+
         // write time series
-        const auto time_series_ptr = time_series_holder.time_series_ptr.get();
+        const auto time_series_ptr = time_series_holder.h_time_series.get();
         const size_t time_series_length = time_series_holder.time_series_length;
         time_series_output_stream.write(
             reinterpret_cast<const char*>(time_series_ptr),
             time_series_length *
-                sizeof(decltype(time_series_holder
-                                    .time_series_ptr)::element_type));
+                sizeof(
+                    decltype(time_series_holder.h_time_series)::element_type));
         time_series_output_stream.flush();
 
         // draw time series using matplotlib cpp

commit 4888b4a33d300c6a7c3997aff9ae252cc1aed0b0

[userspace] signal_detect_pipe: fix destruction before copy finishes
Corrupted time series has been found for many months,
and now it turn out to related to destruction of device memory
before copy was finished.
This patch transfers ownership of device memory to the work
so now it should work correctly.

Fixes: 4a1f047 ("[userspace] signal_detect_pipe: copy time series asynchronously")
Signed-off-by: fxzjshm <fxzjshm@163.com>

userspace/include/srtb/pipeline/signal_detect_pipe.hpp
@@ -406,7 +406,8 @@ class signal_detect_pipe_2 : public pipe<signal_detect_pipe_2> {
           sycl::event event =
               q.copy(d_time_series, /* -> */ h_out, time_series_count);
           srtb::work::time_series_holder time_series_holder{
-              .h_time_series = h_out_shared,
+              .h_time_series = std::move(h_out_shared),
+              .d_time_series = d_time_series_shared,
               .time_series_length = time_series_count,
               .boxcar_length = 1,
               .transfer_event = event};
@@ -424,16 +425,15 @@ class signal_detect_pipe_2 : public pipe<signal_detect_pipe_2> {
         sycl::impl::inclusive_scan(execution_policy, d_time_series,
                                    d_time_series + time_series_count,
                                    d_accumulated, srtb::real{0}, std::plus());
-        // TODO: reuse d_time_series ?
-        // this is reused for different boxcar_length
-        auto d_boxcared_time_series_unique_ptr =
-            srtb::device_allocator.allocate_unique<srtb::real>(
-                time_series_count);
-        auto d_boxcared_time_series = d_boxcared_time_series_unique_ptr.get();
 
+        // this is reused for different boxcar_length
+        auto d_boxcared_time_series_shared =
+            srtb::device_allocator.allocate_shared<srtb::real>(
+                time_series_count);
+        auto d_boxcared_time_series = d_boxcared_time_series_shared.get();
         const size_t max_boxcar_length =
             srtb::config.signal_detect_max_boxcar_length;
-        // TODO: async submit kernel ?
+        // TODO: async submit kernel failed (output corrupt, why?)
         for (size_t boxcar_length = 2; (boxcar_length <= max_boxcar_length &&
                                         boxcar_length < time_series_count);
              boxcar_length *= 2) {
@@ -463,7 +463,8 @@ class signal_detect_pipe_2 : public pipe<signal_detect_pipe_2> {
             sycl::event event = q.copy(d_boxcared_time_series, /* -> */ h_out,
                                        boxcared_time_series_count);
             srtb::work::time_series_holder time_series_holder{
-                .h_time_series = h_out_shared,
+                .h_time_series = std::move(h_out_shared),
+                .d_time_series = std::move(d_boxcared_time_series_shared),
                 .time_series_length = boxcared_time_series_count,
                 .boxcar_length = boxcar_length,
                 .transfer_event = event};

userspace/include/srtb/work.hpp
@@ -256,6 +256,7 @@ using signal_detect_work = simplify_spectrum_work;
  */
 struct time_series_holder {
   std::shared_ptr<srtb::real> h_time_series;
+  std::shared_ptr<srtb::real> d_time_series;
   size_t time_series_length;
   /** @brief size of boxcar used to compute the time series; = 1 if not used */
   size_t boxcar_length;