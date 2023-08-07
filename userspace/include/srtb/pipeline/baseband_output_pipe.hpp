fxzjshm/simple-radio-telescope-backend

commit 4443a7aa7758df32a4830ea40af495a8366fb391

[userspace] pipeline: add signal_detect_pipe & refactor baseband_output_pipe

userspace/include/srtb/pipeline/baseband_output_pipe.hpp
@@ -1,0 +1,150 @@
+/******************************************************************************* 
+ * Copyright (c) 2022 fxzjshm
+ * This software is licensed under Mulan PubL v2.
+ * You can use this software according to the terms and conditions of the Mulan PubL v2.
+ * You may obtain a copy of Mulan PubL v2 at:
+ *          http://license.coscl.org.cn/MulanPubL-2.0
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PubL v2 for more details.
+ ******************************************************************************/
+
+#pragma once
+#ifndef __SRTB_PIPELINE_FILE_PIPE__
+#define __SRTB_PIPELINE_FILE_PIPE__
+
+#include <fstream>
+
+#include "srtb/commons.hpp"
+#include "srtb/pipeline/pipe.hpp"
+
+namespace srtb {
+namespace pipeline {
+
+/**
+ * @brief this pipe reads from baseband output pipe and write it to file.
+ */
+template <bool continuous_write = srtb::write_all_baseband>
+class baseband_output_pipe;
+
+template <>
+class baseband_output_pipe<true> : public pipe<baseband_output_pipe<true> > {
+  friend pipe<baseband_output_pipe<true> >;
+
+ protected:
+  std::ofstream file_output_stream;
+
+ public:
+  baseband_output_pipe() {}
+
+ protected:
+  void run_once_impl() {
+    srtb::work::baseband_output_work baseband_output_work;
+    SRTB_POP_WORK(" [baseband_output_pipe] ", srtb::baseband_output_queue,
+                  baseband_output_work);
+
+    // since writing all baseband, ignore signal detect result
+    while (!srtb::signal_detect_result_queue.empty()) {
+      signal_detect_result_queue.pop();
+    }
+
+    if (!file_output_stream) {
+      std::string file_path = srtb::config.baseband_output_file_prefix +
+                              std::to_string(baseband_output_work.timestamp) +
+                              ".bin";
+      file_output_stream = std::ofstream(file_path.c_str(), std::ios::binary);
+    }
+
+    const char* ptr = reinterpret_cast<char*>(baseband_output_work.ptr.get());
+    const size_t baseband_input_count = baseband_output_work.count;
+    size_t write_count;
+
+    // reserved some samples for next round
+    const size_t nsamps_reserved = srtb::codd::nsamps_reserved();
+
+    if (nsamps_reserved < baseband_input_count) {
+      write_count = baseband_input_count - nsamps_reserved;
+      SRTB_LOGD << " [baseband_output_pipe] "
+                << "reserved " << nsamps_reserved << " samples" << srtb::endl;
+    } else {
+      SRTB_LOGW << " [baseband_output_pipe] "
+                << "baseband_input_count = " << baseband_input_count
+                << " >= nsamps_reserved = " << nsamps_reserved << srtb::endl;
+      write_count = baseband_input_count;
+    }
+    file_output_stream.write(
+        ptr,
+        write_count * sizeof(decltype(baseband_output_work.ptr)::element_type));
+  }
+};
+
+template <>
+class baseband_output_pipe<false> : public pipe<baseband_output_pipe<false> > {
+  friend pipe<baseband_output_pipe<false> >;
+
+ public:
+  baseband_output_pipe() {}
+
+ protected:
+  void run_once_impl() {
+    srtb::work::baseband_output_work baseband_output_work;
+    srtb::work::signal_detect_result signal_detect_result;
+    SRTB_POP_WORK(" [baseband_output_pipe] ", srtb::baseband_output_queue,
+                  baseband_output_work);
+    SRTB_POP_WORK(" [baseband_output_pipe] ", srtb::signal_detect_result_queue,
+                  signal_detect_result);
+    while (baseband_output_work.timestamp != signal_detect_result.timestamp)
+      [[unlikely]] {
+        if (baseband_output_work.timestamp < signal_detect_result.timestamp) {
+          SRTB_LOGW << "[baseband_output_pipe] "
+                    << "baseband_output_work.timestamp = "
+                    << baseband_output_work.timestamp << " < "
+                    << "signal_detect_result.timestamp = "
+                    << signal_detect_result.timestamp << srtb::endl;
+          SRTB_POP_WORK(" [baseband_output_pipe] ", srtb::baseband_output_queue,
+                        baseband_output_work);
+        } else if (baseband_output_work.timestamp >
+                   signal_detect_result.timestamp) {
+          // baseband_output_work.timestamp > signal_detect_result.timestamp
+          SRTB_LOGW << "[baseband_output_pipe] "
+                    << "baseband_output_work.timestamp = "
+                    << baseband_output_work.timestamp << " > "
+                    << "signal_detect_result.timestamp = "
+                    << signal_detect_result.timestamp << srtb::endl;
+          SRTB_POP_WORK(" [baseband_output_pipe] ",
+                        srtb::signal_detect_result_queue, signal_detect_result);
+        } else {
+          SRTB_LOGE << "[baseband_output_pipe] "
+                    << "Logic error. Something must be wrong." << srtb::endl;
+        }
+      }
+
+    if (signal_detect_result.has_signal) {
+      auto timestamp = baseband_output_work.timestamp;
+      SRTB_LOGI << "[baseband_output_pipe] "
+                << "Begin writing baseband data, timestamp = " << timestamp
+                << srtb::endl;
+      std::string file_path = srtb::config.baseband_output_file_prefix +
+                              std::to_string(timestamp) + ".bin";
+      std::ofstream file_output_stream{file_path.c_str(), std::ios::binary};
+
+      const char* ptr = reinterpret_cast<char*>(baseband_output_work.ptr.get());
+      const size_t baseband_input_count = baseband_output_work.count;
+      const size_t write_count = baseband_input_count;
+
+      file_output_stream.write(
+          ptr, write_count *
+                   sizeof(decltype(baseband_output_work.ptr)::element_type));
+      file_output_stream.close();
+      SRTB_LOGI << "[baseband_output_pipe] "
+                << "Finished writing baseband data, timestamp = " << timestamp
+                << srtb::endl;
+    }
+  }
+};
+
+}  // namespace pipeline
+}  // namespace srtb
+
+#endif  // __SRTB_PIPELINE_FILE_PIPE__

userspace/include/srtb/config.hpp
@@ -61,6 +61,8 @@ using udp_packet_counter_type = uint64_t;
 inline constexpr bool fft_window_precompute = false;
 inline constexpr bool fft_operate_in_place = true;
 
+inline constexpr bool write_all_baseband = false;
+
 // TODO: is this necessary or too large?
 inline constexpr size_t MEMORY_ALIGNMENT = 64ul;
 
@@ -186,6 +188,16 @@ struct configs {
    *        of complex numbers, so <= baseband_input_count / 2
    */
   size_t refft_length = 1 << 15;
+
+  /**
+   * @brief threshold for signal detect.
+   *        If $ \exist x_i $ s.t. $$ x_i > \mu + k * \sigma $$,
+   *        where $ x_i $ is a value of time series,
+   *              $ \mu $ is its mean value, $ \sigma $ is variance,
+   *              $ k $ is this threshold
+   *        then it is thought a signal.
+   */
+  srtb::real signal_detect_threshold = 6;
 
   /**
    * @brief Wait time in naneseconds for a thread to sleep if it fails to get work now.

userspace/include/srtb/global_variables.hpp
@@ -20,15 +20,14 @@
  */
 
 #include <condition_variable>
+#include <map>
 #include <mutex>
 #include <thread>
-#include <map>
 
 #include "srtb/config.hpp"
 #include "srtb/sycl.hpp"
 
 namespace srtb {
-
 
 // configs
 
@@ -40,7 +39,6 @@ inline srtb::configs config;
 
 /** @brief names and expressions of changed items of @c srtb::config */
 inline std::map<std::string, std::string> changed_configs;
-
 
 /** @brief default queue for all operations if no queue is specified */
 inline sycl::queue queue;
@@ -84,11 +82,22 @@ inline srtb::work_queue<srtb::work::refft_1d_c2c_work> refft_1d_c2c_queue;
 inline srtb::work_queue<srtb::work::simplify_spectrum_work>
     simplify_spectrum_queue;
 inline srtb::work_queue<srtb::work::draw_spectrum_work> draw_spectrum_queue;
-inline srtb::work_queue<srtb::work::baseband_output_work> baseband_output_queue;
+inline srtb::work_queue<srtb::work::signal_detect_work> signal_detect_queue;
+
+// data of this queue are on host memory, so resource is not very restricted.
+// it also acts as a buffer, that is, only when signal_detect_pipe has result
+// can baseband_output_pipe write or drop it,
+// so capacity must be LARGE
+inline srtb::work_queue<srtb::work::baseband_output_work,
+                        /* fixed_size = */ false>
+    baseband_output_queue{1024 * srtb::work_queue_capacity};
+inline srtb::work_queue<srtb::work::signal_detect_result>
+    signal_detect_result_queue;
 
 namespace pipeline {
 
-// currently used for end of pipeline to send a signal to start of the pipeline.
+// used for end of pipeline to send a signal to start of the pipeline,
+// currently enabled only when input source is a file
 inline std::mutex pipeline_mutex;
 inline std::condition_variable pipeline_cv;
 inline bool need_more_work = false;

userspace/include/srtb/pipeline/fft_pipe.hpp
@@ -285,6 +285,14 @@ class refft_1d_c2c_pipe : public pipe<refft_1d_c2c_pipe> {
     simplify_spectrum_work.timestamp = refft_1d_c2c_work.timestamp;
     SRTB_PUSH_WORK(" [refft 1d c2c pipe] ", srtb::simplify_spectrum_queue,
                    simplify_spectrum_work);
+
+    srtb::work::signal_detect_work signal_detect_work;
+    signal_detect_work.ptr = d_out_shared;
+    signal_detect_work.count = refft_length;
+    signal_detect_work.batch_size = refft_batch_size;
+    signal_detect_work.timestamp = refft_1d_c2c_work.timestamp;
+    SRTB_PUSH_WORK(" [refft 1d c2c pipe] ", srtb::signal_detect_queue,
+                   signal_detect_work);
   }
 };
 

userspace/include/srtb/pipeline/file_pipe.hpp
@@ -1,75 +1,0 @@
-/******************************************************************************* 
- * Copyright (c) 2022 fxzjshm
- * This software is licensed under Mulan PubL v2.
- * You can use this software according to the terms and conditions of the Mulan PubL v2.
- * You may obtain a copy of Mulan PubL v2 at:
- *          http://license.coscl.org.cn/MulanPubL-2.0
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
- * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
- * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
- * See the Mulan PubL v2 for more details.
- ******************************************************************************/
-
-#pragma once
-#ifndef __SRTB_PIPELINE_FILE_PIPE__
-#define __SRTB_PIPELINE_FILE_PIPE__
-
-#include "srtb/commons.hpp"
-#include "srtb/pipeline/pipe.hpp"
-
-namespace srtb {
-namespace pipeline {
-
-/**
- * @brief this pipe reads from baseband output pipe and write it to file.
- * TODO: write only if signal is detected.
- */
-class baseband_output_pipe : public pipe<baseband_output_pipe> {
-  friend pipe<baseband_output_pipe>;
-
- protected:
-  std::ofstream file_output_stream;
-
- public:
-  baseband_output_pipe() {}
-
- protected:
-  void run_once_impl() {
-    srtb::work::baseband_output_work baseband_output_work;
-    SRTB_POP_WORK(" [baseband_output_pipe] ", srtb::baseband_output_queue,
-                  baseband_output_work);
-    // currently save into one file
-    if (!file_output_stream) [[unlikely]] {
-      std::string file_path = srtb::config.baseband_output_file_prefix +
-                              std::to_string(baseband_output_work.timestamp) +
-                              ".bin";
-      file_output_stream = std::ofstream(file_path.c_str(), std::ios::binary);
-    }
-
-    const char* ptr = reinterpret_cast<char*>(baseband_output_work.ptr.get());
-    size_t baseband_input_count = baseband_output_work.count;
-    size_t write_count;
-
-    // reserved some samples for next round
-    const size_t nsamps_reserved = srtb::codd::nsamps_reserved();
-
-    if (nsamps_reserved < baseband_input_count) {
-      write_count = baseband_input_count - nsamps_reserved;
-      SRTB_LOGD << " [baseband_output_pipe] "
-                << "reserved " << nsamps_reserved << " samples" << srtb::endl;
-    } else {
-      SRTB_LOGW << " [baseband_output_pipe] "
-                << "baseband_input_count = " << baseband_input_count
-                << " >= nsamps_reserved = " << nsamps_reserved << srtb::endl;
-      write_count = baseband_input_count;
-    }
-    file_output_stream.write(
-        ptr,
-        write_count * sizeof(decltype(baseband_output_work.ptr)::element_type));
-  }
-};  // namespace srtb
-
-}  // namespace pipeline
-}  // namespace srtb
-
-#endif  // __SRTB_PIPELINE_FILE_PIPE__

userspace/include/srtb/pipeline/read_file_pipe.hpp
@@ -43,7 +43,7 @@ class read_file_pipe : public pipe<read_file_pipe> {
    * 
    * @param file_path path of the file to read
    * @param q a sycl queue to use
-   * @note TODO: share some part of the code with unpack pipe
+   * @note TODO: read file header to get correct timestamp (if exists)
    */
   void read_file(const std::string& file_path,
                  const size_t baseband_input_count,
@@ -56,10 +56,6 @@ class read_file_pipe : public pipe<read_file_pipe> {
 
     input_file_stream.ignore(input_file_offset_bytes);
 
-    std::shared_ptr<char> h_in_shared =
-        srtb::host_allocator.allocate_shared<char>(time_sample_bytes);
-    char* h_in = h_in_shared.get();
-
     // counting bytes read manually because position of file may stop at end of
     // file when reading to that, but that calculating position when reserving
     // sample needs continuous bytes counting, i.e.
@@ -71,6 +67,9 @@ class read_file_pipe : public pipe<read_file_pipe> {
     std::streampos logical_file_pos = input_file_offset_bytes;
 
     while (input_file_stream) {
+      std::shared_ptr<char> h_in_shared =
+          srtb::host_allocator.allocate_shared<char>(time_sample_bytes);
+      char* h_in = h_in_shared.get();
       // parallel memset ?
       std::memset(h_in, 0, time_sample_bytes);
       input_file_stream.read(reinterpret_cast<char*>(h_in), time_sample_bytes);
@@ -83,13 +82,24 @@ class read_file_pipe : public pipe<read_file_pipe> {
 
       uint64_t timestamp =
           std::chrono::system_clock::now().time_since_epoch().count();
+      {
+        srtb::work::unpack_work unpack_work;
+        unpack_work.ptr = std::reinterpret_pointer_cast<std::byte>(d_in_shared);
+        unpack_work.count = time_sample_bytes;
+        unpack_work.timestamp = timestamp;
+        unpack_work.baseband_input_bits = baseband_input_bits;
+        SRTB_PUSH_WORK(" [read_file] ", srtb::unpack_queue, unpack_work);
+      }
 
-      srtb::work::unpack_work unpack_work;
-      unpack_work.ptr = std::reinterpret_pointer_cast<std::byte>(d_in_shared);
-      unpack_work.count = time_sample_bytes;
-      unpack_work.timestamp = timestamp;
-      unpack_work.baseband_input_bits = baseband_input_bits;
-      SRTB_PUSH_WORK(" [read_file] ", srtb::unpack_queue, unpack_work);
+      {
+        srtb::work::baseband_output_work baseband_output_work;
+        baseband_output_work.ptr =
+            std::reinterpret_pointer_cast<std::byte>(h_in_shared);
+        baseband_output_work.count = time_sample_bytes;
+        baseband_output_work.timestamp = timestamp;
+        SRTB_PUSH_WORK(" [udp receiver pipe] ", srtb::baseband_output_queue,
+                       baseband_output_work);
+      }
 
       // reserved some samples for next round
       const size_t nsamps_reserved = srtb::codd::nsamps_reserved();

userspace/include/srtb/pipeline/signal_detect_pipe.hpp
@@ -1,0 +1,125 @@
+/******************************************************************************* 
+ * Copyright (c) 2022 fxzjshm
+ * This software is licensed under Mulan PubL v2.
+ * You can use this software according to the terms and conditions of the Mulan PubL v2.
+ * You may obtain a copy of Mulan PubL v2 at:
+ *          http://license.coscl.org.cn/MulanPubL-2.0
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PubL v2 for more details.
+ ******************************************************************************/
+
+#pragma once
+#ifndef __SRTB_PIPELINE_SIGNAL_DETECT_PIPE__
+#define __SRTB_PIPELINE_SIGNAL_DETECT_PIPE__
+
+#include "srtb/commons.hpp"
+#include "srtb/pipeline/pipe.hpp"
+// --- divide line for clang-format
+#include "srtb/algorithm/map_reduce.hpp"
+#include "srtb/algorithm/multi_reduce.hpp"
+
+namespace srtb {
+namespace pipeline {
+
+/**
+ * @brief this pipe reads from refft-ed spectrum, sum it into time series,
+ *        and detect if there's signal in it.
+ *        If yes, notify someone to write the original baseband data;
+ *        otherwise, just drop it.
+ * TODO: separate this into 2 pipes ? 
+ *       (one for sum into time series, one for actual signal detect)
+ */
+class signal_detect_pipe : public pipe<signal_detect_pipe> {
+  friend pipe<signal_detect_pipe>;
+
+ public:
+ protected:
+  void setup_impl() {}
+
+  void run_once_impl() {
+    srtb::work::signal_detect_work signal_detect_work;
+    SRTB_POP_WORK(" [signal_detect_pipe] ", srtb::signal_detect_queue,
+                  signal_detect_work);
+
+    auto& d_in_shared = signal_detect_work.ptr;
+    auto d_in = d_in_shared.get();
+    const size_t count_per_batch = signal_detect_work.count;
+    const size_t batch_size = signal_detect_work.batch_size;
+    auto d_out_shared =
+        srtb::device_allocator.allocate_shared<srtb::real>(batch_size);
+    auto d_out = d_out_shared.get();
+
+    constexpr auto map = []([[maybe_unused]] size_t pos,
+                            srtb::complex<srtb::real> c) {
+      return srtb::norm(c);
+    };
+
+    srtb::algorithm::multi_mapreduce(
+        d_in, count_per_batch, batch_size, d_out, map,
+        /* reduce = */ sycl::plus<srtb::real>(), q);
+
+    d_in = nullptr;
+    d_in_shared.reset();
+
+    const srtb::real threshold = srtb::config.signal_detect_threshold;
+
+    // remove baseline -- substract average
+    // this is done first to avoid big float - big float when calculating variance
+    // TODO: does baseline changes a lot in this time scale ? Is a linear approximation needed ?
+    {
+      auto d_average_shared = srtb::algorithm::map_average(
+          d_out, batch_size, srtb::algorithm::map_identity(), q);
+      auto d_average = d_average_shared.get();
+      q.parallel_for(sycl::range<1>{batch_size}, [=](sycl::item<1> item) {
+         const auto i = item.get_id(0);
+         d_out[i] -= (*d_average);
+       }).wait();
+    }
+
+    // TODO: matched filter
+
+    // trivial signal detect
+    {
+      auto d_variance_shared = srtb::algorithm::map_average(
+          d_out, batch_size,
+          []([[maybe_unused]] size_t pos, srtb::real x) { return x * x; }, q);
+      auto d_variance = d_variance_shared.get();
+      q.single_task([=]() { (*d_variance) = sycl::sqrt(*d_variance); }).wait();
+
+      auto d_signal_count_shared = srtb::algorithm::map_sum(
+          d_out, batch_size, /* map = */
+          [=]([[maybe_unused]] size_t pos, srtb::real x) -> size_t {
+            // also known as count_if
+            if (srtb::abs(x) > threshold * (*d_variance)) {
+              return size_t{1};
+            } else {
+              return size_t{0};
+            }
+          },
+          q);
+      size_t* d_signal_count = d_signal_count_shared.get();
+      size_t h_signal_count;
+      q.copy(d_signal_count, /* -> */ &h_signal_count, /* size = */ 1);
+
+      const bool has_signal = (h_signal_count > 0);
+      srtb::work::signal_detect_result signal_detect_result{
+          .timestamp = signal_detect_work.timestamp, .has_signal = has_signal};
+      SRTB_PUSH_WORK(" [signal_detect_pipe] ", srtb::signal_detect_result_queue,
+                     signal_detect_result);
+      if (has_signal) {
+        SRTB_LOGI << " [signal_detect_pipe] " << h_signal_count
+                  << " signal(s) detected!" << srtb::endl;
+      } else {
+        SRTB_LOGD << " [signal_detect_pipe] "
+                  << "no signal detected." << srtb::endl;
+      }
+    }
+  }
+};
+
+}  // namespace pipeline
+}  // namespace srtb
+
+#endif  // __SRTB_PIPELINE_SIGNAL_DETECT_PIPE__

userspace/include/srtb/pipeline/udp_receiver_pipe.hpp
@@ -83,7 +83,7 @@ class udp_receiver_pipe : public pipe<udp_receiver_pipe> {
       unpack_work.timestamp = timestamp;
       SRTB_PUSH_WORK(" [udp receiver pipe] ", srtb::unpack_queue, unpack_work);
     }
-    /*
+
     {
       srtb::work::baseband_output_work baseband_output_work;
       baseband_output_work.ptr = h_ptr;
@@ -92,7 +92,6 @@ class udp_receiver_pipe : public pipe<udp_receiver_pipe> {
       SRTB_PUSH_WORK(" [udp receiver pipe] ", srtb::baseband_output_queue,
                      baseband_output_work);
     }
-    */
 
     // reserved some samples for next round
     const size_t nsamps_reserved = srtb::codd::nsamps_reserved();

commit 13b0e28241546c7d0eda75f91c9fc9d4cec60fae

[userspace] baseband_output_pipe: better error check
There are many types of error when `std::ofstream` is invalid,
a simple `if` cannot cover all situations, so separate intentional
error (empty file handle) from real errors, otherwise it will
silencely restart writing when something is really wrong.

This is noticed when hot-fixing a fixed bug on deploy machine.

Related fixed bug: 1c334a0 ("[userspace] pipeline: also write processed spectrum")
(accidentally commited without correct message)

Reported-by: wanghaoda47 <wanghaoda47@163.com>
Reported-by: rszhao <201907007@gznu.edu.cn>
Fixes: 4443a7a ("[userspace] pipeline: add signal_detect_pipe & refactor baseband_output_pipe")
Signed-off-by: fxzjshm <fxzjshm@163.com>

userspace/include/srtb/pipeline/baseband_output_pipe.hpp
@@ -54,7 +54,8 @@ class baseband_output_pipe</* continuous_write = */ true>
   friend pipe<baseband_output_pipe<true> >;
 
  protected:
-  std::ofstream file_output_stream;
+  std::optional<std::ofstream> opt_file_output_stream;
+  std::string file_path;
 
  public:
   baseband_output_pipe() {}
@@ -67,20 +68,23 @@ class baseband_output_pipe</* continuous_write = */ true>
                             stop_token);
 
     // file name need time stamp, so cannot create early
-    if (!file_output_stream) [[unlikely]] {
+    if (!opt_file_output_stream) [[unlikely]] {
       auto file_counter = baseband_output_work.udp_packet_counter;
       if (file_counter == baseband_output_work.no_udp_packet_counter) {
         file_counter = baseband_output_work.timestamp;
       }
-      std::string file_path = srtb::config.baseband_output_file_prefix +
-                              std::to_string(file_counter) + ".bin";
-      file_output_stream = std::ofstream(file_path.c_str(), std::ios::binary);
-      if (!file_output_stream) [[unlikely]] {
-        auto err = "Cannot open file " + file_path;
+      file_path = srtb::config.baseband_output_file_prefix +
+                  std::to_string(file_counter) + ".bin";
+      opt_file_output_stream.emplace(file_path.c_str(), std::ios::binary);
+      if (!opt_file_output_stream || !opt_file_output_stream.value())
+          [[unlikely]] {
+        std::string err = "Cannot open file " + file_path;
         SRTB_LOGE << " [baseband_output_pipe] " << err << srtb::endl;
         throw std::runtime_error{err};
       }
     }
+
+    auto& file_output_stream = opt_file_output_stream.value();
 
     const char* ptr = reinterpret_cast<char*>(
         baseband_output_work.baseband_data.baseband_ptr.get());
@@ -107,6 +111,11 @@ class baseband_output_pipe</* continuous_write = */ true>
     file_output_stream.write(
         ptr, write_count * sizeof(decltype(baseband_output_work.baseband_data
                                                .baseband_ptr)::element_type));
+    if (!file_output_stream) [[unlikely]] {
+      std::string err = "Cannot write to " + file_path;
+      SRTB_LOGE << " [baseband_output_pipe] " << err << srtb::endl;
+      throw std::runtime_error{err};
+    }
 
     srtb::pipeline::notify();
   }