fxzjshm/simple-radio-telescope-backend

commit 2b302fe9271a0112f685f39616da8cd14ebbf33c

[userspace] pipe: set thread name if supported

userspace/include/srtb/pipeline/pipe.hpp
@@ -55,6 +55,12 @@ class pipe {
   std::jthread start() {
     std::jthread jthread{
         [this](std::stop_token stop_token) { run(stop_token); }};
+#if __has_include(<pthread.h>)
+    const std::string thread_name = generate_thread_name();
+    pthread_setname_np(jthread.native_handle(), thread_name.c_str());
+#else
+#warning not setting thread name of pipe (TODO)
+#endif
     jthread.detach();
     return jthread;
   }
@@ -94,6 +100,43 @@ class pipe {
    * @brief Shortcut for CRTP.
    */
   Derived& sub() { return static_cast<Derived&>(*this); }
+
+  /**
+   * @brief thread name of a pipe is type name of the pipe, for debugging
+   * 
+   * @return std::string 
+   */
+  static inline auto generate_thread_name() -> std::string {
+    const auto full_type_name = srtb::type_name<Derived>();
+    // example full type name:
+    //   1) srtb::pipeline::xxx_pipe
+    //   2) srtb::pipeline::xxx_pipe<some_template_parameter>
+    //   3) xxx_pipe
+    // need to deal with all of these
+    constexpr std::string_view template_mark{"<"};
+    constexpr std::string_view namespace_mark{"::"};
+    const size_t template_mark_position = full_type_name.find(template_mark);
+    const auto end = full_type_name.size();
+    size_t start;
+    if (template_mark_position != std::string::npos) {
+      start = full_type_name.rfind(namespace_mark, template_mark_position);
+    } else {
+      start = full_type_name.rfind(namespace_mark, end);
+    }
+    start += namespace_mark.size();
+    auto name = full_type_name;
+    if (start != std::string::npos && start < end) {
+      name = full_type_name.substr(start, (end - start));
+    }
+
+    // pthread restrict thread name length < 16 characters (not including \0 ?),
+    // otherwise pthread_setname_np has no effect
+    constexpr size_t thread_name_length_limit = 15;
+    if (name.size() > thread_name_length_limit) {
+      name = name.substr(0, thread_name_length_limit);
+    }
+    return std::string{name};
+  }
 };
 
 inline void wait_for_notify(std::stop_token stop_token) {

userspace/3rdparty/unmangled_type_name.hpp
@@ -1,0 +1,64 @@
+// https://bitwizeshift.github.io/posts/2021/03/09/getting-an-unmangled-type-name-at-compile-time/
+// author: @bitwizeshift
+// licensed under the MIT License (https://github.com/bitwizeshift/bitwizeshift.github.io/blob/source/LICENSE)
+// edit:
+//     1) add #pramga once & marco for protection
+//     2) wrap into a namespace
+#pragma once
+#ifndef UNMANGLED_TYPE_NAME_HPP
+#define UNMANGLED_TYPE_NAME_HPP
+
+#include <array>  // std::array
+#include <string>
+#include <string_view>
+#include <utility>  // std::index_sequence
+
+namespace unmangled_type_name {
+
+template <std::size_t... Idxs>
+constexpr auto substring_as_array(std::string_view str,
+                                  std::index_sequence<Idxs...>) {
+  return std::array{str[Idxs]..., '\n'};
+}
+
+template <typename T>
+constexpr auto type_name_array() {
+#if defined(__clang__)
+  constexpr auto prefix = std::string_view{"[T = "};
+  constexpr auto suffix = std::string_view{"]"};
+  constexpr auto function = std::string_view{__PRETTY_FUNCTION__};
+#elif defined(__GNUC__)
+  constexpr auto prefix = std::string_view{"with T = "};
+  constexpr auto suffix = std::string_view{"]"};
+  constexpr auto function = std::string_view{__PRETTY_FUNCTION__};
+#elif defined(_MSC_VER)
+  constexpr auto prefix = std::string_view{"type_name_array<"};
+  constexpr auto suffix = std::string_view{">(void)"};
+  constexpr auto function = std::string_view{__FUNCSIG__};
+#else
+#error Unsupported compiler
+#endif
+
+  constexpr auto start = function.find(prefix) + prefix.size();
+  constexpr auto end = function.rfind(suffix);
+
+  static_assert(start < end);
+
+  constexpr auto name = function.substr(start, (end - start));
+  return substring_as_array(name, std::make_index_sequence<name.size()>{});
+}
+
+template <typename T>
+struct type_name_holder {
+  static inline constexpr auto value = type_name_array<T>();
+};
+
+template <typename T>
+constexpr auto type_name() -> std::string_view {
+  constexpr auto& value = type_name_holder<T>::value;
+  return std::string_view{value.data(), value.size()};
+}
+
+}  // namespace unmangled_type_name
+
+#endif  // UNMANGLED_TYPE_NAME_HPP

userspace/CMakeLists.txt
@@ -109,6 +109,7 @@ endif()
 ## ref: https://github.com/lava/matplotlib-cpp/blob/master/CMakeLists.txt
 find_package(Python3 COMPONENTS Interpreter Development REQUIRED)
 find_package(Python3 COMPONENTS NumPy)
+include_directories("3rdparty/")
 include_directories("3rdparty/matplotlib-cpp/")
 find_package(Qt5 COMPONENTS Quick Gui REQUIRED)
 

userspace/include/srtb/commons.hpp
@@ -19,6 +19,13 @@
  * declaration if needed.
  */
 
+#if defined(__clang__) || defined(__GNUC__) || defined(_MSC_VER)
+#define SRTB_HAS_CONSTEXPR_UNMANGLED_TYPE_NAME
+#include "unmangled_type_name.hpp"
+#else
+#include <boost/core/demangle.hpp>
+#endif
+
 #include "srtb/sycl.hpp"
 
 // ------ dividing line for clang-format ------
@@ -61,6 +68,18 @@ inline constexpr auto abs(const T x) noexcept {
     return sycl::fabs(x);
   }
 }
+
+#ifdef SRTB_HAS_CONSTEXPR_UNMANGLED_TYPE_NAME
+template <typename T>
+static inline constexpr auto type_name() {
+  return unmangled_type_name::type_name<T>();
+}
+#else
+template <typename T>
+static inline auto type_name() {
+  return boost::core::demangle(typeid(T).name());
+}
+#endif  // SRTB_HAS_CONSTEXPR_UNMANGLED_TYPE_NAME
 
 }  // namespace srtb
 

commit 92e9a14a7c547d7e2e704c06b67871a460010f90

[userspace] pipeline: fix log tag generation
old logic seems not working properly (because not fully tested),
actually not working for

2) `srtb::pipeline::xxx_pipe<some_template_parameter>`
3) `xxx_pipe`
4) `xxx_pipe<some_template_parameter>`

this hopefully implements these
(although still only 1) and 2) are tested).

Also the function is converted to class so that can be partialized
for special types, like
`composite_pipe<pipe_1, pipe_2>` -> "pipe_1, pipe_2"

Fixes: 2b302fe ("[userspace] pipe: set thread name if supported")
Signed-off-by: fxzjshm <fxzjshm@163.com>

userspace/include/srtb/pipeline/pipe.hpp
@@ -33,34 +33,50 @@ inline namespace detail {
 
 /** @brief class name without namespace or template */
 template <typename Type>
-inline auto class_name() -> std::string {
-  const auto full_type_name = srtb::type_name<Type>();
-  // example full type name:
-  //   1) srtb::pipeline::xxx_pipe
-  //   2) srtb::pipeline::xxx_pipe<some_template_parameter>
-  // need to deal with all of these
-  constexpr std::string_view template_mark{"<"};
-  constexpr std::string_view namespace_mark{"::"};
-  const size_t template_mark_position = full_type_name.find(template_mark);
-  const auto end = full_type_name.size();
-  size_t start;
-  if (template_mark_position != std::string::npos) {
-    start = full_type_name.rfind(namespace_mark, template_mark_position);
-  } else {
-    start = full_type_name.rfind(namespace_mark, end);
+struct type_helper {
+  inline auto class_name() -> std::string {
+    const auto full_type_name = srtb::type_name<Type>();
+    // example full type name:
+    //   1) srtb::pipeline::xxx_pipe
+    //   2) srtb::pipeline::xxx_pipe<some_template_parameter>
+    // need to deal with all of these
+    constexpr std::string_view template_mark{"<"};
+    constexpr std::string_view namespace_mark{"::"};
+
+    constexpr std::string_view blank_chars = " \n\t";
+    const size_t full_type_name_end =
+        full_type_name.find_last_not_of(blank_chars) + 1;
+
+    const size_t template_mark_position = full_type_name.find(template_mark);
+    size_t namespace_pos;
+    if (template_mark_position != std::string::npos) {
+      namespace_pos =
+          full_type_name.rfind(namespace_mark, template_mark_position);
+    } else {
+      namespace_pos = full_type_name.rfind(namespace_mark, full_type_name_end);
+    }
+
+    size_t start;
+    if (namespace_pos != std::string::npos) {
+      start = namespace_pos + namespace_mark.size();
+    } else {
+      start = 0;
+    }
+
+    const size_t end = std::min(full_type_name_end, template_mark_position);
+
+    auto name = full_type_name;
+    if (0 <= start && start < end && end < full_type_name.size()) {
+      name = full_type_name.substr(start, (end - start));
+    }
+    return std::string{name};
   }
-  start += namespace_mark.size();
-  auto name = full_type_name;
-  if (start != std::string::npos && start < end) {
-    name = full_type_name.substr(start, (end - start));
-  }
-  return std::string{name};
-}
+};
 
 /** @brief thread name of a pipe is type name of the pipe, for debugging */
 template <typename Type>
 inline auto generate_thread_name() -> std::string {
-  auto name = class_name<Type>();
+  auto name = type_helper<Type>{}.class_name();
 
   // pthread restrict thread name length < 16 characters (not including \0 ?),
   // otherwise pthread_setname_np has no effect
@@ -99,14 +115,15 @@ class pipe {
    * @brief Run on this thread. May block this thread.
    */
   void run(std::stop_token stop_token) {
+    const auto class_name = type_helper<PipeFunctor>{}.class_name();
+    const auto tag = " [" + class_name + "] ";
     srtb::pipeline::running_pipe_count++;
     while (is_running(stop_token)) [[likely]] {
       std::optional opt_in_work = in_functor(stop_token);
       if (!is_running(stop_token)) [[unlikely]] {
         break;
       }
-      SRTB_LOGD << " [" << class_name<PipeFunctor>() << "] "
-                << "popped work" << srtb::endl;
+      SRTB_LOGD << tag << "popped work" << srtb::endl;
       auto in_work = opt_in_work.value();
       std::optional opt_out_work = pipe_functor(stop_token, in_work);
       if (!is_running(stop_token)) [[unlikely]] {
@@ -114,8 +131,7 @@ class pipe {
       }
       auto out_work = opt_out_work.value();
       out_functor(stop_token, out_work);
-      SRTB_LOGD << " [" << class_name<PipeFunctor>() << "] "
-                << "pushed work" << srtb::endl;
+      SRTB_LOGD << tag << "pushed work" << srtb::endl;
     }
     srtb::pipeline::running_pipe_count--;
   }