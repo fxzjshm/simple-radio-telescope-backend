fxzjshm/simple-radio-telescope-backend

commit 01ce4f0f889e9160d0c7f6396ae102cab60999a4

[userspace] pipeline: rewrite main pipeline
Signed-off-by: fxzjshm <fxzjshm@163.com>

userspace/src/main.cpp
@@ -26,6 +26,7 @@
 #include "srtb/commons.hpp"
 #include "srtb/io/udp_receiver.hpp"
 #include "srtb/pipeline/baseband_output_pipe.hpp"
+#include "srtb/pipeline/copy_to_device_pipe.hpp"
 #include "srtb/pipeline/dedisperse_pipe.hpp"
 #include "srtb/pipeline/exit_handler.hpp"
 #include "srtb/pipeline/fft_pipe.hpp"
@@ -182,7 +183,10 @@ int main(int argc, char** argv) {
   if (std::filesystem::exists(input_file_path)) {
     SRTB_LOGI << " [main] "
               << "Reading file " << input_file_path << srtb::endl;
-    input_thread.push_back(srtb::pipeline::read_file_pipe::start());
+    input_thread.push_back(
+        srtb::pipeline::start_pipe<srtb::pipeline::read_file_pipe>(
+            srtb::queue, srtb::pipeline::dummy_in_functor{},
+            srtb::pipeline::queue_out_functor{srtb::copy_to_device_queue}));
     input_pipe_count = 1;
   } else {
     if (input_file_path != "") {
@@ -195,7 +199,10 @@ int main(int argc, char** argv) {
     allocate_memory_regions(input_pipe_count);
     for (size_t i = 0; i < input_pipe_count; i++) {
       input_thread.push_back(
-          srtb::pipeline::udp_receiver_pipe::start(/* id = */ i));
+          srtb::pipeline::start_pipe<srtb::pipeline::udp_receiver_pipe>(
+              srtb::queue, srtb::pipeline::dummy_in_functor{},
+              srtb::pipeline::queue_out_functor{srtb::copy_to_device_queue},
+              /* id = */ i));
     }
   }
 
@@ -214,46 +221,98 @@ int main(int argc, char** argv) {
     plt::cla();
   }
 
-  std::jthread unpack_thread = srtb::pipeline::unpack_pipe::start();
+  std::jthread unpack_thread =
+      srtb::pipeline::start_pipe<srtb::pipeline::composite_pipe<
+          srtb::pipeline::copy_to_device_pipe, srtb::pipeline::unpack_pipe> >(
+          srtb::queue,
+          srtb::pipeline::queue_in_functor{srtb::copy_to_device_queue},
+          srtb::pipeline::queue_out_functor{srtb::fft_1d_r2c_queue});
 
-  std::jthread fft_1d_r2c_thread = srtb::pipeline::fft_1d_r2c_pipe::start();
+  std::jthread fft_1d_r2c_thread =
+      srtb::pipeline::start_pipe<srtb::pipeline::fft_1d_r2c_pipe>(
+          srtb::queue, srtb::pipeline::queue_in_functor{srtb::fft_1d_r2c_queue},
+          srtb::pipeline::queue_out_functor{srtb::rfi_mitigation_queue});
 
   std::jthread rfi_mitigation_thread =
-      srtb::pipeline::rfi_mitigation_pipe::start();
+      srtb::pipeline::start_pipe<srtb::pipeline::rfi_mitigation_pipe>(
+          srtb::queue,
+          srtb::pipeline::queue_in_functor{srtb::rfi_mitigation_queue},
+          srtb::pipeline::queue_out_functor{srtb::dedisperse_queue});
 
-  std::jthread dedisperse_thread = srtb::pipeline::dedisperse_pipe::start();
+  std::jthread dedisperse_thread =
+      srtb::pipeline::start_pipe<srtb::pipeline::dedisperse_pipe>(
+          srtb::queue, srtb::pipeline::queue_in_functor{srtb::dedisperse_queue},
+          srtb::pipeline::queue_out_functor{srtb::ifft_1d_c2c_queue});
 
-  //std::jthread ifft_1d_c2c_thread = srtb::pipeline::ifft_1d_c2c_pipe::start();
+  //std::jthread ifft_1d_c2c_thread =
+  //    srtb::pipeline::start_pipe<srtb::pipeline::ifft_1d_c2c_pipe>(
+  //        srtb::queue,
+  //        srtb::pipeline::queue_in_functor{srtb::ifft_1d_c2c_queue},
+  //        srtb::pipeline::queue_out_functor{srtb::refft_1d_c2c_queue});
 
-  //std::jthread refft_1d_c2c_thread = srtb::pipeline::refft_1d_c2c_pipe::start();
+  //std::jthread refft_1d_c2c_thread =
+  //    srtb::pipeline::start_pipe<srtb::pipeline::refft_1d_c2c_pipe>(
+  //        srtb::queue,
+  //        srtb::pipeline::queue_in_functor{srtb::refft_1d_c2c_queue},
+  //        srtb::pipeline::queue_out_functor{srtb::signal_detect_queue});
 
   std::jthread watfft_1d_c2c_thread =
-      srtb::pipeline::watfft_1d_c2c_pipe::start();
+      srtb::pipeline::start_pipe<srtb::pipeline::watfft_1d_c2c_pipe>(
+          srtb::queue,
+          srtb::pipeline::queue_in_functor{srtb::ifft_1d_c2c_queue},
+          srtb::pipeline::queue_out_functor{srtb::signal_detect_queue});
 
   //std::jthread signal_detect_thread =
-  //    srtb::pipeline::signal_detect_pipe::start();
+  //    srtb::pipeline::start_pipe<srtb::pipeline::signal_detect_pipe>(
+  //        srtb::queue,
+  //        srtb::pipeline::queue_in_functor{srtb::signal_detect_queue},
+  //        srtb::pipeline::multiple_out_functor{
+  //            srtb::pipeline::queue_out_functor{srtb::baseband_output_queue},
+  //            srtb::pipeline::queue_out_functor{
+  //                srtb::simplify_spectrum_queue}});
 
   std::jthread signal_detect_thread =
-      srtb::pipeline::signal_detect_pipe_2::start();
+      srtb::pipeline::start_pipe<srtb::pipeline::signal_detect_pipe_2>(
+          srtb::queue,
+          srtb::pipeline::queue_in_functor{srtb::signal_detect_queue},
+          srtb::pipeline::multiple_out_functor{
+              srtb::pipeline::queue_out_functor{srtb::baseband_output_queue},
+              srtb::pipeline::queue_out_functor{
+                  srtb::simplify_spectrum_queue}});
 
   std::jthread baseband_output_thread;
   if (srtb::config.baseband_write_all) {
     SRTB_LOGW << " [main] "
               << "Writing all baseband data, take care of disk space!"
               << srtb::endl;
-    baseband_output_thread = srtb::pipeline::baseband_output_pipe<
-        /* continuous_write = */ true>::start();
+    baseband_output_thread =
+        srtb::pipeline::start_pipe<srtb::pipeline::baseband_output_pipe<
+            /* continuous_write = */ true> >(
+            srtb::queue,
+            srtb::pipeline::queue_in_functor{srtb::baseband_output_queue},
+            srtb::pipeline::dummy_out_functor<srtb::work::dummy_work>{});
   } else {
-    baseband_output_thread = srtb::pipeline::baseband_output_pipe<
-        /* continuous_write = */ false>::start();
+    baseband_output_thread =
+        srtb::pipeline::start_pipe<srtb::pipeline::baseband_output_pipe<
+            /* continuous_write = */ false> >(
+            srtb::queue,
+            srtb::pipeline::queue_in_functor{srtb::baseband_output_queue},
+            srtb::pipeline::dummy_out_functor<srtb::work::dummy_work>{});
   }
 
   std::jthread simplify_spectrum_thread;
 #if SRTB_ENABLE_GUI
   if (srtb::config.gui_enable) {
-    //simplify_spectrum_thread = srtb::pipeline::simplify_spectrum_pipe::start();
+    //simplify_spectrum_thread =
+    //    srtb::pipeline::start_pipe<srtb::pipeline::simplify_spectrum_pipe>(
+    //        srtb::queue,
+    //        srtb::pipeline::queue_in_functor{srtb::simplify_spectrum_queue},
+    //        srtb::pipeline::queue_out_functor{srtb::draw_spectrum_queue});
     simplify_spectrum_thread =
-        srtb::pipeline::simplify_spectrum_pipe_2::start();
+        srtb::pipeline::start_pipe<srtb::pipeline::simplify_spectrum_pipe_2>(
+            srtb::queue,
+            srtb::pipeline::queue_in_functor{srtb::simplify_spectrum_queue},
+            srtb::pipeline::queue_out_functor{srtb::draw_spectrum_queue_2});
   }
 #endif  // SRTB_ENABLE_GUI
 

userspace/include/srtb/global_variables.hpp
@@ -73,6 +73,8 @@ inline srtb::memory::cached_allocator<
 
 // work queues
 // TODO: global work queues seem ugly (no extensibility), better ideas ?
+inline srtb::work_queue<srtb::work::copy_to_device_work, /* spsc = */ false>
+    copy_to_device_queue;
 inline srtb::work_queue<srtb::work::unpack_work, /* spsc = */ false>
     unpack_queue;
 inline srtb::work_queue<srtb::work::fft_1d_r2c_work> fft_1d_r2c_queue;

userspace/include/srtb/pipeline/baseband_output_pipe.hpp
@@ -36,8 +36,8 @@
 
 #include "cnpy.h"
 #include "matplotlibcpp.h"
-#include "srtb/commons.hpp"
 #include "srtb/coherent_dedispersion.hpp"
+#include "srtb/commons.hpp"
 #include "srtb/pipeline/framework/pipe.hpp"
 
 namespace srtb {
@@ -49,7 +49,7 @@ namespace pipeline {
 template <bool continuous_write = false>
 class baseband_output_pipe;
 
-template<>
+template <>
 class baseband_output_pipe</* continuous_write = */ true> {
  protected:
   std::optional<std::ofstream> opt_file_output_stream;
@@ -59,7 +59,7 @@ class baseband_output_pipe</* continuous_write = */ true> {
  public:
   baseband_output_pipe(sycl::queue q_) : q{q_} {}
 
-  auto operator()(std::stop_token stop_token,
+  auto operator()([[maybe_unused]] std::stop_token stop_token,
                   srtb::work::baseband_output_work baseband_output_work) {
     //srtb::work::baseband_output_work baseband_output_work;
     //SRTB_POP_WORK_OR_RETURN(" [baseband_output_pipe] ",
@@ -123,7 +123,7 @@ class baseband_output_pipe</* continuous_write = */ true> {
 
 // --------------------------------------------------------------------
 
-template<>
+template <>
 class baseband_output_pipe</* continuous_write = */ false> {
   /** @brief local container of recent works with no signal detected (negative) */
   std::deque<srtb::work::baseband_output_work> recent_negative_works;
@@ -171,8 +171,7 @@ class baseband_output_pipe</* continuous_write = */ false> {
     }
   }
 
- protected:
-  auto generate_time_tag() -> std::string {
+  static auto generate_time_tag() -> std::string {
     // modified from example from https://en.cppreference.com/w/cpp/chrono/c/strftime
     std::time_t time = std::time({});
     // '\0' should included in string literal
@@ -182,7 +181,8 @@ class baseband_output_pipe</* continuous_write = */ false> {
     return std::string{time_string};
   }
 
-  auto operator()(std::stop_token stop_token, srtb::work::baseband_output_work baseband_output_work) {
+  auto operator()([[maybe_unused]] std::stop_token stop_token,
+                  srtb::work::baseband_output_work baseband_output_work) {
     //srtb::work::baseband_output_work baseband_output_work;
     //SRTB_POP_WORK_OR_RETURN(" [baseband_output_pipe] ",
     //                        srtb::baseband_output_queue, baseband_output_work,

userspace/include/srtb/pipeline/copy_to_device_pipe.hpp
@@ -1,0 +1,60 @@
+/******************************************************************************* 
+ * Copyright (c) 2023 fxzjshm
+ * This software is licensed under Mulan PubL v2.
+ * You can use this software according to the terms and conditions of the Mulan PubL v2.
+ * You may obtain a copy of Mulan PubL v2 at:
+ *          http://license.coscl.org.cn/MulanPubL-2.0
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PubL v2 for more details.
+ ******************************************************************************/
+
+#pragma once
+#ifndef __SRTB_PIPELINE_COPY_TO_DEVICE_PIPE__
+#define __SRTB_PIPELINE_COPY_TO_DEVICE_PIPE__
+
+#include <cstddef>
+#include <stop_token>
+
+#include "srtb/pipeline/framework/pipe.hpp"
+#include "srtb/sycl.hpp"
+
+namespace srtb {
+namespace pipeline {
+
+/**
+ * @brief this pipe copy data from host to device.
+ * TODO: check if device has unified memory access
+ */
+class copy_to_device_pipe {
+ public:
+  sycl::queue q;
+
+  auto operator()([[maybe_unused]] std::stop_token stop_token,
+                  srtb::work::copy_to_device_work work) {
+    auto& h_in_shared = work.baseband_data.baseband_ptr;
+    auto h_in = h_in_shared.get();
+    const size_t baseband_input_bytes = work.baseband_data.baseband_input_bytes;
+
+    auto d_in_shared =
+        srtb::device_allocator.allocate_shared<std::byte>(baseband_input_bytes);
+    auto d_in = d_in_shared.get();
+
+    q.copy(h_in, /* -> */ d_in, baseband_input_bytes).wait();
+
+    srtb::work::unpack_work unpack_work;
+    unpack_work.ptr = std::move(d_in_shared);
+    unpack_work.count = baseband_input_bytes;
+    unpack_work.baseband_data = std::move(work.baseband_data);
+    unpack_work.timestamp = work.timestamp;
+    unpack_work.udp_packet_counter = work.udp_packet_counter;
+    unpack_work.baseband_input_bits = srtb::config.baseband_input_bits;
+    return std::optional{unpack_work};
+  }
+};
+
+}  // namespace pipeline
+}  // namespace srtb
+
+#endif  // __SRTB_PIPELINE_COPY_TO_DEVICE_PIPE__

userspace/include/srtb/pipeline/dedisperse_pipe.hpp
@@ -25,10 +25,10 @@ namespace pipeline {
  *        this pipe applies coherent dedispertion to FFT-ed data.
  */
 class dedisperse_pipe {
+ public:
   sycl::queue q;
 
- public:
-  auto operator()(std::stop_token stop_token,
+  auto operator()([[maybe_unused]] std::stop_token stop_token,
                   srtb::work::dedisperse_work work) {
     //SRTB_POP_WORK_OR_RETURN(" [dedisperse pipe] ", srtb::dedisperse_queue, work,
     //                        stop_token);

userspace/include/srtb/pipeline/fft_pipe.hpp
@@ -39,7 +39,7 @@ class fft_1d_r2c_pipe {
                            /* batch_size = */ 1, q);
   }
 
-  auto operator()(std::stop_token stop_token,
+  auto operator()([[maybe_unused]] std::stop_token stop_token,
                   srtb::work::fft_1d_r2c_work fft_1d_r2c_work) {
     // assume opt_dispatcher has value
     auto& dispatcher = opt_dispatcher.value();
@@ -113,7 +113,7 @@ class ifft_1d_c2c_pipe {
                                             /* n = */ input_count, q);
   }
 
-  auto run_once_impl(std::stop_token stop_token,
+  auto run_once_impl([[maybe_unused]] std::stop_token stop_token,
                      srtb::work::ifft_1d_c2c_work ifft_1d_c2c_work) {
     auto& ifft_dispatcher = opt_ifft_dispatcher.value();
 
@@ -242,7 +242,7 @@ class refft_1d_c2c_pipe {
                                              /* n = */ refft_length, q);
   }
 
-  auto operator()(std::stop_token stop_token,
+  auto operator()([[maybe_unused]] std::stop_token stop_token,
                   srtb::work::refft_1d_c2c_work refft_1d_c2c_work) {
     //srtb::work::refft_1d_c2c_work refft_1d_c2c_work;
     //SRTB_POP_WORK_OR_RETURN(" [refft 1d c2c pipe] ", srtb::refft_1d_c2c_queue,
@@ -339,11 +339,11 @@ class watfft_1d_c2c_pipe {
                                               /* n = */ watfft_length, q);
   }
 
-  auto operator()(std::stop_token stop_token,
+  auto operator()([[maybe_unused]] std::stop_token stop_token,
                   srtb::work::ifft_1d_c2c_work ifft_1d_c2c_work) {
-    srtb::work::ifft_1d_c2c_work ifft_1d_c2c_work;
-    SRTB_POP_WORK_OR_RETURN(" [watfft 1d c2c pipe] ", srtb::ifft_1d_c2c_queue,
-                            ifft_1d_c2c_work, stop_token);
+    //srtb::work::ifft_1d_c2c_work ifft_1d_c2c_work;
+    //SRTB_POP_WORK_OR_RETURN(" [watfft 1d c2c pipe] ", srtb::ifft_1d_c2c_queue,
+    //                        ifft_1d_c2c_work, stop_token);
     const size_t input_count = ifft_1d_c2c_work.count;
 
     auto& watfft_dispatcher = opt_watfft_dispatcher.value();

userspace/include/srtb/pipeline/read_file_pipe.hpp
@@ -46,8 +46,6 @@ class read_file_pipe {
     //}
 
     auto file_path = srtb::config.input_file_path;
-    auto baseband_input_count = srtb::config.baseband_input_count;
-    auto baseband_input_bits = srtb::config.baseband_input_bits;
     auto input_file_offset_bytes = srtb::config.input_file_offset_bytes;
 
     input_file_stream =
@@ -68,7 +66,8 @@ class read_file_pipe {
     log_given = false;
   }
 
-  auto operator()(std::stop_token stop_token, srtb::work::dummy_work) {
+  auto operator()([[maybe_unused]] std::stop_token stop_token,
+                  srtb::work::dummy_work) {
     if (input_file_stream) {
       auto baseband_input_count = srtb::config.baseband_input_count;
       auto baseband_input_bits = srtb::config.baseband_input_bits;
@@ -107,16 +106,17 @@ class read_file_pipe {
       const uint64_t timestamp =
           std::chrono::system_clock::now().time_since_epoch().count();
 
-      srtb::work::unpack_work unpack_work;
-      unpack_work.ptr = std::reinterpret_pointer_cast<std::byte>(d_in_shared);
-      unpack_work.count = time_sample_bytes;
-      unpack_work.baseband_data = {
+      srtb::work::copy_to_device_work copy_to_device_work;
+      copy_to_device_work.ptr =
+          std::reinterpret_pointer_cast<std::byte>(d_in_shared);
+      copy_to_device_work.count = time_sample_bytes;
+      copy_to_device_work.baseband_data = {
           std::reinterpret_pointer_cast<std::byte>(h_in_shared),
           time_sample_bytes};
-      unpack_work.timestamp = timestamp;
-      unpack_work.udp_packet_counter = unpack_work.no_udp_packet_counter;
-      unpack_work.baseband_input_bits = baseband_input_bits;
-      return std::optional{unpack_work};
+      copy_to_device_work.timestamp = timestamp;
+      copy_to_device_work.udp_packet_counter =
+          copy_to_device_work.no_udp_packet_counter;
+      return std::optional{copy_to_device_work};
       //SRTB_PUSH_WORK_OR_RETURN(" [read_file] ", srtb::unpack_queue,
       //                         unpack_work, stop_token);
 
@@ -134,6 +134,7 @@ class read_file_pipe {
         SRTB_LOGI << " [read_file] " << file_path << " has been read"
                   << srtb::endl;
       }
+      return std::optional<srtb::work::copy_to_device_work>{};
     }
   }
 };

userspace/include/srtb/pipeline/rfi_mitigation_pipe.hpp
@@ -40,7 +40,7 @@ class rfi_mitigation_pipe {
  public:
   rfi_mitigation_pipe(sycl::queue q_) : q{q_} {}
 
-  auto operator()(std::stop_token stop_token,
+  auto operator()([[maybe_unused]] std::stop_token stop_token,
                   srtb::work::rfi_mitigation_work rfi_mitigation_work) {
     //srtb::work::rfi_mitigation_work rfi_mitigation_work;
     //SRTB_POP_WORK_OR_RETURN(" [rfi mitigation pipe] ",

commit 6d5447f5e4b97a2ebcefe231d4b748e6720638a8

[userspace] pipeline: fix pipeline stuck when GUI is slow
re-implement part of 782e39e ("[userspace] rfi_mitigation: add spectral kurtosis method")

when pushing work to simplify_spectrum_queue,
"just try once, in case GUI is stuck (e.g. when using X forwarding on SSH)"
now implemented by loosely connected queue out functor
(still try once only)

Fixes: 01ce4f0 ("[userspace] pipeline: rewrite main pipeline")
Signed-off-by: fxzjshm <fxzjshm@163.com>

userspace/include/srtb/pipeline/framework/pipe_io.hpp
@@ -75,6 +75,24 @@ class queue_out_functor {
   }
 };
 
+/** @brief This functor pushes output work to work queue, but only try once */
+template <typename Queue>
+class loose_queue_out_functor {
+ protected:
+  Queue& work_queue;
+  using Work = typename Queue::work_type;
+
+ public:
+  explicit loose_queue_out_functor(Queue& work_queue_)
+      : work_queue{work_queue_} {}
+
+  auto operator()(std::stop_token stop_token, Work work) {
+    if (!stop_token.stop_requested()) [[likely]] {
+      work_queue.push(work);
+    }
+  }
+};
+
 /** @brief This functor copies work and give it to multiple out functors. */
 template <typename... OutFunctors>
 class multiple_out_functor {

userspace/src/main.cpp
@@ -278,7 +278,7 @@ int main(int argc, char** argv) {
           srtb::pipeline::queue_in_functor{srtb::rfi_mitigation_s2_queue},
           srtb::pipeline::multiple_out_functor{
               srtb::pipeline::queue_out_functor{srtb::signal_detect_queue},
-              srtb::pipeline::queue_out_functor{
+              srtb::pipeline::loose_queue_out_functor{
                   srtb::simplify_spectrum_queue}});
 
   std::jthread signal_detect_thread =