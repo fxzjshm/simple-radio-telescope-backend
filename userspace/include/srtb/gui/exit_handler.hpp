fxzjshm/simple-radio-telescope-backend

commit 75b4af2eaae4f91dea9bdb17e7b3720457fd5442

[userspace] pipeline: setup pipeline framework
* move supporting code to framework/
* gather pipeline in/out functor into single file
  * including dummy io instead of lambda
* correct pipe abstract logic
  * empty work currently means no more work, and this pipe should stop
* improve pipe logging

Signed-off-by: fxzjshm <fxzjshm@163.com>

userspace/src/main.cpp
@@ -29,6 +29,9 @@
 #include "srtb/pipeline/dedisperse_pipe.hpp"
 #include "srtb/pipeline/exit_handler.hpp"
 #include "srtb/pipeline/fft_pipe.hpp"
+#include "srtb/pipeline/framework/composite_pipe.hpp"
+#include "srtb/pipeline/framework/pipe.hpp"
+#include "srtb/pipeline/framework/pipe_io.hpp"
 #include "srtb/pipeline/read_file_pipe.hpp"
 #include "srtb/pipeline/rfi_mitigation_pipe.hpp"
 #include "srtb/pipeline/signal_detect_pipe.hpp"

userspace/include/srtb/pipeline/baseband_output_pipe.hpp
@@ -38,7 +38,7 @@
 #include "matplotlibcpp.h"
 #include "srtb/commons.hpp"
 #include "srtb/coherent_dedispersion.hpp"
-#include "srtb/pipeline/pipe.hpp"
+#include "srtb/pipeline/framework/pipe.hpp"
 
 namespace srtb {
 namespace pipeline {
@@ -117,7 +117,7 @@ class baseband_output_pipe</* continuous_write = */ true> {
     }
 
     srtb::pipeline::notify();
-    return std::optional{true};
+    return std::optional{srtb::work::dummy_work{}};
   }
 };
 
@@ -396,7 +396,7 @@ class baseband_output_pipe</* continuous_write = */ false> {
     }  // if (opt_work_to_write.has_value())
 
     srtb::pipeline::notify();
-    return std::optional{true};
+    return std::optional{srtb::work::dummy_work{}};
   }  // auto operator()
 };
 

userspace/include/srtb/pipeline/dedisperse_pipe.hpp
@@ -15,7 +15,7 @@
 #define __SRTB_PIPELINE_DEDISPERSE_PIPE__
 
 #include "srtb/coherent_dedispersion.hpp"
-#include "srtb/pipeline/pipe.hpp"
+#include "srtb/pipeline/framework/pipe.hpp"
 
 namespace srtb {
 namespace pipeline {

userspace/include/srtb/pipeline/fft_pipe.hpp
@@ -17,7 +17,7 @@
 #include "srtb/commons.hpp"
 #include "srtb/fft/fft.hpp"
 #include "srtb/fft/fft_window.hpp"
-#include "srtb/pipeline/pipe.hpp"
+#include "srtb/pipeline/framework/pipe.hpp"
 #include "srtb/spectrum/rfi_mitigation.hpp"
 
 namespace srtb {

userspace/include/srtb/pipeline/framework/composite_pipe.hpp
@@ -1,0 +1,72 @@
+/******************************************************************************* 
+ * Copyright (c) 2023 fxzjshm
+ * This software is licensed under Mulan PubL v2.
+ * You can use this software according to the terms and conditions of the Mulan PubL v2.
+ * You may obtain a copy of Mulan PubL v2 at:
+ *          http://license.coscl.org.cn/MulanPubL-2.0
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PubL v2 for more details.
+ ******************************************************************************/
+
+#pragma once
+#ifndef __SRTB_PIPELINE_COMPOSITE_PIPE__
+#define __SRTB_PIPELINE_COMPOSITE_PIPE__
+
+#include <stop_token>
+
+#include "srtb/pipeline/framework/pipe.hpp"
+
+namespace srtb {
+namespace pipeline {
+
+/**
+ * @brief this pipe serializes multiple pipes
+ */
+template <typename Pipe1, typename... Pipes>
+class composite_pipe {
+ public:
+  Pipe1 pipe_1;
+  composite_pipe<Pipes...> pipes;
+
+  // TODO: support arguments
+  explicit composite_pipe(sycl::queue q_) : pipe_1{q_}, pipes{q_} {}
+
+  auto operator()(std::stop_token stop_token, auto in_work) {
+    std::optional opt_work = pipe_1(stop_token, in_work);
+    using out_type = decltype(pipes(stop_token, opt_work.value()));
+    if (opt_work) [[likely]] {
+      return pipes(stop_token, opt_work.value());
+    } else [[unlikely]] {
+      return out_type{};
+    }
+  }
+};
+
+template <typename Pipe>
+class composite_pipe<Pipe> : public Pipe {};
+
+inline namespace detail {
+
+template <typename Pipe1, typename... Pipes>
+struct type_helper<composite_pipe<Pipe1, Pipes...> > {
+  inline auto class_name() -> std::string {
+    return type_helper<Pipe1>{}.class_name() + ", " +
+           type_helper<Pipes...>{}.class_name();
+  }
+};
+
+template <typename Pipe>
+struct type_helper<composite_pipe<Pipe> > {
+  inline auto class_name() -> std::string {
+    return type_helper<Pipe>{}.class_name();
+  }
+};
+
+}  // namespace detail
+
+}  // namespace pipeline
+}  // namespace srtb
+
+#endif  // __SRTB_PIPELINE_COMPOSITE_PIPE__

userspace/include/srtb/pipeline/framework/pipe.hpp
@@ -98,7 +98,7 @@ inline bool is_running(std::stop_token stop_token) {
  * @brief Represents a pipe, which receives work from a work_queue,
  *        apply transform and then push the result to next one, on different threads.
  * @note TODO: decouple work queue push and pop from @c run_once_impl
- * @tparam PipeFunctor actural pipe, with signature pipe_functor(std::stop_token, in_work_type) -> out_work_type
+ * @tparam PipeFunctor actural pipe, with signature pipe_functor(std::stop_token, in_work_type) -> std::optional<out_work_type>
  * @tparam InFunctor funtor to provide work for this pipe, with signature @c in_functor(std::stop_token) -> std::optional<in_work_type>
  * @tparam OutFunctor functor to store result of this pipe, with signature @c out_functor(std::stop_token, out_work_type) -> void
  */
@@ -120,70 +120,20 @@ class pipe {
     srtb::pipeline::running_pipe_count++;
     while (is_running(stop_token)) [[likely]] {
       std::optional opt_in_work = in_functor(stop_token);
-      if (!is_running(stop_token)) [[unlikely]] {
+      if (!is_running(stop_token) || !opt_in_work) [[unlikely]] {
         break;
       }
-      SRTB_LOGD << tag << "popped work" << srtb::endl;
+      SRTB_LOGD << tag << "got work" << srtb::endl;
       auto in_work = opt_in_work.value();
       std::optional opt_out_work = pipe_functor(stop_token, in_work);
-      if (!is_running(stop_token)) [[unlikely]] {
+      if (!is_running(stop_token) || !opt_out_work) [[unlikely]] {
         break;
       }
       auto out_work = opt_out_work.value();
       out_functor(stop_token, out_work);
-      SRTB_LOGD << tag << "pushed work" << srtb::endl;
+      SRTB_LOGD << tag << "work finished" << srtb::endl;
     }
     srtb::pipeline::running_pipe_count--;
-  }
-};
-
-template <typename Queue>
-class queue_in_functor {
- protected:
-  Queue& work_queue;
-  using Work = typename Queue::work_type;
-
- public:
-  explicit queue_in_functor(Queue& work_queue_) : work_queue{work_queue_} {}
-
-  auto operator()(std::stop_token stop_token) {
-    Work work;
-    bool ret = work_queue.pop(work);
-    if (!ret) [[unlikely]] {
-      while (!ret) {
-        if (stop_token.stop_requested()) [[unlikely]] {
-          return std::optional<Work>{};
-        }
-        std::this_thread::sleep_for(
-            std::chrono::nanoseconds(srtb::config.thread_query_work_wait_time));
-        ret = work_queue.pop(work);
-      }
-    }
-    return std::optional{work};
-  }
-};
-
-template <typename Queue>
-class queue_out_functor {
- protected:
-  Queue& work_queue;
-  using Work = typename Queue::work_type;
-
- public:
-  explicit queue_out_functor(Queue& work_queue_) : work_queue{work_queue_} {}
-
-  auto operator()(std::stop_token stop_token, Work work) {
-    bool ret = work_queue.push(work);
-    if (!ret) [[unlikely]] {
-      while (!ret) {
-        if (stop_token.stop_requested()) [[unlikely]] {
-          return;
-        }
-        std::this_thread::sleep_for(
-            std::chrono::nanoseconds(srtb::config.thread_query_work_wait_time));
-        ret = work_queue.push(work);
-      }
-    }
   }
 };
 

userspace/include/srtb/pipeline/framework/pipe_io.hpp
@@ -1,0 +1,113 @@
+/******************************************************************************* 
+ * Copyright (c) 2023 fxzjshm
+ * This software is licensed under Mulan PubL v2.
+ * You can use this software according to the terms and conditions of the Mulan PubL v2.
+ * You may obtain a copy of Mulan PubL v2 at:
+ *          http://license.coscl.org.cn/MulanPubL-2.0
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PubL v2 for more details.
+ ******************************************************************************/
+
+#pragma once
+#ifndef __SRTB_PIPELINE_PIPE_IO__
+#define __SRTB_PIPELINE_PIPE_IO__
+
+#include <stop_token>
+#include <thread>
+#include <type_traits>
+
+#include "srtb/commons.hpp"
+
+namespace srtb {
+namespace pipeline {
+
+/** @brief This functor reads input work from work queue */
+template <typename Queue>
+class queue_in_functor {
+ protected:
+  Queue& work_queue;
+  using Work = typename Queue::work_type;
+
+ public:
+  explicit queue_in_functor(Queue& work_queue_) : work_queue{work_queue_} {}
+
+  auto operator()(std::stop_token stop_token) {
+    Work work;
+    bool ret = work_queue.pop(work);
+    if (!ret) [[unlikely]] {
+      while (!ret) {
+        if (stop_token.stop_requested()) [[unlikely]] {
+          return std::optional<Work>{};
+        }
+        std::this_thread::sleep_for(
+            std::chrono::nanoseconds(srtb::config.thread_query_work_wait_time));
+        ret = work_queue.pop(work);
+      }
+    }
+    return std::optional{work};
+  }
+};
+
+/** @brief This functor pushes output work to work queue */
+template <typename Queue>
+class queue_out_functor {
+ protected:
+  Queue& work_queue;
+  using Work = typename Queue::work_type;
+
+ public:
+  explicit queue_out_functor(Queue& work_queue_) : work_queue{work_queue_} {}
+
+  auto operator()(std::stop_token stop_token, Work work) {
+    bool ret = work_queue.push(work);
+    if (!ret) [[unlikely]] {
+      while (!ret) {
+        if (stop_token.stop_requested()) [[unlikely]] {
+          return;
+        }
+        std::this_thread::sleep_for(
+            std::chrono::nanoseconds(srtb::config.thread_query_work_wait_time));
+        ret = work_queue.push(work);
+      }
+    }
+  }
+};
+
+/** @brief This functor copies work and give it to multiple out functors. */
+template <typename... OutFunctors>
+class multiple_out_functor {
+  // ref: https://stackoverflow.com/questions/54631547/using-stdapply-with-variadic-packs/54631721
+ public:
+  std::tuple<OutFunctors...> out_functors;
+
+  explicit multiple_out_functor(OutFunctors&&... out_functors_)
+      : out_functors{out_functors_...} {}
+
+  template <typename Work>
+  auto operator()(std::stop_token stop_token, Work work) {
+    std::apply(
+        [=](auto&... out_functor) { (out_functor(stop_token, work), ...); },
+        out_functors);
+  }
+};
+
+template <typename T = srtb::work::dummy_work>
+class dummy_in_functor {
+ public:
+  auto operator()([[maybe_unused]] std::stop_token) {
+    return std::optional{T{}};
+  }
+};
+
+template <typename T = srtb::work::dummy_work>
+class dummy_out_functor {
+ public:
+  auto operator()([[maybe_unused]] std::stop_token, [[maybe_unused]] T) {}
+};
+
+}  // namespace pipeline
+}  // namespace srtb
+
+#endif  // __SRTB_PIPELINE_PIPE_IO__

userspace/include/srtb/pipeline/read_file_pipe.hpp
@@ -19,7 +19,7 @@
 
 #include "srtb/coherent_dedispersion.hpp"
 #include "srtb/commons.hpp"
-#include "srtb/pipeline/pipe.hpp"
+#include "srtb/pipeline/framework/pipe.hpp"
 
 namespace srtb {
 namespace pipeline {
@@ -68,7 +68,7 @@ class read_file_pipe {
     log_given = false;
   }
 
-  auto operator()(std::stop_token stop_token) {
+  auto operator()(std::stop_token stop_token, srtb::work::dummy_work) {
     if (input_file_stream) {
       auto baseband_input_count = srtb::config.baseband_input_count;
       auto baseband_input_bits = srtb::config.baseband_input_bits;

commit 7ac8faebb7ad78c91c016db1ff7ca597f1a6ac6d

[userspace] gui: remove GUI exit_handler
not needed any more as main can call exit_handler after GUI exit;
also moved exit_handler to pipeline/framework, which is where it should be

Fixes: 75b4af2 ("[userspace] pipeline: setup pipeline framework")
Signed-off-by: fxzjshm <fxzjshm@163.com>

userspace/include/srtb/gui/exit_handler.hpp
@@ -1,57 +1,0 @@
-/******************************************************************************* 
- * Copyright (c) 2022 fxzjshm
- * This software is licensed under Mulan PubL v2.
- * You can use this software according to the terms and conditions of the Mulan PubL v2.
- * You may obtain a copy of Mulan PubL v2 at:
- *          http://license.coscl.org.cn/MulanPubL-2.0
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
- * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
- * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
- * See the Mulan PubL v2 for more details.
- ******************************************************************************/
-
-#pragma once
-#ifndef __SRTB_GUI_EXIT_HANDLER__
-#define __SRTB_GUI_EXIT_HANDLER__
-
-#include <thread>
-#include <vector>
-
-#include "srtb/commons.hpp"
-#include "srtb/pipeline/exit_handler.hpp"
-
-// Qt related things
-// some macro defined in Qt may conflict with some STL class, especially C++11 or newer ones
-// so put these last
-#include <QObject>
-
-namespace srtb {
-namespace gui {
-
-/** 
- * @brief this class requests every thread to stop and wait for them
- *        when the program is about to (normally) exit.
- * @note Q_OBJECT does not support template class, so std::array is not used here.
- * @note moved to srtb::pipeline::on_exit() for no-GUI usage
- * @see srtb::termination_handler for handler of unexpected exit
- */
-class ExitHandler : public QObject {
-  Q_OBJECT
-
- public:
-  std::vector<std::jthread> threads;
-
- public:
-  explicit ExitHandler(std::vector<std::jthread> threads_)
-      : threads{std::move(threads_)} {}
-
- public Q_SLOTS:
-  void onExit() {
-    srtb::pipeline::on_exit(std::move(threads));
-  }
-};
-
-}  // namespace gui
-}  // namespace srtb
-
-#endif  // __SRTB_GUI_EXIT_HANDLER__

userspace/include/srtb/gui/gui.hpp
@@ -24,7 +24,6 @@
 #include <QQmlComponent>
 #include <QQuickWindow>
 
-#include "srtb/gui/exit_handler.hpp"
 #include "srtb/gui/spectrum_image_provider.hpp"
 
 namespace srtb {
@@ -32,7 +31,7 @@ namespace gui {
 
 // QML related things in src/main.qml, which is treated as a .cpp file.
 
-inline int show_gui(int argc, char **argv, std::vector<std::jthread> threads) {
+inline int show_gui(int argc, char **argv) {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
   QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
@@ -62,10 +61,6 @@ inline int show_gui(int argc, char **argv, std::vector<std::jthread> threads) {
       },
       Qt::QueuedConnection);
   engine.load(url);
-
-  ExitHandler exit_handler{std::move(threads)};
-  QObject::connect(&app, &QCoreApplication::aboutToQuit, &exit_handler,
-                   &srtb::gui::ExitHandler::onExit);
 
   return app.exec();
 }

userspace/include/srtb/pipeline/framework/exit_handler.hpp
@@ -25,7 +25,6 @@ namespace pipeline {
 /** 
  * @brief this function requests every thread to stop and wait for them
  *        when the program is about to (normally) exit.
- * @note extracted from srtb::gui::ExitHandler for no-GUI use
  * @see srtb::termination_handler for handler of unexpected exit
  */
 inline void on_exit(std::vector<std::jthread> threads) {

userspace/src/CMakeLists.txt
@@ -5,7 +5,6 @@ if(SRTB_ENABLE_GUI)
     ${SRTB_MAIN_SOURCE_FILES}
     qml.qrc 
     ../include/srtb/gui/spectrum_image_provider.hpp
-    ../include/srtb/gui/exit_handler.hpp
   )
 endif()
 add_executable(simple-radio-telescope-backend ${SRTB_MAIN_SOURCE_FILES})
@@ -35,7 +34,6 @@ if(SRTB_ENABLE_GUI)
     test-gui.cpp
     qml.qrc
     ../include/srtb/gui/spectrum_image_provider.hpp
-    ../include/srtb/gui/exit_handler.hpp
   )
   srtb_add_dependency(test-gui)
 endif()

userspace/src/main.cpp
@@ -28,7 +28,7 @@
 #include "srtb/pipeline/baseband_output_pipe.hpp"
 #include "srtb/pipeline/copy_to_device_pipe.hpp"
 #include "srtb/pipeline/dedisperse_pipe.hpp"
-#include "srtb/pipeline/exit_handler.hpp"
+#include "srtb/pipeline/framework/exit_handler.hpp"
 #include "srtb/pipeline/fft_pipe.hpp"
 #include "srtb/pipeline/framework/composite_pipe.hpp"
 #include "srtb/pipeline/framework/dummy_pipe.hpp"
@@ -383,7 +383,7 @@ int main(int argc, char** argv) {
 
 #if SRTB_ENABLE_GUI
   if (srtb::config.gui_enable) {
-    return_value = srtb::gui::show_gui(argc, argv, std::move(threads));
+    return_value = srtb::gui::show_gui(argc, argv);
   } else {
 #endif  // SRTB_ENABLE_GUI
 
@@ -406,12 +406,13 @@ int main(int argc, char** argv) {
           std::chrono::nanoseconds(srtb::config.thread_query_work_wait_time));
     }
 
-    srtb::pipeline::on_exit(std::move(threads));
     return_value = EXIT_SUCCESS;
 
 #if SRTB_ENABLE_GUI
   }
 #endif  // SRTB_ENABLE_GUI
+
+  srtb::pipeline::on_exit(std::move(threads));
 
   SRTB_LOGI << " [main] "
             << "Exiting." << srtb::endl;