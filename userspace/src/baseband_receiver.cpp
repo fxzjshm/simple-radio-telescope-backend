3c6a122a8c08ad72440954d47a567ad198299ed9
fxzjshm <fxzjshm@163.com>
2023-10-03 16:27:21 +0800

    [userspace] baseband_receiver: fix compile error

--- userspace/src/baseband_receiver.cpp
+++ userspace/src/baseband_receiver.cpp
@@ -14,6 +14,7 @@
 #include <optional>
 
 #include "srtb/commons.hpp"
+#include "srtb/pipeline/framework/composite_pipe.hpp"
 #include "srtb/pipeline/framework/pipe.hpp"
 #include "srtb/pipeline/framework/pipe_io.hpp"
 #include "srtb/pipeline/udp_receiver_pipe.hpp"
@@ -21,6 +22,32 @@
 #include "srtb/program_options.hpp"
 
 namespace srtb {
+
+namespace work {
+
+using baseband_receiver_cast_work =
+    srtb::work::work<std::shared_ptr<std::byte>>;
+
+}
+
+namespace pipeline {
+
+class baseband_receiver_cast_pipe {
+ public:
+  baseband_receiver_cast_pipe() = default;
+  baseband_receiver_cast_pipe([[maybe_unused]] sycl::queue q) {}
+
+  auto operator()([[maybe_unused]] std::stop_token stop_token,
+                  srtb::work::baseband_receiver_cast_work in_work) {
+    srtb::work::write_file_work out_work;
+    out_work.move_parameter_from(std::move(in_work));
+    // don't need to care about other things
+    return out_work;
+  }
+};
+
+}  // namespace pipeline
+
 namespace main {
 
 /**
@@ -34,15 +61,18 @@ int baseband_receiver(int argc, char** argv) {
                                                srtb::config);
 
   sycl::queue q;
-  srtb::work_queue<srtb::work::baseband_output_work> baseband_output_queue;
+
+  // type mismatch; manual cast required
+  srtb::work_queue<srtb::work::copy_to_device_work> write_file_queue;
+
+  using namespace srtb::pipeline;
   std::jthread udp_receiver_thread =
-      srtb::pipeline::start_pipe<srtb::pipeline::udp_receiver_pipe>(
-          q, srtb::pipeline::dummy_in_functor{},
-          srtb::pipeline::queue_out_functor{baseband_output_queue});
-  std::jthread baseband_output_thread =
-      srtb::pipeline::start_pipe<srtb::pipeline::write_file_pipe>(
-          q, srtb::pipeline::queue_in_functor{baseband_output_queue},
-          srtb::pipeline::dummy_out_functor{});
+      srtb::pipeline::start_pipe<udp_receiver_pipe>(
+          q, dummy_in_functor{}, queue_out_functor{write_file_queue});
+  std::jthread baseband_output_thread = srtb::pipeline::start_pipe<
+      composite_pipe<baseband_receiver_cast_pipe, write_file_pipe>>(
+      q, queue_in_functor{write_file_queue}, dummy_out_functor{});
+
   return EXIT_SUCCESS;
 }
 
