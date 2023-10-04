9e9eb9ce35f0f59cd48d0a7c512548e65629bd34
fxzjshm <fxzjshm@163.com>
2023-10-04 21:50:46 +0800

    [userspace] 21cma-genfil: support multiple data streams
    Now write_multi_filterbank_pipe will dispatch work
    according to data_stream_id.

--- userspace/src/21cma-genfil/write_multi_filterbank_pipe.hpp
+++ userspace/src/21cma-genfil/write_multi_filterbank_pipe.hpp
@@ -18,6 +18,7 @@
 #include <chrono>
 #include <cstddef>
 #include <fstream>
+#include <map>
 #include <stop_token>
 
 #include "srtb/algorithm/mjd.hpp"
@@ -41,7 +42,7 @@ namespace pipeline {
  * @note spectrum has been reversed & cut in fft_r2c_post_process_pipe
  * TODO: in_work.data_stream_id
  */
-class write_multi_filterbank_pipe {
+class write_multi_filterbank_pipe_impl {
  public:
   using in_work_type = srtb::work::write_multi_filterbank_work;
   using out_work_type = srtb::work::dummy_work;
@@ -49,13 +50,14 @@ class write_multi_filterbank_pipe {
 
  public:
   sycl::queue q;
+  uint64_t id;
   std::optional<std::ofstream> opt_fout;
   size_t written_bytes;
   uint64_t file_number;
   std::string file_path;
 
-  explicit write_multi_filterbank_pipe(sycl::queue q_)
-      : q{q_}, written_bytes{0}, file_number{1} {}
+  explicit write_multi_filterbank_pipe_impl(sycl::queue q_, uint64_t id_)
+      : q{q_}, id{id_}, written_bytes{0}, file_number{1} {}
 
   auto operator()([[maybe_unused]] std::stop_token stop_token,
                   in_work_type in_work) -> out_work_type {
@@ -65,6 +67,13 @@ class write_multi_filterbank_pipe {
     const size_t count = in_work.count;
     const size_t batch_size = in_work.batch_size;
     const size_t total_bytes = count * batch_size;
+    const size_t data_stream_id = in_work.data_stream_id;
+    if (data_stream_id != id) {
+      SRTB_LOGW << " [write_multi_filterbank_pipe_impl] "
+                << "data_stream_id = " << data_stream_id
+                << " != writer id = " << id << srtb::endl;
+      return srtb::work::dummy_work{};
+    }
 
     auto h_in_shared =
         srtb::host_allocator.allocate_shared<std::byte>(total_bytes);
@@ -81,19 +90,21 @@ class write_multi_filterbank_pipe {
                                    .str();
       file_path = srtb::config.baseband_output_file_prefix + name;
 
-      SRTB_LOGI << " [write_multi_filterbank_pipe] "
+      SRTB_LOGI << " [write_multi_filterbank_pipe_impl] "
                 << "Writing to " << name << srtb::endl;
 
       // create file handle
       if (std::filesystem::exists(file_path)) [[unlikely]] {
         throw std::runtime_error{
-            "[write_multi_filterbank_pipe] file already exists: " + file_path};
+            "[write_multi_filterbank_pipe_impl] file already exists: " +
+            file_path};
       }
       opt_fout.emplace(file_path.c_str(), std::ios::binary);
       auto& fout = opt_fout.value();
       if (fout.bad()) [[unlikely]] {
         throw std::runtime_error{
-            "[write_multi_filterbank_pipe] cannot write to " + file_path};
+            "[write_multi_filterbank_pipe_impl] cannot write to " +
+            file_path};
       }
 
       // write header
@@ -131,7 +142,8 @@ class write_multi_filterbank_pipe {
 
     if (!fout) [[unlikely]] {
       throw std::runtime_error{
-          "[write_multi_filterbank_pipe] cannot write to " + file_path};
+          "[write_multi_filterbank_pipe_impl] cannot write to " +
+          file_path};
     }
 
     written_bytes += total_bytes;
@@ -144,6 +156,39 @@ class write_multi_filterbank_pipe {
       file_number++;
     }
 
+    return srtb::work::dummy_work{};
+  }
+};
+
+class write_multi_filterbank_pipe {
+ public:
+  using in_work_type = write_multi_filterbank_pipe_impl::in_work_type;
+  using out_work_type = write_multi_filterbank_pipe_impl::out_work_type;
+
+ protected:
+  sycl::queue q;
+  std::map<uint64_t,
+           std::tuple<std::jthread,
+                      std::shared_ptr<srtb::work_queue<in_work_type> > > >
+      dispatch_table;
+
+ public:
+  explicit write_multi_filterbank_pipe(sycl::queue q_) : q{q_} {}
+
+  auto operator()([[maybe_unused]] std::stop_token stop_token,
+                  in_work_type in_work) -> out_work_type {
+    const uint64_t data_stream_id = in_work.data_stream_id;
+    if (!dispatch_table.contains(data_stream_id)) {
+      auto work_queue = std::make_shared<srtb::work_queue<in_work_type> >();
+      std::jthread impl_thread =
+          srtb::pipeline::start_pipe<write_multi_filterbank_pipe_impl>(
+              q, queue_in_functor{*work_queue}, dummy_out_functor{},
+              data_stream_id);
+      dispatch_table.emplace(std::make_pair(
+          data_stream_id, std::make_tuple(std::move(impl_thread), work_queue)));
+    }
+
+    std::get<1>(dispatch_table.at(data_stream_id))->push(in_work);
     return srtb::work::dummy_work{};
   }
 };
