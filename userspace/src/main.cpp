fxzjshm/simple-radio-telescope-backend

commit f747efaf321a78320c42806ab4a7070359e528ee

[userspace] pipeline: support multiple UDP formats
Enable runtime configuration of UDP packet format used.
Refer to packet parser for format of these packets.
Data separation during unpack should also be properly handled.

Format names are temporary, as it's not clear how widely these formats are used.

userspace/src/main.cpp
@@ -213,21 +213,14 @@ int main(int argc, char** argv) {
   // setup threads for pipes
   using namespace srtb::pipeline;
 
-  std::jthread unpack_thread;
-  if (srtb::config.baseband_format_type.starts_with("interleaved_samples_2")) {
-    unpack_thread = srtb::pipeline::start_pipe<composite_pipe<
-        copy_to_device_pipe, unpack_interleaved_samples_2_pipe> >(
-        q, queue_in_functor{copy_to_device_queue},
-        multiple_works_out_functor{queue_out_functor{fft_1d_r2c_queue}});
-  } else if (srtb::config.baseband_format_type == "simple") {
-    unpack_thread = srtb::pipeline::start_pipe<
-        composite_pipe<copy_to_device_pipe, unpack_pipe> >(
-        q, queue_in_functor{copy_to_device_queue},
-        queue_out_functor{fft_1d_r2c_queue});
-  } else {
-    throw std::runtime_error{"[main] Unknown baseband_format_type = " +
-                             srtb::config.baseband_format_type};
-  }
+  std::jthread copy_to_device_thread =
+      srtb::pipeline::start_pipe<copy_to_device_pipe>(
+          q, queue_in_functor{copy_to_device_queue},
+          queue_out_functor{unpack_queue});
+
+  std::jthread unpack_thread = srtb::pipeline::start_unpack_pipe(
+      srtb::config.baseband_format_type, q, queue_in_functor{unpack_queue},
+      queue_out_functor{fft_1d_r2c_queue});
 
   std::jthread fft_1d_r2c_thread = srtb::pipeline::start_pipe<fft_1d_r2c_pipe>(
       q, queue_in_functor{fft_1d_r2c_queue},
@@ -351,7 +344,7 @@ int main(int argc, char** argv) {
           return std::optional{srtb::work::dummy_work{}};
         },
         multiple_out_functors_functor{queue_out_functor{copy_to_device_queue},
-                             increase_work_count}));
+                                      increase_work_count}));
     input_pipe_count = 1;
   } else {
     if (input_file_path != "") {
@@ -363,8 +356,8 @@ int main(int argc, char** argv) {
               << "Receiving UDP packets" << srtb::endl;
     allocate_memory_regions(input_pipe_count);
     for (size_t i = 0; i < input_pipe_count; i++) {
-      input_thread.push_back(srtb::pipeline::start_pipe<udp_receiver_pipe>(
-          q,
+      input_thread.push_back(srtb::pipeline::start_udp_receiver_pipe(
+          srtb::config.baseband_format_type, q,
           // don't wait for last work
           [work_in_pipeline_count](
               [[maybe_unused]] std::stop_token stop_token) {
@@ -372,13 +365,14 @@ int main(int argc, char** argv) {
             return std::optional{srtb::work::dummy_work{}};
           },
           multiple_out_functors_functor{queue_out_functor{copy_to_device_queue},
-                               increase_work_count},
+                                        increase_work_count},
           /* id = */ i));
     }
   }
 
   std::vector<std::jthread> threads;
   threads = std::move(input_thread);
+  threads.push_back(std::move(copy_to_device_thread));
   threads.push_back(std::move(unpack_thread));
   threads.push_back(std::move(fft_1d_r2c_thread));
   threads.push_back(std::move(rfi_mitigation_s1_thread));

userspace/include/srtb/config.hpp
@@ -107,10 +107,9 @@ struct configs {
 
   /**
    * @brief Type of baseband format: 
-   *          simple (just stream of samples from 1 source), 
-   *          interleaved_samples_2 (interleaved 2 stream, each sample from one stream)
+   *        naocpsr_roach2, naocpsr_snap1, gznupsr_a1
    */
-  std::string baseband_format_type = "simple";
+  std::string baseband_format_type = "naocpsr_roach2";
 
   /**
    * @brief Lowerest frequency of received baseband signal, in MHz.

userspace/include/srtb/pipeline/read_file_pipe.hpp
@@ -19,6 +19,7 @@
 
 #include "srtb/coherent_dedispersion.hpp"
 #include "srtb/commons.hpp"
+#include "srtb/io/backend_registry.hpp"
 #include "srtb/pipeline/framework/pipe.hpp"
 
 namespace srtb {
@@ -60,15 +61,13 @@ class read_file_pipe {
       // count per polarization
       auto baseband_input_count = srtb::config.baseband_input_count;
       auto baseband_input_bits = srtb::config.baseband_input_bits;
-      size_t count_of_polarization = 1;
-      if (srtb::config.baseband_format_type.starts_with(
-              "interleaved_samples_2")) {
-        count_of_polarization = 2;
-      }
+      const size_t data_stream_count =
+          srtb::io::backend_registry::get_data_stream_count(
+              srtb::config.baseband_format_type);
 
       const size_t time_sample_bytes = baseband_input_count *
                                        std::abs(baseband_input_bits) /
-                                       BITS_PER_BYTE * count_of_polarization;
+                                       BITS_PER_BYTE * data_stream_count;
 
       std::shared_ptr<char> h_in_shared =
           srtb::host_allocator.allocate_shared<char>(time_sample_bytes);
@@ -85,9 +84,9 @@ class read_file_pipe {
 
       // reserved some samples for next round
       const size_t nsamps_reserved = srtb::codd::nsamps_reserved();
-      const std::streamoff reserved_bytes =
-          nsamps_reserved * std::abs(baseband_input_bits) / BITS_PER_BYTE *
-          count_of_polarization;
+      const std::streamoff reserved_bytes = nsamps_reserved *
+                                            std::abs(baseband_input_bits) /
+                                            BITS_PER_BYTE * data_stream_count;
       if (static_cast<size_t>(reserved_bytes) < time_sample_bytes) {
         logical_file_pos -= reserved_bytes;
         input_file_stream.seekg(logical_file_pos);

userspace/include/srtb/pipeline/udp_receiver_pipe.hpp
@@ -17,6 +17,7 @@
 #include <optional>
 
 #include "srtb/coherent_dedispersion.hpp"
+#include "srtb/io/backend_registry.hpp"
 #include "srtb/io/udp/asio_udp_packet_provider.hpp"
 #include "srtb/io/udp/packet_parser.hpp"
 #include "srtb/io/udp/udp_receiver.hpp"
@@ -31,17 +32,17 @@ namespace pipeline {
  * @see @c srtb::io::udp::udp_receiver_worker
  * TODO: separate reserving samples for coherent dedispersion
  */
+template <typename PacketProvider, typename PacketParser>
 class udp_receiver_pipe {
  protected:
   sycl::queue q;
-  std::optional<srtb::io::udp::udp_receiver_worker<
-      srtb::io::udp::asio_packet_provider,
-      srtb::io::udp::naocpsr_roach2_packet_parser> >
+  std::optional<
+      srtb::io::udp::udp_receiver_worker<PacketProvider, PacketParser> >
       opt_worker;
   /**
    * @brief identifier for different pipe instances
    * 
-   * e.g. id == 0 and 1 for two polarizations
+   * e.g. id == 0 and 1 for two polarizations, or 0-3 for 4 antennas.
    */
   size_t id;
 
@@ -113,11 +114,7 @@ class udp_receiver_pipe {
     const size_t baseband_input_bytes = baseband_input_count *
                                         std::abs(baseband_input_bits) /
                                         srtb::BITS_PER_BYTE;
-    size_t count_of_polarization = 1;
-    if (srtb::config.baseband_format_type.starts_with(
-            "interleaved_samples_2")) {
-      count_of_polarization = 2;
-    }
+    const size_t data_stream_count = PacketParser::data_stream_count;
 
     // reserved some samples for next round
     size_t nsamps_reserved = srtb::codd::nsamps_reserved();
@@ -138,8 +135,8 @@ class udp_receiver_pipe {
               << "id = " << id << ": "
               << "start receiving" << srtb::endl;
     auto [h_ptr, first_counter] = worker.receive(
-        /* required_length = */ baseband_input_bytes * count_of_polarization,
-        /* reserved_length = */ nsamps_reserved * count_of_polarization);
+        /* required_length = */ baseband_input_bytes * data_stream_count,
+        /* reserved_length = */ nsamps_reserved * data_stream_count);
     SRTB_LOGD << " [udp receiver pipe] "
               << "id = " << id << ": "
               << "receive finished" << srtb::endl;
@@ -152,7 +149,7 @@ class udp_receiver_pipe {
             .count();
 
     srtb::work::baseband_data_holder baseband_data{
-        h_ptr, baseband_input_bytes * count_of_polarization};
+        h_ptr, baseband_input_bytes * data_stream_count};
     srtb::work::copy_to_device_work copy_to_device_work;
     copy_to_device_work.ptr = nullptr;
     copy_to_device_work.count = 0;
@@ -172,6 +169,32 @@ class udp_receiver_pipe {
     return std::optional{copy_to_device_work};
   }
 };
+
+template <typename... Args>
+inline auto start_udp_receiver_pipe(std::string_view backend_name,
+                                    Args... args) {
+  using namespace srtb::io::backend_registry;
+  using provider_t = srtb::io::udp::asio_packet_provider;
+
+  if (backend_name == naocpsr_roach2::name) {
+    using backend_t = naocpsr_roach2;
+    using pipe_t = udp_receiver_pipe<provider_t, backend_t::packet_parser>;
+    return start_pipe<pipe_t>(args...);
+  }
+  if (backend_name == naocpsr_snap1::name) {
+    using backend_t = naocpsr_snap1;
+    using pipe_t = udp_receiver_pipe<provider_t, backend_t::packet_parser>;
+    return start_pipe<pipe_t>(args...);
+  }
+  if (backend_name == gznupsr_a1::name) {
+    using backend_t = gznupsr_a1;
+    using pipe_t = udp_receiver_pipe<provider_t, backend_t::packet_parser>;
+    return start_pipe<pipe_t>(args...);
+  }
+  throw std::invalid_argument{
+      "[start_udp_receiver_pipe] Unknown backend name: " +
+      std::string{backend_name}};
+}
 
 }  // namespace pipeline
 }  // namespace srtb

userspace/include/srtb/pipeline/unpack_pipe.hpp
@@ -18,6 +18,7 @@
 
 #include "srtb/fft/fft_window.hpp"
 #include "srtb/pipeline/framework/pipe.hpp"
+#include "srtb/pipeline/framework/pipe_io.hpp"
 #include "srtb/unpack.hpp"
 
 namespace srtb {
@@ -201,7 +202,7 @@ class unpack_interleaved_samples_2_pipe {
       // -8 -> int8_t / signed char / i8
       using T = int8_t;
       // std::string.contains (c++23) :(
-      if (srtb::config.baseband_format_type.find("snap1") !=
+      if (srtb::config.baseband_format_type.find("naocpsr_snap1") !=
           std::string::npos) {
         srtb::unpack::unpack_naocpsr_snap1(reinterpret_cast<T*>(d_in), d_out_1,
                                            d_out_2, out_count,
@@ -387,6 +388,27 @@ class unpack_gznupsr_a1_v2_1_pipe {
     return std::optional{fft_1d_r2c_work};
   }
 };
+
+template <typename InFunctor, typename OutFunctor, typename... Args>
+inline auto start_unpack_pipe(std::string_view backend_name, sycl::queue q,
+                              InFunctor in_functor, OutFunctor out_functor,
+                              Args... args) {
+  using namespace srtb::io::backend_registry;
+
+  if (backend_name == naocpsr_roach2::name) {
+    return start_pipe<unpack_pipe>(q, in_functor, out_functor, args...);
+  }
+  if (backend_name == naocpsr_snap1::name) {
+    return start_pipe<unpack_interleaved_samples_2_pipe>(
+        q, in_functor, multiple_works_out_functor{out_functor}, args...);
+  }
+  if (backend_name == gznupsr_a1::name) {
+    return start_pipe<unpack_gznupsr_a1_v2_1_pipe>(
+        q, in_functor, multiple_works_out_functor{out_functor}, args...);
+  }
+  throw std::invalid_argument{"[start_unpack_pipe] Unknown backend name: " +
+                              std::string{backend_name}};
+}
 
 }  // namespace pipeline
 }  // namespace srtb

userspace/include/srtb/program_options.hpp
@@ -81,9 +81,7 @@ namespace program_options {
        "Currently supported: 1(uint1), 2(uint2), 4(uint4), 8(uint8), -8(int8), 32(float), 64(double)")
       ("baseband_format_type", boost::program_options::value<std::string>(),
        "Type of baseband format: "
-       "simple (just stream of samples from 1 source), "
-       "interleaved_samples_2 (interleaved 2 stream, each sample from one stream, in '1' '2' '1' '2' ... format), "
-       "interleaved_samples_2:snap1 (interleaved 2 stream, each sample from one stream, in '1' '1' '2' '2' ... format)")
+       "naocpsr_roach2, naocpsr_snap1, gznupsr_a1")
       ("baseband_freq_low", boost::program_options::value<std::string>(),
        "Lowerest frequency of received baseband signal, in MHz. ")
       ("baseband_bandwidth", boost::program_options::value<std::string>(),

userspace/src/21cma-genfil/21cma-genfil.cpp
@@ -72,7 +72,7 @@ namespace main {
  */
 int genfil_21cma(int argc, char** argv) {
   srtb::config.udp_receiver_can_restart = false;
-  srtb::config.baseband_format_type = "interleaved_samples_2:snap1";
+  srtb::config.baseband_format_type = "naocpsr_snap1";
   srtb::changed_configs = srtb::program_options::parse_arguments(
       argc, argv, std::string(srtb::config.config_file_name));
   srtb::program_options::apply_changed_configs(srtb::changed_configs,

commit 8140c4a2fdc8841b695c07c415134fa352880f0d

[userspace] main: fix pipeline work count
This commit should fix the functions of work_in_pipeline_count, which is added to comment.

Fixes: f747efa ("[userspace] pipeline: support multiple UDP formats")
Signed-off-by: fxzjshm <fxzjshm@163.com>

userspace/src/main.cpp
@@ -206,9 +206,30 @@ int main(int argc, char** argv) {
   srtb::work_queue<srtb::work::signal_detect_work> signal_detect_queue;
   srtb::work_queue<srtb::work::write_signal_work> baseband_output_queue;
 
-  /** @brief count of works in a pipeline. */
+  /** 
+   * @brief count of works in a pipeline.
+   * This work count is utilized for these functions:
+   * * pipeline should exit only after all works are done, especially when reading files
+   * * when reading file without GUI, pipeline should exit after end of file
+   * * when reading file, there should be only one file in pipeline, to reduce VRAM usage
+   */
   std::shared_ptr<std::atomic<int32_t> > work_in_pipeline_count =
       std::make_shared<std::atomic<int32_t> >(0);
+  auto increase_work_count = [work_in_pipeline_count](
+                                 [[maybe_unused]] std::stop_token stop_token,
+                                 [[maybe_unused]] auto work) {
+    // take in account that unpack_pipe may generate more than 1 works per input work
+    (*work_in_pipeline_count) +=
+        srtb::io::backend_registry::get_data_stream_count(
+            srtb::config.baseband_format_type);
+    assert((*work_in_pipeline_count) >= 0);
+  };
+  auto decrease_work_count = [work_in_pipeline_count](
+                                 [[maybe_unused]] std::stop_token stop_token,
+                                 [[maybe_unused]] auto work) {
+    (*work_in_pipeline_count)--;
+    assert((*work_in_pipeline_count) >= 0);
+  };
 
   // setup threads for pipes
   using namespace srtb::pipeline;
@@ -274,12 +295,6 @@ int main(int argc, char** argv) {
           queue_out_functor{baseband_output_queue});
 
   std::jthread baseband_output_thread;
-  auto decrease_work_count = [work_in_pipeline_count](
-                                 [[maybe_unused]] std::stop_token stop_token,
-                                 [[maybe_unused]] auto work) {
-    (*work_in_pipeline_count)--;
-    assert((*work_in_pipeline_count) >= 0);
-  };
   if (srtb::config.baseband_write_all) {
     SRTB_LOGW << " [main] "
               << "Writing all baseband data, take care of disk space!"
@@ -318,12 +333,6 @@ int main(int argc, char** argv) {
                size_t{1});
 
   std::vector<std::jthread> input_thread;
-  auto increase_work_count = [work_in_pipeline_count](
-                                 [[maybe_unused]] std::stop_token stop_token,
-                                 [[maybe_unused]] auto work) {
-    (*work_in_pipeline_count)++;
-    assert((*work_in_pipeline_count) >= 0);
-  };
   auto input_file_path = srtb::config.input_file_path;
   if (std::filesystem::exists(input_file_path)) {
     SRTB_LOGI << " [main] "