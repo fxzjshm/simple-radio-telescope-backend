fxzjshm/simple-radio-telescope-backend

commit 55ecb01b7214d36af5f85df8f99e8e80ddf0dedd

[21cma-make_beam] main: implement median absolute deviation based 32 to 8 conversion
Signed-off-by: fxzjshm <fxzjshm@163.com>

userspace/src/21cma-make_beam/make_beam.cpp
@@ -16,6 +16,7 @@
 #include <fstream>
 #include <functional>
 #include <future>
+#include <limits>
 #include <map>
 #include <memory>
 #include <optional>
@@ -52,6 +53,7 @@
 #include "global_variables.hpp"
 #include "program_options.hpp"
 #include "rfi_mitigation.hpp"
+#include "statistics.hpp"
 
 namespace srtb::_21cma::make_beam {
 
@@ -64,7 +66,7 @@ struct mem {
   SizeType count;
   using Type = Pointer::element_type;
 
-  operator std::span<Type, std::dynamic_extent>() { return std::span{ptr.get(), count}; }
+  operator std::span<Type>() { return std::span{ptr.get(), count}; }
 
   template <typename... T>
   auto get_mdspan(T... sizes) {
@@ -150,10 +152,19 @@ auto main(int argc, char **argv) -> int {
       device_allocator.allocate_shared<srtb::complex<srtb::real>>(n_station * n_channel), n_station * n_channel};
   mem<std::shared_ptr<srtb::complex<srtb::real>>, size_t> d_formed = {
       device_allocator.allocate_shared<srtb::complex<srtb::real>>(n_sample * n_channel), n_sample * n_channel};
+
+  // normalization coefficients: new_val = scale.k * old_val + scale.b;
+  struct scale_t {
+    srtb::real k, b;
+  };
+  std::optional<scale_t> opt_scale;
+
   mem<std::shared_ptr<srtb::real>, size_t> d_cut = {
       device_allocator.allocate_shared<srtb::real>(n_sample * n_channel_remain), n_sample * n_channel_remain};
-  mem<std::shared_ptr<srtb::real>, size_t> h_cut = {
-      host_allocator.allocate_shared<srtb::real>(n_sample * n_channel_remain), n_sample * n_channel_remain};
+  mem<std::shared_ptr<uint8_t>, size_t> d_out = {device_allocator.allocate_shared<uint8_t>(n_sample * n_channel_remain),
+                                                 n_sample * n_channel_remain};
+  mem<std::shared_ptr<uint8_t>, size_t> h_out = {host_allocator.allocate_shared<uint8_t>(n_sample * n_channel_remain),
+                                                 n_sample * n_channel_remain};
 
   std::vector<std::ofstream> fout{n_pointing};
   BOOST_ASSERT(n_pointing == cfg.out_path.size());
@@ -223,7 +234,7 @@ auto main(int argc, char **argv) -> int {
       send(fout.at(i_pointing), "tsamp", double{1.0 / sample_rate * n_channel * 2});
       send(fout.at(i_pointing), "nbeams", static_cast<int>(n_pointing));
       send(fout.at(i_pointing), "ibeam", static_cast<int>(i_pointing));
-      send(fout.at(i_pointing), "nbits", static_cast<int>(32));
+      send(fout.at(i_pointing), "nbits", static_cast<int>(8));
       send(fout.at(i_pointing), "nifs", int{1});
       send(fout.at(i_pointing), "src_raj", double{to_sigproc_dms(pointing.ra_hour)});
       send(fout.at(i_pointing), "src_dej", double{to_sigproc_dms(pointing.dec_deg)});
@@ -315,6 +326,7 @@ auto main(int argc, char **argv) -> int {
           break;
       }
 
+      // cut spectrum
       auto d_cut_ = d_cut.get_mdspan(n_sample, n_channel_remain);
       q.parallel_for(sycl::range<1>{n_channel_remain * n_sample}, [=](sycl::item<1> id) {
          const auto i = id.get_id(0);
@@ -334,11 +346,65 @@ auto main(int argc, char **argv) -> int {
         srtb::_21cma::spectrum::mitigate_rfi_spectral_kurtosis_method(d_cut_, sk_threshold.value(), q);
       }
 
+      // quantize
+#define SRTB_21CMA_USE_MAD
+#ifdef SRTB_21CMA_USE_MAD
+      // current range: [max(0, median - 3 * mad), median + 10 * mad] -> [0, 255],
+      // where mad is median absolute deviation, mad(x) = median({x - median(x)})
+      // TODO: 10 seems arbitary, alway check this with bandpass
+#else
+      // RFI makes average & standard deviation
+#endif
+      if (!opt_scale.has_value()) {
+        auto d_mad_in = d_cut;
+        std::span<srtb::real> d_mad_in_ = d_mad_in;
+        auto [avg_val, std_val] = srtb::_21cma::make_beam::standard_deviation(d_mad_in_, q);
+#ifdef SRTB_21CMA_USE_MAD
+        mem<std::shared_ptr<srtb::real>, size_t> d_temp1 = {
+            device_allocator.allocate_shared<srtb::real>(d_mad_in.count), d_mad_in.count};
+        mem<std::shared_ptr<srtb::real>, size_t> d_temp2 = {
+            device_allocator.allocate_shared<srtb::real>(d_mad_in.count), d_mad_in.count};
+        std::span<srtb::real> d_temp1_ = d_temp1, d_temp2_ = d_temp2;
+        auto [median_val, mad_val] =
+            srtb::_21cma::make_beam::median_absolute_deviation(d_mad_in_, d_temp1_, d_temp2_, q);
+        const srtb::real real_range_min = sycl::max(srtb::real{0}, median_val - 3 * mad_val);
+        const srtb::real real_range_max = median_val + 10 * mad_val;
+#else
+        const srtb::real real_range_min = sycl::max(srtb::real{0}, avg_val - std_val);
+        const srtb::real real_range_max = avg_val + std_val;
+#endif
+        const srtb::real real_range_size = real_range_max - real_range_min;
+        constexpr srtb::real uint8_range_min = std::numeric_limits<uint8_t>::min();
+        constexpr srtb::real uint8_range_max = std::numeric_limits<uint8_t>::max();
+        constexpr srtb::real uint8_range_size = uint8_range_max - uint8_range_min;
+        opt_scale = scale_t{.k = uint8_range_size / real_range_size,
+                            .b = uint8_range_size / real_range_size * (-real_range_min)};
+#ifdef SRTB_21CMA_USE_MAD
+        SRTB_LOGI << " [21cma-make_beam] " << " [quantize] " << "median_val = " << median_val << ", "
+                  << "median absolute deviation = " << mad_val << srtb::endl;
+#endif
+        SRTB_LOGI << " [21cma-make_beam] " << " [quantize] " << "avg_val = " << avg_val << ", "
+                  << "std_val = " << std_val << srtb::endl;
+        SRTB_LOGI << " [21cma-make_beam] " << " [quantize] " << "scale.k = " << opt_scale.value().k << ", "
+                  << "scale.b = " << opt_scale.value().b << srtb::endl;
+      }
+      BOOST_ASSERT(d_cut.count == d_out.count);
+      std::span<srtb::real> d_cut_span = d_cut;
+      std::span<uint8_t> d_out_span = d_out;
+      auto scale = opt_scale.value();
+      q.parallel_for(sycl::range<1>{d_out.count}, [=](sycl::item<1> id) {
+         const auto i = id.get_id(0);
+         constexpr srtb::real uint8_range_min = std::numeric_limits<uint8_t>::min();
+         constexpr srtb::real uint8_range_max = std::numeric_limits<uint8_t>::max();
+         d_out_span[i] = static_cast<uint8_t>(
+             std::max(std::min(scale.k * d_cut_span[i] + scale.b, uint8_range_max), uint8_range_min));
+       }).wait();
+
       // write out
-      BOOST_ASSERT(d_cut.count == h_cut.count);
-      q.copy(d_cut.ptr.get(), h_cut.ptr.get(), d_cut.count).wait();
+      BOOST_ASSERT(d_out.count == h_out.count);
+      q.copy(d_out.ptr.get(), h_out.ptr.get(), d_out.count).wait();
       fout.at(i_pointing)
-          .write(reinterpret_cast<char *>(h_cut.ptr.get()), h_cut.count * sizeof(decltype(h_cut.ptr)::element_type));
+          .write(reinterpret_cast<char *>(h_out.ptr.get()), h_out.count * sizeof(decltype(h_out.ptr)::element_type));
       if (!fout.at(i_pointing)) {
         throw std::runtime_error{"Cannot write to output, i_pointing = " + std::to_string(i_pointing)};
       }

userspace/src/21cma-make_beam/merge_sort.hpp
@@ -1,0 +1,136 @@
+//  Distributed under the Boost Software License, Version 1.0.
+//  See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt
+
+#pragma once
+#ifndef __SRTB_21CMA_MAKE_BEAM_MERGE_SORT__
+#define __SRTB_21CMA_MAKE_BEAM_MERGE_SORT__
+
+// merge_sort from Boost.Compute
+
+#include <cstddef>
+#include <iterator>
+#include <sycl/sycl.hpp>
+#include <type_traits>
+
+namespace sycl_stl {
+
+/** Calculate NdRange.
+ * @brief Calculates an nd_range with a global size divisable by problemSize
+ * @param problemSize : The problem size
+ */
+inline sycl::nd_range<1> calculateNdRange(sycl::queue& q, size_t problemSize) {
+  const auto& d = q.get_device();
+  const sycl::id<3> maxWorkItemSizes = d.template get_info<sycl::info::device::max_work_item_sizes<3> >();
+  const auto localSize = std::min(
+      problemSize, std::min(d.template get_info<sycl::info::device::max_work_group_size>(), maxWorkItemSizes[0]));
+
+  size_t globalSize;
+  if (problemSize % localSize == 0) {
+    globalSize = problemSize;
+  } else {
+    globalSize = (problemSize / localSize + 1) * localSize;
+  }
+
+  return sycl::nd_range<1>{sycl::range<1>{globalSize}, sycl::range<1>{localSize}};
+}
+
+// reference: boost/compute/algorithm/detail/merge_sort_on_gpu.hpp
+template <class InputIterator, class OutputIterator, class Compare>
+inline void merge_blocks_on_gpu(sycl::queue& queue, InputIterator input_iterator, OutputIterator output_iterator,
+                                Compare compare, const size_t count, const size_t block_size) {
+  typedef typename std::iterator_traits<InputIterator>::value_type key_type;
+
+  const auto ndRange = calculateNdRange(queue, count);
+
+  auto f = [=](sycl::handler& h) {
+    auto input = input_iterator;
+    auto output = output_iterator;
+    h.parallel_for(ndRange, [=](sycl::nd_item<1> id) {
+      const size_t gid = id.get_global_id(0);
+      if (gid >= count) {
+        return;
+      }
+      const key_type my_key = input[gid];
+      const size_t my_block_idx = gid / block_size;
+      const bool my_block_idx_is_odd = my_block_idx & 0x1;
+      const size_t other_block_idx = my_block_idx_is_odd ? my_block_idx - 1 : my_block_idx + 1;
+      const size_t my_block_start = std::min(my_block_idx * block_size, count);
+      const size_t my_block_end = std::min((my_block_idx + 1) * block_size, count);
+      const size_t other_block_start = std::min(other_block_idx * block_size, count);
+      const size_t other_block_end = std::min((other_block_idx + 1) * block_size, count);
+      if (other_block_start == count) {
+        output[gid] = my_key;
+        return;
+      }
+      size_t left_idx = other_block_start;
+      size_t right_idx = other_block_end;
+      while (left_idx < right_idx) {
+        size_t mid_idx = (left_idx + right_idx) / 2;
+        key_type mid_key = input[mid_idx];
+        bool smaller = compare(mid_key, my_key);
+        left_idx = smaller ? mid_idx + 1 : left_idx;
+        right_idx = smaller ? right_idx : mid_idx;
+      }
+      right_idx = other_block_end;
+      if (my_block_idx_is_odd && left_idx != right_idx) {
+        key_type upper_key = input[left_idx];
+        while (!(compare(upper_key, my_key)) && !(compare(my_key, upper_key)) && left_idx < right_idx) {
+          size_t mid_idx = (left_idx + right_idx) / 2;
+          key_type mid_key = input[mid_idx];
+          bool equal = !(compare(mid_key, my_key)) && !(compare(my_key, mid_key));
+          left_idx = equal ? mid_idx + 1 : left_idx + 1;
+          right_idx = equal ? right_idx : mid_idx;
+          upper_key = input[left_idx];
+        }
+      }
+      size_t offset = 0;
+      offset += gid - my_block_start;
+      offset += left_idx - other_block_start;
+      offset += std::min(my_block_start, other_block_start);
+      output[offset] = my_key;
+    });
+  };
+  queue.submit(f).wait();
+}
+
+// reference: boost/compute/algorithm/detail/merge_sort_on_gpu.hpp
+template <class Iterator, class Iterator2, class Compare>
+inline void merge_sort_on_gpu(sycl::queue& queue, Iterator first, Iterator last, Iterator2 temp_keys, Compare compare) {
+  typedef typename std::iterator_traits<Iterator>::value_type key_type;
+  static_assert(std::is_same_v<key_type, typename std::iterator_traits<Iterator2>::value_type>);
+
+  size_t count = std::distance(first, last);
+  if (count < 2) {
+    return;
+  }
+
+  size_t block_size = 1;
+
+  bool result_in_temporary_buffer = false;
+
+  for (; block_size < count; block_size *= 2) {
+    result_in_temporary_buffer = !result_in_temporary_buffer;
+    if (result_in_temporary_buffer) {
+      merge_blocks_on_gpu(queue, first, temp_keys, compare, count, block_size);
+    } else {
+      merge_blocks_on_gpu(queue, temp_keys, first, compare, count, block_size);
+    }
+  }
+
+  if (result_in_temporary_buffer) {
+    // ::sycl::impl::copy(exec, temp_keys, temp_keys + count, first);
+    queue
+        .parallel_for(sycl::range<1>{count},
+                      [=](sycl::item<1> id) {
+                        const auto i = id.get_id(0);
+                        first[i] = temp_keys[i];
+                      })
+        .wait();
+  }
+  queue.wait();
+}
+
+}  // namespace sycl_stl
+
+#endif  // __SRTB_21CMA_MAKE_BEAM_MERGE_SORT__

userspace/src/21cma-make_beam/statistics.hpp
@@ -1,0 +1,100 @@
+/*******************************************************************************
+ * Copyright (c) 2024 fxzjshm
+ * 21cma-make_beam is licensed under Mulan PubL v2.
+ * You can use this software according to the terms and conditions of the Mulan PubL v2.
+ * You may obtain a copy of Mulan PubL v2 at:
+ *          http://license.coscl.org.cn/MulanPubL-2.0
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PubL v2 for more details.
+ ******************************************************************************/
+
+#pragma once
+#ifndef __SRTB_21CMA_MAKE_BEAM_STATISTICS__
+#define __SRTB_21CMA_MAKE_BEAM_STATISTICS__
+
+#include <span>
+#include <sycl/execution_policy>
+// --
+#include <sycl/algorithm/transform_reduce.hpp>
+
+#include "srtb/sycl.hpp"
+// ---
+#include "assert.hpp"
+#include "merge_sort.hpp"
+
+namespace srtb::_21cma::make_beam {
+
+/**
+ * @brief Calculate median value of input array. 
+ *        Input in d_temp1 but will be overwritten by sorted values.
+ */
+template <typename T>
+[[nodiscard]]
+inline auto median(std::span<T> d_temp1, std::span<T> d_temp2, sycl::queue& q) -> T {
+  BOOST_ASSERT(d_temp1.size() == d_temp2.size());
+  const auto n = d_temp1.size();
+  sycl_stl::merge_sort_on_gpu(q, d_temp1.data(), d_temp1.data() + n, d_temp2.data(), std::less<T>());
+  // d_temp1 should be sorted
+  T median_val;
+  q.copy(d_temp1.data() + (n / 2), &median_val, 1).wait();
+  return median_val;
+}
+
+/**
+ * @brief Calculate median value of input array. 
+ *        Input in d_in and will not be overwritten, but needs 1 copy to d_temp1.
+ *        After execution, d_temp1 is sorted version of d_in.
+ */
+template <typename T>
+[[nodiscard]]
+inline auto median(const std::span<T> d_in, std::span<T> d_temp1, std::span<T> d_temp2, sycl::queue& q) -> T {
+  BOOST_ASSERT(d_in.size() == d_temp1.size());
+  BOOST_ASSERT(d_in.size() == d_temp2.size());
+  const auto n = d_in.size();
+  q.copy(d_in.data(), d_temp1.data(), n).wait();
+  return median(d_temp1, d_temp2);
+}
+
+/**
+ * @brief get median & mean absolute deviation of d_in
+ */
+template <typename T>
+[[nodiscard]]
+inline auto median_absolute_deviation(std::span<T> d_in, std::span<T> d_temp1, std::span<T> d_temp2,
+                                      sycl::queue& q) -> std::pair<T, T> {
+  BOOST_ASSERT(d_in.size() == d_temp1.size());
+  BOOST_ASSERT(d_in.size() == d_temp2.size());
+  const auto n = d_in.size();
+  q.copy(d_in.data(), d_temp1.data(), n).wait();
+  const T in_median = median(d_temp1, d_temp2, q);
+  // now d_temp1 is sorted version of d_in
+  q.parallel_for(sycl::range<1>{n}, [=](sycl::item<1> id) {
+     const auto i = id.get_id(0);
+     d_temp1[i] = sycl::fabs(d_temp1[i] - in_median);
+   }).wait();
+  sycl_stl::merge_sort_on_gpu(q, d_temp1.data(), d_temp1.data() + n, d_temp2.data(), std::less<T>());
+  // d_temp1 should be sorted again
+  T mad;
+  q.copy(d_temp1.data() + (n / 2), &mad, 1).wait();
+  return std::pair{in_median, mad};
+}
+
+template <typename T>
+[[nodiscard]]
+inline auto standard_deviation(std::span<T> d_in, sycl::queue& q) -> std::pair<T, T> {
+  sycl::sycl_execution_policy<> snp{q};
+  const T sum_val =
+      sycl::impl::transform_reduce(snp, d_in.data(), d_in.data() + d_in.size(), std::identity(), T{0}, std::plus());
+  const T avg_val = sum_val / d_in.size();
+  const T std2_val = sycl::impl::transform_reduce(
+      snp, d_in.data(), d_in.data() + d_in.size(), [=](T x) { return (x - avg_val) * (x - avg_val); }, T{0},
+      std::plus());
+  const T std_val = sycl::sqrt(std2_val);
+  return std::pair{avg_val, std_val};
+}
+
+}  // namespace srtb::_21cma::make_beam
+
+#endif  // __SRTB_21CMA_MAKE_BEAM_GET_DELAYS__

commit 980b1ea5e238cde0284171bef822d3d0ad532ea7

[21cma-make_beam] main: update doc
Fixes: 55ecb01 ("[21cma-make_beam] main: implement median absolute deviation based 32 to 8 conversion")
Signed-off-by: fxzjshm <fxzjshm@163.com>

userspace/src/21cma-make_beam/make_beam.cpp
@@ -351,9 +351,8 @@ auto main(int argc, char **argv) -> int {
 #ifdef SRTB_21CMA_USE_MAD
       // current range: [max(0, median - 3 * mad), median + 10 * mad] -> [0, 255],
       // where mad is median absolute deviation, mad(x) = median({x - median(x)})
+      // RFI makes average & standard deviation unusable
       // TODO: 10 seems arbitary, alway check this with bandpass
-#else
-      // RFI makes average & standard deviation
 #endif
       if (!opt_scale.has_value()) {
         auto d_mad_in = d_cut;