fxzjshm/simple-radio-telescope-backend

commit 499311bbff8b2a92d5083947939a491ede63e3d2

[userspace] multi_reduce: make this function async
Signed-off-by: fxzjshm <fxzjshm@163.com>

userspace/include/srtb/algorithm/multi_reduce.hpp
@@ -62,9 +62,10 @@ struct sycl_algorithm_descriptor {
 // ref:  https://stackoverflow.com/questions/17862078/reduce-matrix-rows-with-cuda
 template <typename InputIterator, typename OutputIterator, typename Reduce,
           typename Map>
-void multi_mapreduce(InputIterator input, size_t count_per_batch,
+[[nodiscard]]
+auto multi_mapreduce(InputIterator input, size_t count_per_batch,
                      size_t batch_size, OutputIterator output, Map map,
-                     Reduce reduce, sycl::queue& q) {
+                     Reduce reduce, sycl::queue& q) -> sycl::event {
   using B = typename std::iterator_traits<OutputIterator>::value_type;
 
   const size_t total_count = count_per_batch * batch_size;
@@ -111,46 +112,46 @@ void multi_mapreduce(InputIterator input, size_t count_per_batch,
    * 'reduce': B * B -> B
    */
 
-  q.submit([&](sycl::handler& cgh) {
-     sycl::range<1> rg{d.nb_work_group};
-     sycl::range<1> ri{d.nb_work_item};
-     sycl::local_accessor<B, 1> sum{sycl::range<1>(d.nb_work_item), cgh};
-     cgh.parallel_for(
-         sycl::nd_range<1>(rg * ri, ri), [=](sycl::nd_item<1> nd_item) {
-           size_t group_id = nd_item.get_group(0);
-           size_t group_begin = group_id * d.size_per_work_group;
-           size_t group_end =
-               std::min((group_id + 1) * d.size_per_work_group, d.size);
-           //assert(group_id < d.nb_work_group);
-           //assert(group_begin < group_end); //< as we properly selected the
-           //  number of work_group
-           size_t local_id = nd_item.get_local_id(0);
-           size_t local_pos = group_begin + local_id;
-           if (local_pos < group_end) {
-             //we peal the first iteration
-             B acc = map(local_pos, input[local_pos]);
-             for (size_t read = local_pos + d.nb_work_item; read < group_end;
-                  read += d.nb_work_item) {
-               acc = reduce(acc, map(read, input[read]));
-             }
-             sum[local_id] = acc;
-           }
+  return q.submit([&](sycl::handler& cgh) {
+    sycl::range<1> rg{d.nb_work_group};
+    sycl::range<1> ri{d.nb_work_item};
+    sycl::local_accessor<B, 1> sum{sycl::range<1>(d.nb_work_item), cgh};
+    cgh.parallel_for(
+        sycl::nd_range<1>(rg * ri, ri), [=](sycl::nd_item<1> nd_item) {
+          size_t group_id = nd_item.get_group(0);
+          size_t group_begin = group_id * d.size_per_work_group;
+          size_t group_end =
+              std::min((group_id + 1) * d.size_per_work_group, d.size);
+          //assert(group_id < d.nb_work_group);
+          //assert(group_begin < group_end); //< as we properly selected the
+          //  number of work_group
+          size_t local_id = nd_item.get_local_id(0);
+          size_t local_pos = group_begin + local_id;
+          if (local_pos < group_end) {
+            //we peal the first iteration
+            B acc = map(local_pos, input[local_pos]);
+            for (size_t read = local_pos + d.nb_work_item; read < group_end;
+                 read += d.nb_work_item) {
+              acc = reduce(acc, map(read, input[read]));
+            }
+            sum[local_id] = acc;
+          }
 
-           nd_item.barrier(sycl::access::fence_space::local_space);
+          nd_item.barrier(sycl::access::fence_space::local_space);
 
-           // reduce_over_group, but SYCL 2020 doesn't support custom operations
-           // so custom one is used
-           if (local_id == 0) {
-             B acc = sum[0];
-             for (size_t i = 1;
-                  i < std::min(d.nb_work_item, group_end - group_begin); i++) {
-               acc = reduce(acc, sum[i]);
-             }
+          // reduce_over_group, but SYCL 2020 doesn't support custom operations
+          // so custom one is used
+          if (local_id == 0) {
+            B acc = sum[0];
+            for (size_t i = 1;
+                 i < std::min(d.nb_work_item, group_end - group_begin); i++) {
+              acc = reduce(acc, sum[i]);
+            }
 
-             output[group_id] = acc;
-           }
-         });
-   }).wait();
+            output[group_id] = acc;
+          }
+        });
+  });
 }
 
 template <typename InputIterator, typename OutputIterator, typename Reduce>

userspace/include/srtb/pipeline/signal_detect_pipe.hpp
@@ -101,7 +101,7 @@ class signal_detect_pipe {
 
     srtb::algorithm::multi_mapreduce(
         d_in, count_per_batch, batch_size, d_time_series, map,
-        /* reduce = */ sycl::plus<srtb::real>(), q);
+        /* reduce = */ sycl::plus<srtb::real>(), q).wait();
 
     const srtb::real snr_threshold =
         srtb::config.signal_detect_signal_noise_threshold;

userspace/include/srtb/spectrum/rfi_mitigation.hpp
@@ -74,8 +74,8 @@ inline auto eval_rfi_ranges(const std::string& mitigate_rfi_freq_list) {
     boost::split(number_strings, str, boost::is_any_of("-"),
                  boost::token_compress_on);
     if (number_strings.size() != 2) [[unlikely]] {
-      SRTB_LOGW << " [eval_rfi_ranges] "
-                << "cannot parse \"" << str << "\"" << srtb::endl;
+      SRTB_LOGW << " [eval_rfi_ranges] " << "cannot parse \"" << str << "\""
+                << srtb::endl;
     } else {
       const srtb::real freq_1 =
           static_cast<srtb::real>(std::stod(number_strings[0]));
@@ -323,7 +323,8 @@ inline void mitigate_rfi_spectral_kurtosis_method_2(
       [](power_pair a, power_pair b) {
         return power_pair{a.first + b.first, a.second + b.second};
       },
-      q);
+      q)
+      .wait();
 
   q.parallel_for(sycl::range<1>(total_size), [=](sycl::item<1> id) {
      const size_t i = id.get_id(0);

userspace/tests/test-multi_reduce.cpp
@@ -51,7 +51,8 @@ int main(int argc, char** argv) {
                 []() { return static_cast<T>(std::rand() % 256); });
   q.copy(&h_in[0], /* -> */ d_in, total_count).wait();
   srtb::algorithm::multi_mapreduce(d_in, count_per_batch, batch_size, d_out,
-                                   map, reduce, q);
+                                   map, reduce, q)
+      .wait();
   std::vector<T> h_out, h_ref;
   h_out.resize(batch_size);
   h_ref.resize(batch_size);

commit 6a4fd1483227e89f8c9e00d2294b8b34404f16d9

[userspace] multi_reduce: make helper function async
Fixes: 499311b ("[userspace] multi_reduce: make this function async")
Signed-off-by: fxzjshm <fxzjshm@163.com>

userspace/include/srtb/algorithm/multi_reduce.hpp
@@ -155,7 +155,8 @@ auto multi_mapreduce(InputIterator input, size_t count_per_batch,
 }
 
 template <typename InputIterator, typename OutputIterator, typename Reduce>
-void multi_reduce(InputIterator input, size_t count_per_batch,
+[[nodiscard]]
+auto multi_reduce(InputIterator input, size_t count_per_batch,
                   size_t batch_size, OutputIterator output, Reduce reduce,
                   sycl::queue& q) {
   return multi_mapreduce(input, count_per_batch, batch_size, output,