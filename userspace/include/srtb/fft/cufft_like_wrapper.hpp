fxzjshm/simple-radio-telescope-backend

commit 8fc1e0c36b9dfa1db70011735d5d6f177c2236e4

[userspace] fft: add option to share work area
to reduce VRAM usage

haven't found FFTW equivalent of this

userspace/include/srtb/fft/cufft_like_wrapper.hpp
@@ -40,6 +40,12 @@ template <std::floating_point T, sycl::backend backend>
 struct cufft_like_trait;
 
 inline std::mutex cufft_like_mutex;
+#ifdef SRTB_FFT_SHARE_WORK_AREA
+// TODO: this seem ugly, re-design this
+inline std::shared_ptr<std::byte> cufft_like_shared_work_area;
+inline size_t cufft_like_shared_work_area_size = 0;
+inline std::atomic<size_t> cufft_like_shared_work_area_users = 0;
+#endif  // SRTB_FFT_SHARE_WORK_AREA
 
 /** @brief common wrapper for different vendor's FFT libraries that has a cufft-like API design. */
 template <sycl::backend backend, srtb::fft::type fft_type,
@@ -129,18 +135,51 @@ class cufft_like_1d_wrapper
       SRTB_CHECK_CUFFT_LIKE(ret_val);
     }
 
+    set_queue_impl(q);
+
+#ifdef SRTB_FFT_SHARE_WORK_AREA
+    const size_t required_work_area_bytes = workSize;
+    if (required_work_area_bytes > cufft_like_shared_work_area_size) {
+      // TODO: is this thread-safe ?
+      cufft_like_shared_work_area_size = 0;
+      cufft_like_shared_work_area.reset();
+      cufft_like_shared_work_area =
+          srtb::device_allocator.allocate_shared<std::byte>(
+              required_work_area_bytes);
+      cufft_like_shared_work_area_size = required_work_area_bytes;
+    }
+    cufft_like_shared_work_area_users++;
+    // set this first, so internal buffer is destroyed
+    // will re-set each execution in case it changes (TODO: does this affect performance ?)
+    set_work_area_impl(
+        reinterpret_cast<void*>(cufft_like_shared_work_area.get()));
+#endif  // SRTB_FFT_SHARE_WORK_AREA
+
     SRTB_LOGI << " [cufft_like_wrapper] "
               << "plan finished. workSize = " << workSize << srtb::endl;
-    set_queue_impl(q);
   }
 
-  void destroy_impl() { SRTB_CHECK_CUFFT_LIKE(trait::fftDestroy(plan)); }
+  void destroy_impl() {
+    SRTB_CHECK_CUFFT_LIKE(trait::fftDestroy(plan));
+#ifdef SRTB_FFT_SHARE_WORK_AREA
+    cufft_like_shared_work_area_users--;
+    if (cufft_like_shared_work_area_users == 0) {
+      cufft_like_shared_work_area_size = 0;
+      cufft_like_shared_work_area.reset();
+    }
+#endif  // SRTB_FFT_SHARE_WORK_AREA
+  }
 
   // enable only if fft_type_ == srtb::fft::type::R2C_1D
   template <typename..., srtb::fft::type fft_type_ = fft_type,
             typename std::enable_if<(fft_type_ == srtb::fft::type::R2C_1D),
                                     int>::type = 0>
   void process_impl(T* in, C* out) {
+#ifdef SRTB_FFT_SHARE_WORK_AREA
+    std::lock_guard lock{srtb::fft::cufft_like_mutex};
+    set_work_area_impl(
+        reinterpret_cast<void*>(cufft_like_shared_work_area.get()));
+#endif  // SRTB_FFT_SHARE_WORK_AREA
     SRTB_CHECK_CUFFT_LIKE(trait::fftExecR2C(
         (*this).plan, static_cast<real*>(in), reinterpret_cast<complex*>(out)));
 
@@ -156,6 +195,11 @@ class cufft_like_1d_wrapper
     constexpr auto direction = (fft_type == srtb::fft::type::C2C_1D_BACKWARD)
                                    ? trait::BACKWARD
                                    : trait::FORWARD;
+#ifdef SRTB_FFT_SHARE_WORK_AREA
+    std::lock_guard lock{srtb::fft::cufft_like_mutex};
+    set_work_area_impl(
+        reinterpret_cast<void*>(cufft_like_shared_work_area.get()));
+#endif  // SRTB_FFT_SHARE_WORK_AREA
     SRTB_CHECK_CUFFT_LIKE(
         trait::fftExecC2C((*this).plan, reinterpret_cast<complex*>(in),
                           reinterpret_cast<complex*>(out), direction));
@@ -167,6 +211,11 @@ class cufft_like_1d_wrapper
             typename std::enable_if<(fft_type_ == srtb::fft::type::C2R_1D),
                                     int>::type = 0>
   void process_impl(C* in, T* out) {
+#ifdef SRTB_FFT_SHARE_WORK_AREA
+    std::lock_guard lock{srtb::fft::cufft_like_mutex};
+    set_work_area_impl(
+        reinterpret_cast<void*>(cufft_like_shared_work_area.get()));
+#endif  // SRTB_FFT_SHARE_WORK_AREA
     SRTB_CHECK_CUFFT_LIKE(trait::fftExecC2R(
         (*this).plan, reinterpret_cast<complex*>(in), static_cast<real*>(out)));
     flush();
@@ -202,6 +251,10 @@ class cufft_like_1d_wrapper
 #warning cufft_like_wrapper::set_queue_impl uses default stream
     stream = nullptr;
 #endif  // SYCL_IMPLEMENTATION_ONEAPI or __HIPSYCL__
+  }
+
+  void set_work_area_impl(void* work_area) {
+    SRTB_CHECK_CUFFT_LIKE(trait::fftSetWorkArea(plan, work_area));
   }
 
   void flush() {

userspace/include/srtb/config.hpp
@@ -50,6 +50,10 @@ using complex = std::complex<T>;
 // option to use managed/unified memory as device memory
 #define SRTB_USE_USM_SHARED_MEMORY
 
+// option to share work area between FFT plans
+// may reduce VRAM usage but increase latency
+#define SRTB_FFT_SHARE_WORK_AREA
+
 /**
  * @brief initial capacity of srtb::work_queue
  */

userspace/include/srtb/fft/cufft_wrapper.hpp
@@ -74,6 +74,11 @@ struct cufft_common_trait {
   static inline decltype(auto) fftSetStream(Args&&... args) {
     return cufftSetStream(std::forward<Args>(args)...);
   }
+
+  template <typename... Args>
+  static inline decltype(auto) fftSetWorkArea(Args&&... args) {
+    return cufftSetWorkArea(std::forward<Args>(args)...);
+  }
 };
 
 template <>

userspace/include/srtb/fft/hipfft_wrapper.hpp
@@ -77,6 +77,11 @@ struct hipfft_common_trait {
   static inline decltype(auto) fftSetStream(Args&&... args) {
     return hipfftSetStream(std::forward<Args>(args)...);
   }
+
+  template <typename... Args>
+  static inline decltype(auto) fftSetWorkArea(Args&&... args) {
+    return hipfftSetWorkArea(std::forward<Args>(args)...);
+  }
 };
 
 template <>

commit d80a531f77baa0fc572d8bed2c8a77fb3d0e4c50

fft: fix sharing work area when using multiple devices
Apparently, different devices uses different shared work area, otherwise segmentation fault.

Fixes: 8fc1e0c ("[userspace] fft: add option to share work area")
Signed-off-by: fxzjshm <fxzjshm@163.com>

userspace/include/srtb/fft/cufft_like_wrapper.hpp
@@ -18,7 +18,8 @@
 #include <mutex>
 
 #include "srtb/fft/fft_wrapper.hpp"
-#include "srtb/global_variables.hpp"
+#include "srtb/memory/mem.hpp"
+#include "srtb/memory/sycl_device_allocator.hpp"
 
 #define SRTB_CHECK_CUFFT_LIKE_WRAPPER(expr, expected)                      \
   SRTB_CHECK(expr, expected,                                               \
@@ -41,10 +42,14 @@ struct cufft_like_trait;
 
 inline std::mutex cufft_like_mutex;
 #ifdef SRTB_FFT_SHARE_WORK_AREA
-// TODO: this seem ugly, re-design this
-inline std::shared_ptr<std::byte> cufft_like_shared_work_area;
-inline size_t cufft_like_shared_work_area_size = 0;
-inline std::atomic<size_t> cufft_like_shared_work_area_users = 0;
+struct shared_work_area {
+  srtb::mem<std::shared_ptr<std::byte>> mem;
+  size_t user_count;
+};
+using work_area_slot_t = std::pair<sycl::backend, /* device index */ int>;
+/** apparently, different devices uses different shared work area */
+std::map<work_area_slot_t, shared_work_area> shared_work_area_map;
+std::map<work_area_slot_t, srtb::memory::device_allocator<std::byte>> device_allocator_map;
 #endif  // SRTB_FFT_SHARE_WORK_AREA
 
 /** @brief common wrapper for different vendor's FFT libraries that has a cufft-like API design. */
@@ -69,10 +74,13 @@ class cufft_like_1d_wrapper
   fft_handle plan;
   size_t workSize;
   stream_t stream;
+  // this is not put into #ifdef SRTB_FFT_SHARE_WORK_AREA to provide binary compatibility
+  // in case some compiled w/ SRTB_FFT_SHARE_WORK_AREA but other modules not
+  work_area_slot_t work_area_slot;
 
  public:
   cufft_like_1d_wrapper(size_t n, size_t batch_size, sycl::queue& queue)
-      : super_class{n, batch_size, queue} {}
+      : super_class{n, batch_size, queue}, work_area_slot{backend, sycl::get_native<backend>(queue.get_device())} {}
 
  protected:
   void create_impl(size_t n, size_t batch_size, sycl::queue& q) {
@@ -87,6 +95,7 @@ class cufft_like_1d_wrapper
     auto device = q.get_device();
     auto native_device = sycl::get_native<backend>(device);
     SRTB_CHECK_CUFFT_LIKE_API(trait::SetDevice(native_device));
+    work_area_slot = std::pair{backend, native_device};
 
     SRTB_CHECK_CUFFT_LIKE(trait::fftCreate(&plan));
 #ifdef SRTB_FFT_SHARE_WORK_AREA
@@ -142,20 +151,21 @@ class cufft_like_1d_wrapper
 
 #ifdef SRTB_FFT_SHARE_WORK_AREA
     const size_t required_work_area_bytes = workSize;
-    if (required_work_area_bytes > cufft_like_shared_work_area_size) {
-      // TODO: is this thread-safe ?
-      cufft_like_shared_work_area_size = 0;
-      cufft_like_shared_work_area.reset();
-      cufft_like_shared_work_area =
-          srtb::device_allocator.allocate_shared<std::byte>(
-              required_work_area_bytes);
-      cufft_like_shared_work_area_size = required_work_area_bytes;
+    if (!device_allocator_map.contains(work_area_slot)) {
+      device_allocator_map.insert({work_area_slot, srtb::memory::device_allocator<std::byte>{q}});
     }
-    cufft_like_shared_work_area_users++;
+    // if (!shared_work_area_map.contains(work_area_slot)) {
+    //   shared_work_area_map.insert(std::pair{work_area_slot, shared_work_area{}});
+    //   shared_work_area_map[work_area_slot].mem.count = 0;
+    // }
+    if (required_work_area_bytes > shared_work_area_map[work_area_slot].mem.count) {
+      shared_work_area_map[work_area_slot].mem =
+          srtb::mem_allocate_shared<std::byte>(device_allocator_map.at(work_area_slot), required_work_area_bytes);
+    }
+    shared_work_area_map[work_area_slot].user_count++;
     // set this first, so internal buffer is destroyed
     // will re-set each execution in case it changes (TODO: does this affect performance ?)
-    set_work_area_impl(
-        reinterpret_cast<void*>(cufft_like_shared_work_area.get()));
+    set_work_area_impl(reinterpret_cast<void*>(shared_work_area_map[work_area_slot].mem.ptr.get()));
 #endif  // SRTB_FFT_SHARE_WORK_AREA
 
     SRTB_LOGI << " [cufft_like_wrapper] "
@@ -166,10 +176,9 @@ class cufft_like_1d_wrapper
     std::lock_guard lock{srtb::fft::cufft_like_mutex};
     SRTB_CHECK_CUFFT_LIKE(trait::fftDestroy(plan));
 #ifdef SRTB_FFT_SHARE_WORK_AREA
-    cufft_like_shared_work_area_users--;
-    if (cufft_like_shared_work_area_users == 0) {
-      cufft_like_shared_work_area_size = 0;
-      cufft_like_shared_work_area.reset();
+    shared_work_area_map.at(work_area_slot).user_count--;
+    if (shared_work_area_map.at(work_area_slot).user_count == 0) {
+      shared_work_area_map.at(work_area_slot).mem = {};
     }
 #endif  // SRTB_FFT_SHARE_WORK_AREA
   }
@@ -180,9 +189,7 @@ class cufft_like_1d_wrapper
                                     int>::type = 0>
   void process_impl(T* in, C* out) {
 #ifdef SRTB_FFT_SHARE_WORK_AREA
-    std::lock_guard lock{srtb::fft::cufft_like_mutex};
-    set_work_area_impl(
-        reinterpret_cast<void*>(cufft_like_shared_work_area.get()));
+    set_work_area();
 #endif  // SRTB_FFT_SHARE_WORK_AREA
     SRTB_CHECK_CUFFT_LIKE(trait::fftExecR2C(
         (*this).plan, static_cast<real*>(in), reinterpret_cast<complex*>(out)));
@@ -200,9 +207,7 @@ class cufft_like_1d_wrapper
                                    ? trait::BACKWARD
                                    : trait::FORWARD;
 #ifdef SRTB_FFT_SHARE_WORK_AREA
-    std::lock_guard lock{srtb::fft::cufft_like_mutex};
-    set_work_area_impl(
-        reinterpret_cast<void*>(cufft_like_shared_work_area.get()));
+    set_work_area();
 #endif  // SRTB_FFT_SHARE_WORK_AREA
     SRTB_CHECK_CUFFT_LIKE(
         trait::fftExecC2C((*this).plan, reinterpret_cast<complex*>(in),
@@ -216,13 +221,16 @@ class cufft_like_1d_wrapper
                                     int>::type = 0>
   void process_impl(C* in, T* out) {
 #ifdef SRTB_FFT_SHARE_WORK_AREA
-    std::lock_guard lock{srtb::fft::cufft_like_mutex};
-    set_work_area_impl(
-        reinterpret_cast<void*>(cufft_like_shared_work_area.get()));
+    set_work_area();
 #endif  // SRTB_FFT_SHARE_WORK_AREA
     SRTB_CHECK_CUFFT_LIKE(trait::fftExecC2R(
         (*this).plan, reinterpret_cast<complex*>(in), static_cast<real*>(out)));
     flush();
+  }
+
+  void set_work_area() {
+    std::lock_guard lock{srtb::fft::cufft_like_mutex};
+    return set_work_area_impl(reinterpret_cast<void*>(shared_work_area_map.at(work_area_slot).mem.ptr.get()));
   }
 
   bool has_inited_impl() {

userspace/include/srtb/memory/sycl_device_allocator.hpp
@@ -15,6 +15,8 @@
 #define __SRTB_MEMORY_DEVICE_ALLOCATOR__
 
 #include <exception>
+#include <memory>
+#include <type_traits>
 
 #include "srtb/sycl.hpp"
 
@@ -36,7 +38,7 @@ class device_allocator {
 
   explicit device_allocator(sycl::queue &queue_) : queue(queue_){};
 
-  T *allocate(std::size_t num_elements) {
+  T* allocate(std::size_t num_elements) {
     T *ptr = sycl::aligned_alloc_device<T>(align, num_elements, queue);
     if (!ptr) [[unlikely]] {
       throw std::runtime_error("device_allocator: Allocation failed");
@@ -44,9 +46,25 @@ class device_allocator {
     return ptr;
   }
 
-  void deallocate(T *ptr, std::size_t size) {
+  void deallocate(T* ptr, std::size_t size) {
     (void)size;
     if (ptr) sycl::free(ptr, queue);
+  }
+
+  template <typename U>  // for srtb::mem
+    requires(std::is_same_v<T, U>)
+  [[nodiscard]] std::shared_ptr<T> allocate_shared(size_t n) {
+    T* ptr = allocate(n);
+    auto deleter = [this, n](T* ptr) { deallocate(ptr, n); };
+    return std::shared_ptr<T>{ptr, deleter};
+  }
+
+  template <typename U>  // for srtb::mem
+    requires(std::is_same_v<T, U>)
+  [[nodiscard]] std::unique_ptr<T> allocate_unique(size_t n) {
+    T* ptr = allocate(n);
+    auto deleter = [this, n](T* ptr) { deallocate(ptr, n); };
+    return std::unique_ptr<T>{ptr, deleter};
   }
 
  private: